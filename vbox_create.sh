7a138f9fa1fbc4490d308eface384a632f361179
Justin Erenkrantz <justin@erenkrantz.com>
2013-05-04 15:10:46 -0400

    To ease bootstrapping, rewrite vbox_create.sh to create one bootstrap VM that will act as Chef server, Cobbler server, DHCP server, and gateway.

--- vbox_create.sh
+++ vbox_create.sh
@@ -14,6 +14,10 @@ pushd $DIR
 
 P=`python -c "import os.path; print os.path.abspath('./')"`
 
+if [ ! -f gpxe-1.0.1-80861004.rom ]; then
+  curl -o gpxe-1.0.1-80861004.rom "http://rom-o-matic.net/gpxe/gpxe-1.0.1/contrib/rom-o-matic/build.php" -H "Origin: http://rom-o-matic.net" -H "Host: rom-o-matic.net" -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" -H "Referer: http://rom-o-matic.net/gpxe/gpxe-1.0.1/contrib/rom-o-matic/build.php" -H "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3" --data "version=1.0.1&use_flags=1&ofmt=ROM+binary+%28flashable%29+image+%28.rom%29&nic=all-drivers&pci_vendor_code=8086&pci_device_code=1004&PRODUCT_NAME=&PRODUCT_SHORT_NAME=gPXE&CONSOLE_PCBIOS=on&BANNER_TIMEOUT=20&NET_PROTO_IPV4=on&COMCONSOLE=0x3F8&COMSPEED=115200&COMDATA=8&COMPARITY=0&COMSTOP=1&DOWNLOAD_PROTO_TFTP=on&DNS_RESOLVER=on&NMB_RESOLVER=off&IMAGE_ELF=on&IMAGE_NBI=on&IMAGE_MULTIBOOT=on&IMAGE_PXE=on&IMAGE_SCRIPT=on&IMAGE_BZIMAGE=on&IMAGE_COMBOOT=on&AUTOBOOT_CMD=on&NVO_CMD=on&CONFIG_CMD=on&IFMGMT_CMD=on&IWMGMT_CMD=on&ROUTE_CMD=on&IMAGE_CMD=on&DHCP_CMD=on&SANBOOT_CMD=on&LOGIN_CMD=on&embedded_script=&A=Get+Image"
+fi
+
 # Grab the Ubuntu 12.04 installer image
 if [ ! -f ubuntu-12.04-mini.iso ]; then
     curl -o ubuntu-12.04-mini.iso http://archive.ubuntu.com/ubuntu/dists/precise/main/installer-amd64/current/images/netboot/mini.iso
@@ -32,17 +36,18 @@ $VBM dhcpserver remove --ifname vboxnet2 || true
 $VBM hostonlyif ipconfig vboxnet0 --ip 10.0.100.2 --netmask 255.255.255.0
 $VBM hostonlyif ipconfig vboxnet1 --ip 172.16.100.2 --netmask 255.255.255.0
 $VBM hostonlyif ipconfig vboxnet2 --ip 192.168.100.2 --netmask 255.255.255.0
-# Create each VM
-for vm in bcpc-vm1 bcpc-vm2 bcpc-vm3; do
+
+# Create bootstrap VM
+for vm in bcpc-bootstrap; do
     # Only if VM doesn't exist
     if ! $VBM list vms | grep $vm ; then
         $VBM createvm --name $vm --ostype Ubuntu_64 --basefolder $P --register
-        $VBM modifyvm $vm --memory 2048
+        $VBM modifyvm $vm --memory 1024
         $VBM storagectl $vm --name "SATA Controller" --add sata
         $VBM storagectl $vm --name "IDE Controller" --add ide
         # Create a number of hard disks
         port=0
-        for disk in a b c d e; do
+        for disk in a; do
             $VBM createhd --filename $P/$vm/$vm-$disk.vdi --size $DRIVE_SIZE
             $VBM storageattach $vm --storagectl "SATA Controller" --device 0 --port $port --type hdd --medium $P/$vm/$vm-$disk.vdi
             port=$((port+1))
@@ -51,8 +56,31 @@ for vm in bcpc-vm1 bcpc-vm2 bcpc-vm3; do
         $VBM modifyvm $vm --nic1 hostonly --hostonlyadapter1 vboxnet0
         $VBM modifyvm $vm --nic2 hostonly --hostonlyadapter2 vboxnet1
         $VBM modifyvm $vm --nic3 hostonly --hostonlyadapter3 vboxnet2
+        $VBM modifyvm $vm --nic4 nat
         # Add the bootable mini ISO for installing Ubuntu 12.04
         $VBM storageattach $vm --storagectl "IDE Controller" --device 0 --port 0 --type dvddrive --medium ubuntu-12.04-mini.iso
+    fi
+done
+
+# Create each VM
+for vm in bcpc-vm1 bcpc-vm2 bcpc-vm3; do
+    # Only if VM doesn't exist
+    if ! $VBM list vms | grep $vm ; then
+        $VBM createvm --name $vm --ostype Ubuntu_64 --basefolder $P --register
+        $VBM modifyvm $vm --memory 2048
+        $VBM storagectl $vm --name "SATA Controller" --add sata
+        # Create a number of hard disks
+        port=0
+        for disk in a b c d e; do
+            $VBM createhd --filename $P/$vm/$vm-$disk.vdi --size $DRIVE_SIZE
+            $VBM storageattach $vm --storagectl "SATA Controller" --device 0 --port $port --type hdd --medium $P/$vm/$vm-$disk.vdi
+            port=$((port+1))
+        done
+        # Add the network interfaces
+        $VBM modifyvm $vm --nic1 hostonly --hostonlyadapter1 vboxnet0 --nictype1 82543GC
+        $VBM setextradata $vm VBoxInternal/Devices/pcbios/0/Config/LanBootRom $P/gpxe-1.0.1-80861004.rom
+        $VBM modifyvm $vm --nic2 hostonly --hostonlyadapter2 vboxnet1
+        $VBM modifyvm $vm --nic3 hostonly --hostonlyadapter3 vboxnet2
     fi
 done
 
