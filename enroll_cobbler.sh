76098e6676ae1e1f8e3b1893b5569f5478ba2054
Pravir Chandra <pchandra7@bloomberg.net>
2013-05-16 15:59:24 -0400

    don't abort if cobbler remove fails

--- enroll_cobbler.sh
+++ enroll_cobbler.sh
@@ -14,9 +14,9 @@ for i in bcpc-vm1 bcpc-vm2 bcpc-vm3; do
   MAC=`VBoxManage showvminfo --machinereadable $i | grep macaddress1 | cut -d \" -f 2 | sed 's/.\{2\}/&:/g;s/:$//'`
   echo "Registering $i with $MAC for ${subnet}.${node}"
   if hash vagrant; then
-    vagrant ssh -c "sudo cobbler system remove --name=$i && sudo cobbler system add --name=$i --hostname=$i --profile=bcpc_host --ip-address=${subnet}.${node} --mac=${MAC}"
+    vagrant ssh -c "sudo cobbler system remove --name=$i; sudo cobbler system add --name=$i --hostname=$i --profile=bcpc_host --ip-address=${subnet}.${node} --mac=${MAC}"
   else
-    ssh -t -i $KEYFILE root@$1 "cobbler system remove --name=$i && cobbler system add --name=$i --hostname=$i --profile=bcpc_host --ip-address=${subnet}.${node} --mac=${MAC}"
+    ssh -t -i $KEYFILE root@$1 "cobbler system remove --name=$i; cobbler system add --name=$i --hostname=$i --profile=bcpc_host --ip-address=${subnet}.${node} --mac=${MAC}"
   fi
   let node=node+1
 done
