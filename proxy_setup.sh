a14a81467e34c3155fc0e90feed0440fb0ec0884
Chris Morgan <cmorgan2@bloomberg.net>
2013-05-29 09:39:04 -0400

    provide an example proxy setup

---
+++ proxy_setup.sh
@@ -0,0 +1,7 @@
+# proxy
+#export http_proxy=http://10.0.1.12:3128/
+#export https_proxy=https://10.0.1.12:3128/
+#export GIT_SSL_NO_VERIFY=true
+#export CURL='curl -k -l -x http://10.0.1.12:3128/'
+# no proxy
+export CURL=curl
