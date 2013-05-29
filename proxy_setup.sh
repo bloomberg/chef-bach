5383b9bb4355da66efee4fc7fab26e4f22a1c533
Chris Morgan <cmorgan2@bloomberg.net>
2013-05-29 11:29:33 -0400

    use the example of a proxy on the hypervisor

--- proxy_setup.sh
+++ proxy_setup.sh
@@ -4,17 +4,17 @@
 # Only define http_proxy if you will be using a proxy
 #
 #
-# sample setup using a local squid cache at 10.0.1.12
+# sample setup using a local squid cache at 10.0.1.2 - the hypervisor
 # change to reflect your real proxy info
 #
-#export http_proxy=http://10.0.1.12:3128/
-#export https_proxy=https://10.0.1.12:3128/
+export http_proxy=http://10.0.100.2:3128/
+export https_proxy=https://10.0.100.2:3128/
 #
 # to ignore SSL errors
 #
-#export GIT_SSL_NO_VERIFY=true
-#export CURL='curl -k -l -x http://10.0.1.12:3128/'
+export GIT_SSL_NO_VERIFY=true
+export CURL='curl -k -l -x http://10.0.100.2:3128/'
 #
 # no proxy
 #
-export CURL=curl
+#export CURL=curl
