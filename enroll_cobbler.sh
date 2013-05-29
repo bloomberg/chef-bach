6cdb035398cab467f8e9d5020e9f7bbb6f4dfe6d
Chris Morgan <cmorgan2@bloomberg.net>
2013-05-29 15:14:07 -0400

    still have to use cobbler account and sudo as far as I can see

--- enroll_cobbler.sh
+++ enroll_cobbler.sh
@@ -25,7 +25,7 @@ for i in bcpc-vm1 bcpc-vm2 bcpc-vm3; do
   if hash vagrant; then
     vagrant ssh -c "sudo cobbler system remove --name=$i; sudo cobbler system add --name=$i --hostname=$i --profile=bcpc_host --ip-address=${subnet}.${node} --mac=${MAC}"
   else
-    ssh -t -i $KEYFILE root@$1 "cobbler system remove --name=$i; cobbler system add --name=$i --hostname=$i --profile=bcpc_host --ip-address=${subnet}.${node} --mac=${MAC}"
+    ssh -t -i $KEYFILE ubuntu@$1 "sudo cobbler system remove --name=$i; sudo cobbler system add --name=$i --hostname=$i --profile=bcpc_host --ip-address=${subnet}.${node} --mac=${MAC}"
   fi
   let node=node+1
 done
@@ -33,5 +33,5 @@ done
 if hash vagrant; then
   vagrant ssh -c "sudo cobbler sync"
 else
-  ssh -t -i $KEYFILE root@$1 "cobbler sync"
+  ssh -t -i $KEYFILE ubuntu@$1 "sudo cobbler sync"
 fi
