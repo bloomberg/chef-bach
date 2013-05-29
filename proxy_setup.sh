440b010af696c3302e0a06ebc76ee21d759e585a
Chris Morgan <cmorgan2@bloomberg.net>
2013-05-29 10:08:15 -0400

    even more comments

--- proxy_setup.sh
+++ proxy_setup.sh
@@ -1,10 +1,17 @@
 # proxy setup
 #
+# Make sure this file defines CURL in any case
+# Only define http_proxy if you will be using a proxy
+#
+#
 # sample setup using a local squid cache at 10.0.1.12
 # change to reflect your real proxy info
 #
 #export http_proxy=http://10.0.1.12:3128/
 #export https_proxy=https://10.0.1.12:3128/
+#
+# to ignore SSL errors
+#
 #export GIT_SSL_NO_VERIFY=true
 #export CURL='curl -k -l -x http://10.0.1.12:3128/'
 #
