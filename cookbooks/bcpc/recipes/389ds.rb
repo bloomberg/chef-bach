ac6a2aeb655a5fa29268aa6ad96e24dc7fbd591e
Pravir Chandra <chandra@list.org>
2013-07-14 17:25:28 -0400

    finished conifguration of base 389-ds services

--- cookbooks/bcpc/recipes/389ds.rb
+++ cookbooks/bcpc/recipes/389ds.rb
@@ -25,6 +25,8 @@ ruby_block "initialize-389ds-config" do
         make_config('389ds-admin-password', secure_password)
         make_config('389ds-rootdn-user', "cn=Directory Manager")
         make_config('389ds-rootdn-password', secure_password)
+        make_config('389ds-replication-user', "cn=Replication Manager")
+        make_config('389ds-replication-password', secure_password)
     end
 end
 
@@ -42,7 +44,27 @@ end
 bash "setup-389ds-server" do
 	user "root"
 	code <<-EOH
-		setup-ds-admin --file=/tmp/389ds-install.inf -k -s
+        service haproxy stop
+        setup-ds-admin --file=/tmp/389ds-install.inf -k -s
+        service dirsrv stop
+        service dirsrv-admin stop
+        service haproxy start
+        sed --in-place 's/^ServerLimit.*/ServerLimit 64/' /etc/dirsrv/admin-serv/httpd.conf
+        sed --in-place '/^MinSpareThreads/d' /etc/dirsrv/admin-serv/httpd.conf
+        sed --in-place '/^MaxSpareThreads/d' /etc/dirsrv/admin-serv/httpd.conf
+        sed --in-place '/^ThreadsPerChild/d' /etc/dirsrv/admin-serv/httpd.conf
+        sed --in-place 's/^nsslapd-port.*/nsslapd-listenhost: #{node[:bcpc][:management][:ip]}\\nnsslapd-port: 389/' /etc/dirsrv/slapd-#{node.hostname}/dse.ldif
+        echo "dn: cn=Replication Manager,cn=config" >> /etc/dirsrv/slapd-#{node.hostname}/dse.ldif
+        echo "objectClass: inetorgperson" >> /etc/dirsrv/slapd-#{node.hostname}/dse.ldif
+        echo "objectClass: person" >> /etc/dirsrv/slapd-#{node.hostname}/dse.ldif
+        echo "objectClass: top" >> /etc/dirsrv/slapd-#{node.hostname}/dse.ldif
+        echo "cn: Replication Manager" >> /etc/dirsrv/slapd-#{node.hostname}/dse.ldif
+        echo "sn: RM" >> /etc/dirsrv/slapd-#{node.hostname}/dse.ldif
+        echo "userPassword: #{get_config('389ds-replication-password')}" >> /etc/dirsrv/slapd-#{node.hostname}/dse.ldif
+        echo "passwordExpirationTime: 20380119031407Z" >> /etc/dirsrv/slapd-#{node.hostname}/dse.ldif
+        echo "nsIdleTimeout: 0" >> /etc/dirsrv/slapd-#{node.hostname}/dse.ldif
+        service dirsrv start
+        service dirsrv-admin start
 	EOH
 	not_if "test -d /etc/dirsrv/slapd-#{node.hostname}"
 end
