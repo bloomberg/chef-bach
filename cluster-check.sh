d2508f6627a7b96c9753cbad08bd8c70e7ffde55
Chris Morgan <cmorgan2@bloomberg.net>
2013-07-17 16:44:16 -0400

    remove hard-coded environment name. Add Usage hint

--- cluster-check.sh
+++ cluster-check.sh
@@ -2,6 +2,8 @@
 #
 # A simple tool to check the basics of our cluster - machines up, services running
 # This tool uses the cluster hardware list file cluster.txt
+# The first param specifies the environment
+# For the second, optional param :
 # - if no param is passed, all nodes are checked
 # - if 'head' is passed only head nodes are checked
 # - if 'work' is passed only work nodes are checked
@@ -9,16 +11,22 @@
 # This may be helpful as a quick check after completing the knife
 # bootstrap phase (assigning roles to nodes).
 #
+if [[ -z "$1" || -z "$2" ]]; then
+    echo "Usage $0 'environment' (role)"
+    exit
+fi
+#set -x
+ENVIRONMENT="$1"
+ROLEWANTED="$2"
 if [[ -f cluster.txt ]]; then
     while read HOSTNAME MACADDR IPADDR DOMAIN ROLE; do
 	if [[ $HOSTNAME = "end" ]]; then
 	    continue
 	fi
-	if [[ -z "$1" || "$1" = "$ROLE"  ]]; then
+	if [[ -z "$ROLEWANTED" || "$ROLEWANTED" = "$ROLE"  ]]; then
 	    HOSTS="$HOSTS $HOSTNAME"
 	fi
     done < cluster.txt
-
     echo "HOSTS = $HOSTS"
 
     for HOST in $HOSTS; do
@@ -28,26 +36,27 @@ if [[ -f cluster.txt ]]; then
 	    echo $HOST is up
 	    UP=$[UP + 1]
 	fi
-	if [[ -z `./nodessh.sh DNY1 $HOST "ip route show table mgmt | grep default"` ]]; then
+	if [[ -z `./nodessh.sh $ENVIRONMENT $HOST "ip route show table mgmt | grep default"` ]]; then
 	    echo "$HOST no mgmt default route !!WARNING!!"
 	else
 	    echo "$HOST has a default mgmt route"
 	    MG=$[MG + 1]
 	fi
-	if [[ -z `./nodessh.sh DNY1 $HOST "ip route show table storage | grep default"` ]]; then
+	if [[ -z `./nodessh.sh $ENVIRONMENT $HOST "ip route show table storage | grep default"` ]]; then
 	    echo "$HOST has no storage default route !!WARNING!!"
 	else
 	    echo "$HOST has a default storage route"
 	    SG=$[SG + 1]
 	fi
-	for SERVICE in keystone glance-api glance-registry cinder-scheduler cinder-volume cinder-api nova-api nova-novncproxy nova-scheduler nova-consoleauth nova-cert nova-conductor nova-compute nova-network; do
-	    STAT=`./nodessh.sh DNY1 $HOST "service $SERVICE status | grep running" sudo`
+	for SERVICE in keystone glance-api glance-registry cinder-scheduler cinder-volume cinder-api nova-api nova-novncproxy nova-scheduler nova-consoleauth nova-cert nova-conductor nova-compute no
+va-network; do
+	    STAT=`./nodessh.sh $ENVIRONMENT $HOST "service $SERVICE status | grep running" sudo`
 	    if [[ ! "$STAT" =~ "unrecognized" ]]; then
 		STAT=`echo $STAT | cut -f2 -d":"`
 		printf "%20s %s\n" "$SERVICE" "$STAT"
 	    fi
 	done
-	STAT=`./nodessh.sh DNY1 $HOST "ceph -s | grep HEALTH" sudo`
+	STAT=`./nodessh.sh $ENVIRONMENT $HOST "ceph -s | grep HEALTH" sudo`
 	STAT=`echo $STAT | cut -f2 -d":"`
 	printf "%20s %s\n" ceph "$STAT"
 	echo
@@ -55,4 +64,4 @@ if [[ -f cluster.txt ]]; then
 else
 	echo "Warning 'cluster.txt' not found"
 fi
-echo "$UP hosts up. $MG hosts with default mgmt route. $SG hosts with default storage route"
+echo "$ENVIRONMENT cluster summary: $UP hosts up. $MG hosts with default mgmt route. $SG hosts with default storage route"
