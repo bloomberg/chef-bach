0699a6073e3e3e48199c0147217d7316d6ead133
Chris Morgan <cmorgan2@bloomberg.net>
2013-08-07 13:12:42 -0400

    finish off param checking

--- cluster-vip.sh
+++ cluster-vip.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 if [[ -z "$1" ]]; then
-	if [[ -d environments ]]; then
-		ENVIRONMENTS=`echo environments/*.json | wc -l`
-	fi
-	if (($ENVIRONMENTS == 1)); then
-		VIP=`grep -i vip environments/*.json | awk '{print $3}' | awk '{print substr($0, 2, length() - 3)}'`
-		echo "VIP from environment = $VIP"
-	else
-		echo "Usage: VIP not specified and more than one environment file found"
-		exit
-	fi
+    if [[ -d environments ]]; then
+	ENVIRONMENTS=`ls -lt environments/*.json | wc -l`
+    else
+	echo "Usage: VIP not specified and can't find environment directory"
+	exit
+    fi
+    if (($ENVIRONMENTS == 1)); then
+	VIP=`grep -i vip environments/*.json | awk '{print $3}' | awk '{print substr($0, 2, length() - 3)}'`
+	echo "VIP from environment = $VIP"
+    else
+	echo "Usage: VIP not specified and more than one environment file found"
+	exit
+    fi
 else 
-    echo "VIP IP address set to $1"
-    VIP="$1"
+    VIP=`grep -i vip environments/"$1".json | awk '{print $3}' | awk '{print substr($0, 2, length() - 3)}'`
+    echo "VIP IP address : $VIP"
 fi
 if [[ ! -z `which fping` ]]; then
     # fping fails faster than ping
-    UP=`fping -aq $VIP | awk '{print $1}'`
+    UP=`fping -aq "$VIP" | awk '{print $1}'`
 else
-    UP=`ping -c1 $VIP | grep ttl | awk '{print $4}'`
+    UP=`ping -c1 "$VIP" | grep ttl | awk '{print $4}'`
 fi
 if [[ ! -z "$UP" ]]; then
     MAC=`arp -n $VIP | grep ether | awk '{print $3}'`
