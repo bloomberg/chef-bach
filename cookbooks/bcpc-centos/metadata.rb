46a7b214149f5821a0da705e0b6ffa5807b323b1
Chris Longo <chris.longo@gmail.com>
2013-08-09 14:06:19 -0400

    default attributes

--- cookbooks/bcpc-centos/metadata.rb
+++ cookbooks/bcpc-centos/metadata.rb
@@ -1,8 +1,8 @@
 name             'bcpc-centos'
 maintainer       'Bloomberg, L.P.'
-maintainer_email 'clongo3@bloomberg.com'
+maintainer_email 'clongo3@bloomberg.net'
 license          "Apache License 2.0"
 description      'Installs/Configures CentOS hosts for use in Hadoop clusters'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 depends          'bcpc'
