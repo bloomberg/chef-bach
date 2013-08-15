025f5dd67d4683fa93b515e1ae58013c517b27d6
Chris Longo <chris.longo@gmail.com>
2013-08-15 15:56:53 -0400

    don't use bcpc default

--- cookbooks/bcpc-centos/recipes/default.rb
+++ cookbooks/bcpc-centos/recipes/default.rb
@@ -16,16 +16,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-require 'ipaddr'
-
-node.set['bcpc']['management']['ip'] = node['network']['interfaces'][node['bcpc']['management']['interface']]['addresses'].select {|k,v| v['family'] == "inet" and k != node['bcpc']['management']['vip'] }[0].first
-
-bitlen = (node['bcpc']['management']['cidr'].match /\d+\.\d+\.\d+\.\d+\/(\d+)/)[1].to_i
-hostaddr = IPAddr.new(node['bcpc']['management']['ip'])<<bitlen>>bitlen
-
-node.set['bcpc']['node_number'] = hostaddr.to_i.to_s
-node.set['bcpc']['storage']['ip'] = ((IPAddr.new(node['bcpc']['storage']['cidr'])>>(32-bitlen)<<(32-bitlen))|hostaddr).to_s
-node.set['bcpc']['floating']['ip'] = ((IPAddr.new(node['bcpc']['floating']['cidr'])>>(32-bitlen)<<(32-bitlen))|hostaddr).to_s
-
-node.save
