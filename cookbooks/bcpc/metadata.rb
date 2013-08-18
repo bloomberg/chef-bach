cffc7e55ce160f65287e15df108012cbdd8ce29e
Justin Erenkrantz <justin@erenkrantz.com>
2013-08-17 22:46:39 -0400

    Bump bcpc cookbook version to 0.4.0 and declare dependency on ntp cookbook.

--- cookbooks/bcpc/metadata.rb
+++ cookbooks/bcpc/metadata.rb
@@ -3,9 +3,10 @@ maintainer_email "pchandra7@bloomberg.net"
 license          "Apache License 2.0"
 description      "Installs/Configures Bloomberg Clustered Private Cloud (BCPC)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.0"
+version          "0.4.0"
 
 depends "apt", ">= 1.9.2"
 depends "ubuntu", ">= 1.1.2"
 depends "chef-client", ">= 2.2.2"
 depends "cron", ">= 1.2.2"
+depends "ntp", ">= 1.3.2"
