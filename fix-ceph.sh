143a69c9a1ebeef498b67ff917e477dad8462721
Chris Morgan <cmorgan2@bloomberg.net>
2013-08-20 13:56:14 -0400

    clarify fix-ceph.sh - this is going away soon however

--- fix-ceph.sh
+++ fix-ceph.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # adjust the following to suit your cluster
-NETWORK="10.0.100"
+STORAGE_NETWORK_PREFIX="10.0.100"
 
 set -x
-sed -i -e '/public network/ s/^/#/'  /etc/ceph/ceph.conf
-sed -i -e '/cluster network/ s/^/#/' /etc/ceph/ceph.conf
+sudo sed -i -e '/public network/ s/^/#/'  /etc/ceph/ceph.conf
+sudo sed -i -e '/cluster network/ s/^/#/' /etc/ceph/ceph.conf
 HOST=$(hostname)
-ADDR=`ifconfig -a | grep -i $NETWORK | awk '{print $2}' | cut -f2 -d':'`
+ADDR=`ifconfig -a | grep -i $STORAGE_NETWORK_PREFIX | awk '{print $2}' | cut -f2 -d':'`
 sudo ceph-mon --cluster=ceph --id=$HOST --public-addr=$ADDR -f
 set +x
