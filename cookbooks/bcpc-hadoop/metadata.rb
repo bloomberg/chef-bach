07dbeb9a92d620ea4fb69f84839f4e61852befbb
Rick Richardson <rick.richardson@gmail.com>
2013-10-21 10:39:35 -0400

    removing zookeeper cookbook depenency, using cloudera apt repo instead

--- cookbooks/bcpc-hadoop/metadata.rb
+++ cookbooks/bcpc-hadoop/metadata.rb
@@ -5,5 +5,4 @@ description      "Installs/Configures Bloomberg Clustered Private Hadoop Cloud (
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-depends "zookeeper", ">= 1.4.6"
 depends "bcpc", ">= 0.5.0"
