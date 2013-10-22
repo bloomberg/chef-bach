9f182375b8c07f95401757ec48e1c7756ee63f52
Chris Morgan <cmorgan2@bloomberg.net>
2013-10-22 15:53:44 -0400

    minor tweaks to cluster install scripts

--- cluster-rechef.sh
+++ cluster-rechef.sh
@@ -8,25 +8,30 @@
 #
 #set -x
 if [[ -z "$1" ]]; then
-   echo "Usage: $0 environment"
+	echo "Usage: $0 environment (role)"
+else
+	ENVIRONMENT="$1"
 fi
+$ROLE_REQUESTED="$2"
 if [[ -f cluster.txt ]]; then
     while read HOSTNAME MACADDR IPADDR ILOIPADDR DOMAIN ROLE; do
-	if [[ $HOSTNAME = "end" ]]; then
-	    continue
-	fi
-	if [[ -z "$1" || "$1" = "$ROLE"  ]]; then
-	    HOSTS="$HOSTS $HOSTNAME"
-	fi
+		if [[ $HOSTNAME = "end" ]]; then
+			continue
+		fi
+		if [[ -z "$ROLE_REQUESTED" || "$ROLE" = "$ROLE_REQUESTED"  ]]; then
+			HOSTS="$HOSTS $HOSTNAME"
+			IPS="$IPS $IPADDR"
+		fi
     done < cluster.txt
     echo "HOSTS = $HOSTS"
-    for HOST in $HOSTS; do
-	if [[ -z `fping -aq $HOST` ]]; then
-	    echo $HOST is down
-	    continue
-	else
-	    echo $HOST is up
-	fi
-	./nodessh.sh $1 $HOST "chef-client" sudo
+    for HOST in $IPS; do
+		if [[ -z `fping -aq $HOST` ]]; then
+			echo $HOST is down
+			continue
+		else
+			echo $HOST is up
+		fi
+		./nodessh.sh $ENVIRONMENT $HOST "chef-client" sudo
     done
 fi
+
--- cluster-whatsup.sh
+++ cluster-whatsup.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 # a script to find which nodes, defined in a cluster definition file
 # cluster.txt are responding to ping
+COUNT=0
 if [[ -f cluster.txt ]]; then
     # fping is fast but might not be available
     if [[ -z `which fping` ]]; then
-	# use standard ping instead
-	while read HOSTNAME MACADDR IPADDR ILOIPADDR DOMAIN ROLE; do
-	    if [[ ! "$HOSTNAME" = end ]]; then
-		echo "scanning $ROLE node $HOSTNAME($IPADDR) using ping" && ping -c 1 $IPADDR | grep ttl |cut -f4 -d" " | cut -f1 -d":"
-	    fi
-	done < cluster.txt
-	echo "Done"
+        # use standard ping instead 
+        while read HOSTNAME MACADDR IPADDR ILOIPADDR DOMAIN ROLE; do
+            if [[ ! "$HOSTNAME" = end ]]; then
+                UP=`ping -c 1 $IPADDR | grep ttl |cut -f4 -d" " | cut -f1 -d":"`
+                if [[ ! -z "$UP" ]]; then
+                    COUNT=$((COUNT + 1))
+                    echo $UP $ROLE
+                fi
+            fi
+        done < cluster.txt                     
     else
-	# we can use fping
-	while read HOSTNAME MACADDR IPADDR DOMAIN ROLE; do
-	    ALLHOSTS="$ALLHOSTS $IPADDR"
-	done < cluster.txt
-	echo "scanning all hosts using fping..."
-	UP=`fping -aq $ALLHOSTS 2> /dev/null`
-	for H in $UP; do
-	    echo $H
-	done
+        # we can use fping
+        while read HOSTNAME MACADDR IPADDR DOMAIN ROLE; do
+            ALLHOSTS="$ALLHOSTS $IPADDR"
+        done < cluster.txt
+        UP=`fping -aq $ALLHOSTS 2> /dev/null`
+        for H in $UP; do
+            COUNT=$((COUNT + 1))
+            echo $H
+        done
     fi
+    echo "$COUNT hosts up"
 else
     echo "Warning : no cluster definition (cluster.txt) found"
 fi
