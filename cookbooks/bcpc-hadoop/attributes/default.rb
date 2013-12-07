c55bd9a3b7ca8d2f312b323bb863b2ef5390e38e
Clay Baenziger <cbaenziger@bloomberg.net>
2013-12-07 00:11:15 +0000

    Necessary changes to bootstrap a cluster before all services are running

--- cookbooks/bcpc-hadoop/attributes/default.rb
+++ cookbooks/bcpc-hadoop/attributes/default.rb
@@ -12,4 +12,5 @@ default[:bcpc][:hadoop][:distribution][:version] = '-cdh4'
 default[:bcpc][:hadoop][:distribution][:key] = 'cloudera-archive-4.key'
 default[:bcpc][:repos][:cloudera] = 'http://archive.cloudera.com/cdh4/ubuntu/precise/amd64/cdh'
 default[:bcpc][:hadoop][:disks] = []
+default[:bcpc][:hadoop][:oozie][:admins] = []
 default[:bcpc][:hadoop][:min_node_count] = 3
