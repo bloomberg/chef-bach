95e65b463384687c72dccadf4ca308ffbe51d7cc
Justin Erenkrantz <justin@erenkrantz.com>
2013-12-22 15:01:00 -0500

    Simplify update script by upload any environment files.

--- vbox_update.sh
+++ vbox_update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 vagrant ssh -c "rsync -avP --exclude vbox --exclude .chef /chef-bcpc-host/ /home/vagrant/chef-bcpc/"
-vagrant ssh -c "cd chef-bcpc && knife environment from file environments/Test-Laptop-Mirror.json && knife role from file roles/*.json && knife cookbook upload -a -o cookbooks"
+vagrant ssh -c "cd chef-bcpc && knife environment from file environments/*.json && knife role from file roles/*.json && knife cookbook upload -a -o cookbooks"
