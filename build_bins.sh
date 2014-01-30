875b2a8f73d636d159024041381704f4178f0b3b
Clay Baenziger <cbaenziger@bloomberg.net>
2014-01-30 18:41:59 -0500

    Clean-up build_bins.sh to not compile Gems as we are not using pre-compiled bins in the end Clean-up cluster-assign-roles.sh to be broken up in to explicit functions

--- build_bins.sh
+++ build_bins.sh
@@ -22,6 +22,8 @@ DIR=`dirname $0`
 
 mkdir -p $DIR/bins
 pushd $DIR/bins/
+
+# create directory for Python bins
 mkdir -p python
 
 # serve the files if nothing else is doing so already
@@ -34,169 +36,145 @@ apt-get -y update
 
 # Install tools needed for packaging
 apt-get -y install git rubygems make pbuilder python-mock python-configobj python-support cdbs python-all-dev python-stdeb libmysqlclient-dev libldap2-dev ruby-dev gcc patch rake ruby1.9.3 ruby1.9.1-dev python-pip python-setuptools dpkg-dev apt-utils haveged
-if [ -z `gem list --local fpm | grep fpm | cut -f1 -d" "` ]; then
+if [[ -z `gem list --local fpm | grep fpm | cut -f1 -d" "` ]]; then
   gem install fpm --no-ri --no-rdoc
-fi
-
-RUBY19=ruby1.9.3
-RUBY18=ruby1.8
-GEM19=gem1.9.1
-GEM18=gem1.8
-RUBY19_ver=$($RUBY19 -e "print RUBY_VERSION")
-RUBY18_ver=$(ruby -e "print RUBY_VERSION")
-
-# need to use gem-compile for gems with native code
-# provide a Ruby 1.9.1 and Ruby 1.8 gem-compile capable RubyGem
-if ! ${RUBY19_ver}_gem_compile/bin/$GEM19 list --local gem-compiler | grep -q gem-compiler || \
-   ! ${RUBY18_ver}_gem_compile/bin/$GEM18 list --local gem-compiler | grep -q gem-compiler; then
-  # need a RubyGems > 1.8.25 for gem-compile (not available via apt-get)
-  $CURL -O http://production.cf.rubygems.org/rubygems/rubygems-update-1.8.29.gem
-  gem unpack rubygems-update-1.8.29.gem
-  pushd rubygems*
-  $RUBY18 setup.rb --prefix `pwd`/../${RUBY18_ver}_gem_compile
-  $RUBY19 setup.rb --prefix `pwd`/../${RUBY19_ver}_gem_compile
-  popd
-  export RUBYLIB=`pwd`/${RUBY18_ver}_gem_compile/lib
-  `pwd`/${RUBY18_ver}_gem_compile/bin/$GEM18 install gem-compile
-  export RUBYLIB=`pwd`/${RUBY19_ver}_gem_compile/lib
-  `pwd`/${RUBY19_ver}_gem_compile/bin/$GEM19 install gem-compile
-  unset RUBYLIB
 fi
 
 # Build kibana3 installable bundle
 if [ ! -f kibana3.tgz ]; then
-    git clone https://github.com/elasticsearch/kibana.git kibana3
-    cd kibana3
-    git archive --output ../kibana3.tgz --prefix kibana3/ $VER_KIBANA
-    cd ..
-    rm -rf kibana3
+  git clone https://github.com/elasticsearch/kibana.git kibana3
+  cd kibana3
+  git archive --output ../kibana3.tgz --prefix kibana3/ $VER_KIBANA
+  cd ..
+  rm -rf kibana3
 fi
 FILES="kibana3.tgz $FILES"
 
 # Grab plugins for fluentd
 for i in elasticsearch tail-multiline tail-ex record-reformer rewrite; do
-    if [[ ! -f fluent-plugin-${i}*.gem && ! -f gems/fluent-plugin-${i}*.gem ]]; then
-        gem fetch fluent-plugin-${i}
-        ln -s fluent-plugin-${i}-*.gem fluent-plugin-${i}.gem || true
-    fi
-    FILES="fluent-plugin-${i}*.gem $FILES"
+  if [[ ! -f fluent-plugin-${i}*.gem && ! -f gems/fluent-plugin-${i}*.gem ]]; then
+    gem fetch fluent-plugin-${i}
+    ln -s fluent-plugin-${i}-*.gem fluent-plugin-${i}.gem || true
+  fi
+  FILES="fluent-plugin-${i}*.gem $FILES"
 done
 
 # Get the Rubygem for zookeeper
-if [[ ! -f zookeeper*.gem && ! -f gems/zookeeper*.gem ]]; then
-    gem fetch zookeeper -v 1.4.7
-    ln -s zookeeper-*.gem zookeeper.gem || true
+if [[ ! -f zookeeper*.gem || ! -f gems/zookeeper*.gem ]]; then
+  gem fetch zookeeper -v 1.4.7
+  ln -s zookeeper-*.gem zookeeper.gem || true
 fi
 FILES="zookeeper*.gem $FILES"
 
 # Fetch the cirros image for testing
-if [ ! -f cirros-0.3.0-x86_64-disk.img ]; then
-    $CURL -O -L https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-x86_64-disk.img
+if [[ ! -f cirros-0.3.0-x86_64-disk.img ]]; then
+  $CURL -O -L https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-x86_64-disk.img
 fi
 FILES="cirros-0.3.0-x86_64-disk.img $FILES"
 
 # Grab the Ubuntu 12.04 installer image
-if [ ! -f ubuntu-12.04-mini.iso ]; then
-    # Download this ISO to get the latest kernel/X LTS stack installer
-    #$CURL -o ubuntu-12.04-mini.iso http://archive.ubuntu.com/ubuntu/dists/precise-updates/main/installer-amd64/current/images/raring-netboot/mini.iso
-    $CURL -o ubuntu-12.04-mini.iso http://archive.ubuntu.com/ubuntu/dists/precise/main/installer-amd64/current/images/netboot/mini.iso
+if [[ ! -f ubuntu-12.04-mini.iso ]]; then
+  # Download this ISO to get the latest kernel/X LTS stack installer
+  #$CURL -o ubuntu-12.04-mini.iso http://archive.ubuntu.com/ubuntu/dists/precise-updates/main/installer-amd64/current/images/raring-netboot/mini.iso
+  $CURL -o ubuntu-12.04-mini.iso http://archive.ubuntu.com/ubuntu/dists/precise/main/installer-amd64/current/images/netboot/mini.iso
 fi
 FILES="ubuntu-12.04-mini.iso $FILES"
 
 # Grab the CentOS 6 PXE boot images
-if [ ! -f centos-6-initrd.img ]; then
-    #$CURL -o centos-6-mini.iso http://mirror.net.cen.ct.gov/centos/6/isos/x86_64/CentOS-6.4-x86_64-netinstall.iso
-    $CURL -o centos-6-initrd.img http://mirror.net.cen.ct.gov/centos/6/os/x86_64/images/pxeboot/initrd.img
+if [[ ! -f centos-6-initrd.img ]]; then
+  $CURL -o centos-6-initrd.img http://mirror.net.cen.ct.gov/centos/6/os/x86_64/images/pxeboot/initrd.img
 fi
 FILES="centos-6-initrd.img $FILES"
 
-if [ ! -f centos-6-vmlinuz ]; then
-    $CURL -o centos-6-vmlinuz http://mirror.net.cen.ct.gov/centos/6/os/x86_64/images/pxeboot/vmlinuz
+if [[ ! -f centos-6-vmlinuz ]]; then
+  $CURL -o centos-6-vmlinuz http://mirror.net.cen.ct.gov/centos/6/os/x86_64/images/pxeboot/vmlinuz
 fi
 FILES="centos-6-vmlinuz $FILES"
 
 # Make the diamond package
-if [ ! -f diamond*.deb ]; then
-    git clone https://github.com/BrightcoveOS/Diamond.git
-    cd Diamond
-    git checkout $VER_DIAMOND
-    make builddeb
-    VERSION=`cat version.txt`
-    cd ..
-    mv Diamond/build/diamond_${VERSION}_all.deb diamond.deb
-    rm -rf Diamond
+if [[ ! -f diamond*.deb ]]; then
+  git clone https://github.com/BrightcoveOS/Diamond.git
+  pushd Diamond
+  git checkout $VER_DIAMOND
+  make builddeb
+  VERSION=`cat version.txt`
+  popd
+  mv Diamond/build/diamond_${VERSION}_all.deb diamond.deb
+  rm -rf Diamond
 fi
 FILES="diamond.deb $FILES"
 
 # Snag elasticsearch
-if [ ! -f elasticsearch-0.90.3.deb ]; then
-    $CURL -O -L https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.3.deb
+if [[ ! -f elasticsearch-0.90.3.deb ]]; then
+  $CURL -O -L https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.3.deb
 fi
 FILES="elasticsearch-0.90.3.deb $FILES"
 
-if [ ! -f elasticsearch-plugins.tgz ]; then
-    git clone https://github.com/mobz/elasticsearch-head.git
-    cd elasticsearch-head
-    git archive --output ../elasticsearch-plugins.tgz --prefix head/_site/ $VER_ESPLUGIN
-    cd ..
-    rm -rf elasticsearch-head
+if [[ ! -f elasticsearch-plugins.tgz ]]; then
+  git clone https://github.com/mobz/elasticsearch-head.git
+  cd elasticsearch-head
+  git archive --output ../elasticsearch-plugins.tgz --prefix head/_site/ $VER_ESPLUGIN
+  cd ..
+  rm -rf elasticsearch-head
 fi
 FILES="elasticsearch-plugins.tgz $FILES"
 
 # Fetch pyrabbit
-if [ ! -f python/pyrabbit-1.0.1.tar.gz ]; then
-    while ! $(file python/pyrabbit-1.0.1.tar.gz | grep -q 'gzip compressed data'); do
-        (cd python && $CURL -O -L http://pypi.python.org/packages/source/p/pyrabbit/pyrabbit-1.0.1.tar.gz)
-    done
+if [[ ! -f python/pyrabbit-1.0.1.tar.gz ]]; then
+  while ! $(file python/pyrabbit-1.0.1.tar.gz | grep -q 'gzip compressed data'); do
+    (cd python && $CURL -O -L http://pypi.python.org/packages/source/p/pyrabbit/pyrabbit-1.0.1.tar.gz)
+  done
 fi
 FILES="pyrabbit-1.0.1.tar.gz $FILES"
 
 # Build graphite packages
-if [ ! -f python-carbon_0.9.10_all.deb ] || [ ! -f python-whisper_0.9.10_all.deb ] || [ ! -f python-graphite-web_0.9.10_all.deb ]; then
-    while ! $(file carbon-0.9.10.tar.gz | grep -q 'gzip compressed data'); do
-        $CURL -L -O http://pypi.python.org/packages/source/c/carbon/carbon-0.9.10.tar.gz
-    done
-    while ! $(file whisper-0.9.10.tar.gz | grep -q 'gzip compressed data'); do
-        $CURL -L -O http://pypi.python.org/packages/source/w/whisper/whisper-0.9.10.tar.gz
-    done
-    while ! $(file graphite-web-0.9.10.tar.gz | grep -q 'gzip compressed data'); do
-        $CURL -L -O http://pypi.python.org/packages/source/g/graphite-web/graphite-web-0.9.10.tar.gz
-    done
-    tar zxf carbon-0.9.10.tar.gz
-    tar zxf whisper-0.9.10.tar.gz
-    tar zxf graphite-web-0.9.10.tar.gz
-    fpm --python-install-bin /opt/graphite/bin -s python -t deb carbon-0.9.10/setup.py
-    fpm --python-install-bin /opt/graphite/bin  -s python -t deb whisper-0.9.10/setup.py
-    fpm --python-install-lib /opt/graphite/webapp -s python -t deb graphite-web-0.9.10/setup.py
+if [[ ! -f python-carbon_0.9.10_all.deb || \
+      ! -f python-whisper_0.9.10_all.deb || \
+      ! -f python-graphite-web_0.9.10_all.deb ]]; then
+  while ! $(file carbon-0.9.10.tar.gz | grep -q 'gzip compressed data'); do
+    $CURL -L -O http://pypi.python.org/packages/source/c/carbon/carbon-0.9.10.tar.gz
+  done
+  while ! $(file whisper-0.9.10.tar.gz | grep -q 'gzip compressed data'); do
+    $CURL -L -O http://pypi.python.org/packages/source/w/whisper/whisper-0.9.10.tar.gz
+  done
+  while ! $(file graphite-web-0.9.10.tar.gz | grep -q 'gzip compressed data'); do
+    $CURL -L -O http://pypi.python.org/packages/source/g/graphite-web/graphite-web-0.9.10.tar.gz
+  done
+  tar zxf carbon-0.9.10.tar.gz
+  tar zxf whisper-0.9.10.tar.gz
+  tar zxf graphite-web-0.9.10.tar.gz
+  fpm --python-install-bin /opt/graphite/bin -s python -t deb carbon-0.9.10/setup.py
+  fpm --python-install-bin /opt/graphite/bin  -s python -t deb whisper-0.9.10/setup.py
+  fpm --python-install-lib /opt/graphite/webapp -s python -t deb graphite-web-0.9.10/setup.py
 fi
 FILES="python-carbon_0.9.10_all.deb python-whisper_0.9.10_all.deb python-graphite-web_0.9.10_all.deb $FILES"
 
 # Download Python requests-aws for Zabbix monitoring
-if [ ! -f python-requests-aws_0.1.5_all.deb ]; then
-    fpm -s python -t deb -v 0.1.5 requests-aws
+if [[ ! -f python-requests-aws_0.1.5_all.deb ]]; then
+  fpm -s python -t deb -v 0.1.5 requests-aws
 fi
 FILES="python-requests-aws_0.1.5_all.deb $FILES"
 
 # Build the zabbix packages
-if [ ! -f zabbix-agent.tar.gz ] || [ ! -f zabbix-server.tar.gz ]; then
-    $CURL -L -O http://sourceforge.net/projects/zabbix/files/ZABBIX%20Latest%20Stable/2.0.7/zabbix-2.0.7.tar.gz
-    tar zxf zabbix-2.0.7.tar.gz
-    rm -rf /tmp/zabbix-install && mkdir -p /tmp/zabbix-install
-    cd zabbix-2.0.7
-    ./configure --prefix=/tmp/zabbix-install --enable-agent --with-ldap
-    make install
-    tar zcf zabbix-agent.tar.gz -C /tmp/zabbix-install .
-    rm -rf /tmp/zabbix-install && mkdir -p /tmp/zabbix-install
-    ./configure --prefix=/tmp/zabbix-install --enable-server --with-mysql --with-ldap
-    make install
-    cp -a frontends/php /tmp/zabbix-install/share/zabbix/
-    cp database/mysql/* /tmp/zabbix-install/share/zabbix/
-    tar zcf zabbix-server.tar.gz -C /tmp/zabbix-install .
-    rm -rf /tmp/zabbix-install
-    cd ..
-    cp zabbix-2.0.7/zabbix-agent.tar.gz .
-    cp zabbix-2.0.7/zabbix-server.tar.gz .
-    rm -rf zabbix-2.0.7 zabbix-2.0.7.tar.gz
+if [[ ! -f zabbix-agent.tar.gz ]] || [[ ! -f zabbix-server.tar.gz ]]; then
+  $CURL -L -O http://sourceforge.net/projects/zabbix/files/ZABBIX%20Latest%20Stable/2.0.7/zabbix-2.0.7.tar.gz
+  tar zxf zabbix-2.0.7.tar.gz
+  rm -rf /tmp/zabbix-install && mkdir -p /tmp/zabbix-install
+  cd zabbix-2.0.7
+  ./configure --prefix=/tmp/zabbix-install --enable-agent --with-ldap
+  make install
+  tar zcf zabbix-agent.tar.gz -C /tmp/zabbix-install .
+  rm -rf /tmp/zabbix-install && mkdir -p /tmp/zabbix-install
+  ./configure --prefix=/tmp/zabbix-install --enable-server --with-mysql --with-ldap
+  make install
+  cp -a frontends/php /tmp/zabbix-install/share/zabbix/
+  cp database/mysql/* /tmp/zabbix-install/share/zabbix/
+  tar zcf zabbix-server.tar.gz -C /tmp/zabbix-install .
+  rm -rf /tmp/zabbix-install
+  cd ..
+  cp zabbix-2.0.7/zabbix-agent.tar.gz .
+  cp zabbix-2.0.7/zabbix-server.tar.gz .
+  rm -rf zabbix-2.0.7 zabbix-2.0.7.tar.gz
 fi
 FILES="zabbix-agent.tar.gz zabbix-server.tar.gz $FILES"
 
@@ -204,12 +182,19 @@ FILES="zabbix-agent.tar.gz zabbix-server.tar.gz $FILES"
 opscode_urls="https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef_11.8.0-1.ubuntu.12.04_amd64.deb
 https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.0.8-1.ubuntu.12.04_amd64.deb"
 for url in $opscode_urls; do
-    if [ ! -f $(basename $url) ]; then
-        $CURL -L -O $url
-    fi
+  if [ ! -f $(basename $url) ]; then
+    $CURL -L -O $url
+  fi
 done
 
-# need a key to sign repo
+###################
+# generate apt-repo
+dpkg-scanpackages . > Packages
+gzip -c Packages > Packages.gz
+apt-ftparchive release . > Release
+rm -f Release.gpg
+
+# generate a key and sign repo
 if [[ ! -f ${HOME}/apt_key.sec || ! -f apt_key.pub ]]; then
   rm -rf ${HOME}/apt_key.sec apt_key.pub
   gpg --batch --gen-key << EOF
@@ -225,14 +210,8 @@ if [[ ! -f ${HOME}/apt_key.sec || ! -f apt_key.pub ]]; then
 EOF
   chmod 700 ${HOME}/apt_key.sec
 fi
-
-###################
-# generate apt-repo
-dpkg-scanpackages . > Packages
-gzip -c Packages > Packages.gz
-apt-ftparchive release . > Release
-rm -f Release.gpg
 gpg -abs --keyring ./apt_key.pub --secret-keyring ${HOME}/apt_key.sec -o Release.gpg Release
+
 
 ####################
 # generate Pypi repo
@@ -255,37 +234,6 @@ gem install builder --no-ri --no-rdoc
 # place all gems into the server normally
 [ ! -d gems ] && mkdir gems
 [ -n "$(echo *.gem)" ] && mv *.gem gems
-
-# compile gems for specific Ruby versions
-[ ! -d ${RUBY19_ver}/gems ] && mkdir -p ${RUBY19_ver}/gems
-[ ! -d ${RUBY18_ver}/gems ] && mkdir -p ${RUBY18_ver}/gems
-
-# copy & compile gems which need compilation
-for g in zookeeper-1.4.7; do
-  [ -f "${RUBY18_ver}/gems/${g}.gem" ] && [ -f "${RUBY19_ver}/gems/${g}.gem" ] && continue
-  export RUBYLIB=`pwd`/${RUBY18_ver}_gem_compile/lib
-  pushd ${RUBY18_ver}/gems
-  cp ../../gems/${g}.gem .
-  ../../${RUBY18_ver}_gem_compile/bin/$GEM18 compile ${g}.gem
-  popd
-  export RUBYLIB=`pwd`/${RUBY19_ver}_gem_compile/lib
-  pushd ${RUBY19_ver}/gems
-  cp ../../gems/${g}.gem .
-  ../../${RUBY19_ver}_gem_compile/bin/$GEM19 compile ${g}.gem
-  popd
-  unset RUBYLIB
-done
-
 gem generate_index --legacy
-( pushd ${RUBY18_ver} &&
-  RUBYLIB=`pwd`/../${RUBY18_ver}_gem_compile/lib ../${RUBY18_ver}_gem_compile/bin/$GEM18 install builder --no-ri --no-rdoc &&
-  RUBYLIB=`pwd`/../${RUBY18_ver}_gem_compile/lib ../${RUBY18_ver}_gem_compile/bin/$GEM18 generate_index --legacy &&
-  popd
-)
-( pushd ${RUBY19_ver} &&
-  RUBYLIB=`pwd`/../${RUBY19_ver}_gem_compile/lib ../${RUBY19_ver}_gem_compile/bin/$GEM19 install builder --no-ri --no-rdoc &&
-  RUBYLIB=`pwd`/../${RUBY19_ver}_gem_compile/lib ../${RUBY19_ver}_gem_compile/bin/$GEM19 generate_index --legacy &&
-  popd
-)
 
 popd
--- cluster-assign-roles.sh
+++ cluster-assign-roles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script to assign roles to cluster nodes based on a definition in cluster.txt :
+# Script to assign roles to cluster nodes based on a definition in cluster.txt:
 #
 # - The environment is needed to find the root password of the machines
 #   and to provide the environment in which to chef them
@@ -11,18 +11,160 @@
 # - if a nodename is provided, either by hostname or ip address, only
 #   that node will be attempted
 #
-# - if the special nodename "heads" is given, all head nodes will be
-#   attempted
+# - if a chef object is provided, e.g. role[ROLE-NAME] or
+#   recipe[RECIPE-NAME], only nodes marked for that action are attempted
 #
-# - if the special nodename "workers" is given, all work nodes will be
-#   attempted
-#
-# - A node may be excluded by setting its role to something other than
-#   "head" or "work" in cluster.txt. For example "done" might be
-#   useful for nodes that have been completed
+# - A node may be excluded by setting its action to SKIP
 
 set -e
 set -x
+
+########################################################################
+# install_machines -  Install a set of machines (will run chefit.sh if no node object for machine)
+# Argument: $1 - a string of role!IP!FQDN pairs separated by white space
+# Will install the machine with role $role
+function install_machines {
+  passwd=`knife data bag show configs $ENVIRONMENT | grep "cobbler-root-password:" | awk ' {print $2}'`
+  for h in $(sort <<< ${*// /\\n}); do
+    local regEx='(.*)!(.*)!.*'
+    [[ "$h" =~ $regEx ]]
+    local run_list="${BASH_REMATCH[1]}"
+    local ip="${BASH_REMATCH[2]}"
+    printf "About to bootstrap node ${run_list}...\n"
+    knife node show $fqdn 2>/dev/null >/dev/null || ./chefit.sh $ip $ENVIRONMENT
+    local SSHCMD="./nodessh.sh $ENVIRONMENT $ip"
+    sudo knife bootstrap -E $ENVIRONMENT -r "$run_list" $ip -x ubuntu  -P $passwd -u admin -k /etc/chef-server/admin.pem --sudo <<< $passwd
+  done
+}
+
+#############################################################################################
+# parse cluster.txt 
+# Argument: $1 - optional case insensitive match text (e.g. hostname, ipaddress, chef object)
+# Returns: List of matching hosts (or all non-skipped hosts) one host per line with ! delimited fileds
+# (Note: if you want to skip a machine, set its role to SKIP in cluster.txt)
+function parse_cluster_txt {
+  local match_text=$1
+  local hosts=""
+  while read host macaddr ipaddr iloipaddr domain role; do
+    shopt -s nocasematch
+    if [[ -z "$match_text" || "$match_text" = "$host" || "$match_text" = "$ipaddr" || "$match_text" = "$role" ]] && \
+       [[ ! "|$role" =~ '|SKIP' ]]; then
+      hosts="$hosts ${role}!${ipaddr}!${host}.$domain"
+    fi
+    shopt -u nocasematch
+  done < cluster.txt
+  printf "$hosts"
+}
+
+##########################################
+# Install Machine Stub
+# Function to create basic machine representation (in parallel) if Chef does not
+# already know about machine
+# Runs: Chef role[Basic], Chef recipe[bcpc::default], recipe[bcpc::networking]
+# Argument: $* - hosts are returned by parse_cluster_txt
+function install_stub {
+  printf "Creating stubs for nodes...\n"
+  for h in $*; do
+    [[ "$h" =~ $REGEX ]]
+    local role="${BASH_REMATCH[1]}"
+    local ip="${BASH_REMATCH[2]}"
+    local fqdn="${BASH_REMATCH[3]}"
+    knife node show $fqdn 2>/dev/null >/dev/null ||  install_machines "role[Basic],recipe[bcpc::default],recipe[bcpc::networking]!$ip!NONE" &
+  done
+  wait
+}
+
+########################################################################
+# Perform OpenStack install
+# Arguments: $* - hosts (as output from parse_cluster_txt)
+# Runs the end-to-end install in the proper order for OpenStack installs
+# Install method:
+# First, install first headnode -- which needs to be an admin
+# Next, install head-nodes start to finish and back to start synchronously
+# (this ensures all headnodes know of eachother)
+# Lastly, install workers in parallel)
+function openstack_install {
+  local hosts="$*"
+  local err_trap="$(trap ERR)"
+  trap '$err_trap; echo -e "####\n#### Failed installing $host\n####"' ERR
+  shopt -u nocasematch
+  printf "Doing OpenStack style install...\n"
+
+  [[ "$(printf ${hosts// /\\n} | grep -i "head" | sort | head -1)" =~ $REGEX ]] && first_head_node="${BASH_REMATCH[3]}" || \
+    (printf "Failed to parse hosts\n"; exit 1 )
+  install_stub $first_head_node
+  # set the first node to admin for creating data bags (short-circuit failures in-case machine already is an admin)
+  printf "/\"admin\": false\ns/false/true\nw\nq\n" | EDITOR=ed knife client edit $first_head_node || /bin/true
+
+  # Do head nodes first and group by type of head
+  printf "Installing heads...\n"
+  install_machines $(printf ${hosts// /\\n} | grep -i "head" | sort)
+  # Redo head nodes in reverse order to ensure they know about eachother
+  # (the last node already knows everyone in the universe of heads)
+  printf "Acquainting heads...\n"
+  install_machines $(printf ${hosts// /\\n} | grep -i "head" | sort | head -n -1 | tac)
+  # Do everything else next and group by type of node
+  printf "Installing workers...\n"
+  for m in $(printf ${hosts// /\\n} | grep -vi "head" | sort); do
+    ( install_machines $m || exit 1 )&
+  done
+  wait
+  trap "$err_trap" ERR
+}
+
+########################################################################
+# Perform Hadoop install
+# Arguments: $* - hosts (as output from parse_cluster_txt)
+# Method:
+# * Installs stubs (create chef nodes and setup networking) for all machines in parallel
+# * Set all headnode to admins
+# * Assigns roles for headnodes
+# * Installs headnodes sorted by role
+# * Unsets all headnode from being admins
+# * Installs worknodes in parallel
+function hadoop_install {
+  local hosts="$*"
+  shopt -u nocasematch
+  local regEx='(.*)!(.*)!(.*)'
+  printf "Doing Hadoop style install...\n"
+  # to prevent needing to re-chef headnodes the Hadoop code base assumes
+  # all nodes and clients have been created and further that all roles
+  # have been assigned before any node Chefing begins
+  install_stub $(printf ${hosts// /\\n} | sort)
+
+  printf "Assigning roles for headnodes...\n"
+  for h in $(printf ${hosts// /\\n} | grep -i "head" | sort); do
+    [[ "$h" =~ $regEx ]]
+    local role="${BASH_REMATCH[1]}"
+    local ip="${BASH_REMATCH[2]}"
+    local fqdn="${BASH_REMATCH[3]}"
+    knife node run_list add $fqdn "$role" &
+  done
+
+  # set the headnodes to admin for creating data bags
+  for h in $(printf ${hosts// /\\n} | grep -i "head" | sort); do
+    [[ "$h" =~ $regEx ]]
+    printf "/\"admin\": false\ns/false/true\nw\nq\n" | EDITOR=ed knife client edit "${BASH_REMATCH[3]}" || /bin/true
+  done
+
+  printf "Installing heads...\n"
+  install_machines $(printf ${hosts// /\\n} | grep -i "head" | sort)
+
+  # remove admin from the headnodes
+  for h in $(printf ${hosts// /\\n} | grep -i "head" | sort); do
+    [[ "$h" =~ $regEx ]]
+    printf "/\"admin\": true\ns/true/false\nw\nq\n" | EDITOR=ed knife client edit "${BASH_REMATCH[3]}"
+  done
+
+  printf "Installing workers...\n"
+  for m in $(printf ${hosts// /\\n} | grep -vi "head" | sort); do
+    install_machines $m &
+  done
+}
+
+############
+# Main Below
+#
 
 if [[ -z "$2" ]]; then
   printf "Usage : $0 environment install_type (hostname)\n" > /dev/stderr
@@ -31,7 +173,7 @@ fi
 
 ENVIRONMENT=$1
 INSTALL_TYPE=$2
-EXACTHOST=$3
+MATCHKEY=$3
 
 shopt -s nocasematch
 if [[ ! "$INSTALL_TYPE" =~ (openstack|hadoop) ]]; then
@@ -45,33 +187,8 @@ if [[ ! -f "environments/$ENVIRONMENT.json" ]]; then
   exit 1
 fi
 
-########################################################################
-# install_machines -  Install a set of machines
-# Argument: $1 - a string of role!IP!FQDN pairs separated by white space
-# Will install the machine with role $role
-function install_machines {
-  PASSWD=`knife data bag show configs $ENVIRONMENT | grep "cobbler-root-password:" | awk ' {print $2}'`
-  for h in $(sort <<< ${*// /\\n}); do
-    local regEx='(.*)!(.*)!.*'
-    [[ "$h" =~ $regEx ]]
-    local run_list="${BASH_REMATCH[1]}"
-    local ip="${BASH_REMATCH[2]}"
-    printf "About to bootstrap node ${run_list}...\n"
-    ./chefit.sh $ip $ENVIRONMENT
-    local SSHCMD="./nodessh.sh $ENVIRONMENT $ip"
-    sudo knife bootstrap -E $ENVIRONMENT -r "$run_list" $ip -x ubuntu  -P $PASSWD -u admin -k /etc/chef-server/admin.pem --sudo <<< $PASSWD
-  done
-}
-
-# if you want to skip a machine, set its role to SKIP
-while read HOST MACADDR IPADDR ILOIPADDR DOMAIN ROLE; do
-  shopt -s nocasematch
-  if [[ -z "$EXACTHOST" || "$EXACTHOST" = "$HOST" || "$EXACTHOST" = "$IPADDR" || "$EXACTHOST" = "$ROLE" ]] && [[ ! "|$ROLE" =~ '|SKIP' ]]; then
-    hosts="$hosts ${ROLE}!${IPADDR}!${HOST}.$DOMAIN"
-  fi
-  shopt -u nocasematch
-done < cluster.txt
-
+# Report which hosts were found
+hosts="$(parse_cluster_txt $MATCHKEY)"
 for h in $hosts; do
   regEx='(.*)!(.*)!(.*)'
   [[ "$h" =~ $regEx ]]
@@ -88,57 +205,10 @@ fi
 
 shopt -s nocasematch
 if [[ "$INSTALL_TYPE" = "OpenStack" ]]; then
-  shopt -u nocasematch
-  printf "Doing OpenStack style install...\n"
-  # Do head nodes first and group by type of head
-  printf "Installing heads...\n"
-  install_machines $(printf ${hosts// /\\n} | grep -i "head" | sort)
-  # Redo head nodes in reverse order to ensure they know about eachother
-  # (the last node already knows everyone in the universe of heads)
-  printf "Acquainting heads...\n"
-  install_machines $(printf ${hosts// /\\n} | grep -i "head" | sort | head -n -1 | tac)
-  # Do everything else next and group by type of node
-  printf "Installing workers...\n"
-  install_machines $(printf ${hosts// /\\n} | grep -vi "head" | sort)
-
+  openstack_install $hosts
 ### Hadoop Install Method
 elif [[ "$INSTALL_TYPE" = "Hadoop" ]]; then
-  shopt -u nocasematch
-  regEx='(.*)!(.*)!(.*)'
-  printf "Doing Hadoop style install...\n"
-  # to prevent needing to re-chef headnodes the Hadoop code base assumes
-  # all nodes and clients have been created and further that all roles
-  # have been assigned before any node Chefing begins
-  printf "Creating stubs for nodes...\n"
-  for h in $(printf ${hosts// /\\n} | sort); do
-    [[ "$h" =~ $regEx ]]
-    role="${BASH_REMATCH[1]}"
-    ip="${BASH_REMATCH[2]}"
-    fqdn="${BASH_REMATCH[3]}"
-    knife node show $fqdn 2>/dev/null >/dev/null ||  install_machines "role[Basic],recipe[bcpc::default],recipe[bcpc::networking]!$ip!NONE" &
-  done
-  wait
-
-  printf "Assigning roles for headnodes...\n"
-  for h in $(printf ${hosts// /\\n} | grep -i "head" | sort); do
-    [[ "$h" =~ $regEx ]]
-    role="${BASH_REMATCH[1]}"
-    ip="${BASH_REMATCH[2]}"
-    fqdn="${BASH_REMATCH[3]}"
-    knife node run_list add $fqdn "role[$role]" &
-  done
-
-  # set the first node to admin for creating data bags
-  [[ "$(printf ${hosts// /\\n} | grep -i "head" | sort | head -1)" =~ $regEx ]] && \
-  printf "/\"admin\": false\ns/false/true\nw\nq\n" | EDITOR=ed knife client edit "${BASH_REMATCH[3]}" || /bin/true
-
-  printf "Installing heads...\n"
-  install_machines $(printf ${hosts// /\\n} | grep -i "head" | sort)
-
-  # unset the first node being an admin to lessen security footprint
-  [[ "$(printf ${hosts// /\\n} | grep -i "head" | sort | head -1)" =~ $regEx ]] && \
-  printf "/\"admin\": true\ns/true/false\nw\nq\n" | EDITOR=ed knife client edit "${BASH_REMATCH[3]}"
-
-  printf "Installing workers...\n"
-  install_machines $(printf ${hosts// /\\n} | grep -vi "head" | sort)
+  hadoop_install $hosts
 fi
+
+printf "#### Install finished\n"
