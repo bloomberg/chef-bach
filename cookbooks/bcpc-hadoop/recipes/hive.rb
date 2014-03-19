f27559e88ef86a0e7d65773155408f76693c5279
Vivek A. Ganesan <onevivek@yahoo.com>
2014-03-19 23:17:54 +0000

    updated recipes for hive

--- cookbooks/bcpc-hadoop/recipes/hive.rb
+++ cookbooks/bcpc-hadoop/recipes/hive.rb
@@ -1,9 +1,26 @@
 
-%w{hive-server2 hive-hbase}.each do |pkg|
+%w{hive hive-hbase}.each do |pkg|
   package pkg do
     action :upgrade
   end
 end
+
+template "hive-config" do
+  path "/usr/lib/hive/bin/hive-config.sh"
+  source "hv_hive-config.sh.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+
+bash "hiveserver2" do
+  code "nohup /usr/lib/hive/bin/hiveserver2 -hiveconf hive.metastore.uris=\" \" > /var/log/hiveServer2.out 2>/var/log/hive/hiveServer2.log &"
+  user "hive"
+  action :run
+  not_if { true }
+end
+
 
 service "hive-server2" do
   action [:enable, :start]
