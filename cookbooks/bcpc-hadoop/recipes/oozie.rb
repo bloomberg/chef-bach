408257e4195eea57cff3b5b043d1e754a1497c4f
Vivek A. Ganesan <onevivek@yahoo.com>
2014-03-20 08:34:09 +0000

    Changes for Oozie

--- cookbooks/bcpc-hadoop/recipes/oozie.rb
+++ cookbooks/bcpc-hadoop/recipes/oozie.rb
@@ -4,20 +4,58 @@ dpkg_autostart "oozie" do
   allow false
 end
 
-%w{libmysql-java oozie oozie-client}.each do |pkg|
+%w{libmysql-java zip unzip extjs hadoop-lzo oozie oozie-client}.each do |pkg|
   package pkg do
     action :upgrade
   end
 end
 
-link "/var/lib/oozie/mysql.jar" do
-  to "/usr/share/java/mysql.jar"
+OOZIE_LIB_PATH='/usr/lib/oozie'
+OOZIE_SERVER_PATH='/var/lib/oozie/oozie-server'
+
+directory "#{OOZIE_LIB_PATH}/libext" do
+  owner "root"
+  group "root"
+  mode 00755
+  action :create
+  recursive true
 end
 
+link "#{OOZIE_LIB_PATH}/libext/ext-2.2.zip" do
+  to "/usr/share/HDP-oozie/ext-2.2.zip"
+end
+
+link "#{OOZIE_LIB_PATH}/libext/mysql-connector-java.jar" do
+  to "/usr/share/java/mysql-connector-java.jar"
+end
+
+bash "oozie_setup_war" do
+  code "#{OOZIE_LIB_PATH}/bin/oozie-setup.sh prepare-war"
+  user "root"
+  action :run
+  not_if do FileTest.file?("#{OOZIE_SERVER_PATH}/webapps/oozie.war") end
+end
+
+directory "#{OOZIE_SERVER_PATH}/work/Catalina/localhost" do
+  owner "oozie"
+  group "oozie"
+  recursive true
+end
+
+directory "/var/log/oozie" do
+  owner "oozie"
+  group "oozie"
+  recursive true
+end
+
+file "#{OOZIE_LIB_PATH}/oozie.sql" do
+  owner "oozie"
+  group "oozie"
+end
 
 ruby_block "oozie-database-creation" do
   cmd = "mysql -uroot -p#{get_config('mysql-root-password')} -e"
-  privs = "SELECT,INSERT,UPDATE,DELETE,LOCK TABLES,EXECUTE" # todo node[:bcpc][:hadoop][:hive_db_privs].join(",")
+  privs = "CREATE,INDEX,SELECT,INSERT,UPDATE,DELETE,LOCK TABLES,EXECUTE" # todo node[:bcpc][:hadoop][:hive_db_privs].join(",")
   block do
 
     if not system " #{cmd} 'SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = \"oozie\"' | grep oozie" then
@@ -31,11 +69,11 @@ ruby_block "oozie-database-creation" do
       IO.popen("mysql -uroot -p#{get_config('mysql-root-password')}", "r+") do |db|
         db.write code
       end
-      system "sudo -u oozie /usr/lib/oozie/bin/ooziedb.sh create -sqlfile /tmp/oozie-create.sql"
-      IO.popen("mysql -uroot -p#{get_config('mysql-root-password')}", "r+") do |db|
-        db.write "USE oozie; SOURCE /tmp/oozie-create.sql"
-      end
-      self.notifies :enable, "service[oozie]", :immediately
+      system "sudo -u oozie #{OOZIE_LIB_PATH}/bin/ooziedb.sh create -sqlfile #{OOZIE_LIB_PATH}/oozie.sql -run Validate DB Connection"
+#      IO.popen("mysql -uroot -p#{get_config('mysql-root-password')}", "r+") do |db|
+#        db.write "USE oozie; SOURCE /tmp/oozie-create.sql"
+#      end
+#      self.notifies :enable, "service[oozie]", :immediately
       self.resolve_notification_references
     end
   end
@@ -73,7 +111,8 @@ end
 #TODO, this probably has dependencies on external services such as yarn and hive as well
 #hopefully it starts up later :)
 service "oozie" do
-  action [:enable, :start]
+#  action [:enable, :start]
+  action :nothing
   subscribes :restart, "template[/etc/oozie/conf/oozie-site.xml]", :delayed
   subscribes :restart, "template[/etc/oozie/conf/oozie-env.sh]", :delayed
 end
