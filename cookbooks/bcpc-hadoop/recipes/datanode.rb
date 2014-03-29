2c683ce766d4aad6a1c79010e602736dd1b251b6
Clay Baenziger <cbaenziger@bloomberg.net>
2014-03-28 20:51:44 -0400

    Remove duplicated hbase-regionserver work from datanode recipe Do not recurse for YARN and Datanode directories -- as if disks are not present I believe an error is correct Compactify-YARN directory creation Clarify Hive bits from YARN and Datanode bits in datanode recipe

--- cookbooks/bcpc-hadoop/recipes/datanode.rb
+++ cookbooks/bcpc-hadoop/recipes/datanode.rb
@@ -1,51 +1,16 @@
-
 %w{hadoop-yarn-nodemanager
    hadoop-hdfs-datanode
    hadoop-mapreduce
-   hbase-regionserver
    hadoop-client
    lzop
-   hadoop-lzo
-   hive}.each do |pkg|
+   hadoop-lzo}.each do |pkg|
   package pkg do
     action :upgrade
   end
 end
 
-# Build nodes for HDFS storage
-node[:bcpc][:hadoop][:mounts].each do |i|
-  directory "/disk/#{i}/dfs/dn" do
-    owner "hdfs"
-    group "hdfs"
-    mode 0700
-    action :create
-    recursive true
-  end
-end
-
-# Build nodes for Yarn log storage
-node[:bcpc][:hadoop][:mounts].each do |i|
-  directory "/disk/#{i}/yarn/local" do
-    owner "yarn"
-    group "yarn"
-    mode 0700
-    action :create
-    recursive true
-  end
-  directory "/disk/#{i}/yarn/logs" do
-    owner "yarn"
-    group "yarn"
-    mode 0700
-    action :create
-    recursive true
-  end
-end
-
-if node[:bcpc][:hadoop][:mounts].length <= node[:bcpc][:hadoop][:hdfs][:failed_volumes_tolerated]
-  Chef::Application.fatal!('You have fewer node[:bcpc][:hadoop][:disks] than node[:bcpc][:hadoop][:hdfs][:failed_volumes_tolerated]! See comments of HDFS-4442.')
-end
-
-%w{libmysql-java}.each do |pkg|
+# Install Hive Bits
+%w{hive libmysql-java}.each do |pkg|
   package pkg do
     action :upgrade
   end
@@ -55,20 +20,50 @@ link "/usr/lib/hive/lib/mysql.jar" do
   to "/usr/share/java/mysql.jar"
 end
 
+# Setup datanode and nodemanager bits
+if node[:bcpc][:hadoop][:mounts].length <= node[:bcpc][:hadoop][:hdfs][:failed_volumes_tolerated]
+  Chef::Application.fatal!("You have fewer #{node[:bcpc][:hadoop][:disks]} than #{node[:bcpc][:hadoop][:hdfs][:failed_volumes_tolerated]}! See comments of HDFS-4442.")
+end
+
+# Build nodes for HDFS storage
+node[:bcpc][:hadoop][:mounts].each do |i|
+  directory "/disk/#{i}/dfs" do
+    owner "hdfs"
+    group "hdfs"
+    mode 0700
+    action :create
+  end
+  directory "/disk/#{i}/dfs/dn" do
+    owner "hdfs"
+    group "hdfs"
+    mode 0700
+    action :create
+  end
+end
+
+# Build nodes for Yarn log storage
+node[:bcpc][:hadoop][:mounts].each do |i|
+  directory "/disk/#{i}/yarn/" do
+    owner "yarn"
+    group "yarn"
+    mode 0700
+    action :create
+  end
+  %w{local logs}.each do |d|
+    directory "/disk/#{i}/yarn/#{d}" do
+      owner "yarn"
+      group "yarn"
+      mode 0700
+      action :create
+    end
+  end
+end
+
 %w{hadoop-yarn-nodemanager hadoop-hdfs-datanode}.each do |svc|
   service svc do
+    supports :status => true, :restart => true, :reload => false
     action [:enable, :start]
     subscribes :restart, "template[/etc/hadoop/conf/hdfs-site.xml]", :delayed
     subscribes :restart, "template[/etc/hadoop/conf/yarn-site.xml]", :delayed
   end
 end
-
-%w{hbase-regionserver}.each do |svc|
-  service svc do
-    action [:enable, :start]
-    subscribes :restart, "template[/etc/hadoop/conf/hdfs-site.xml]", :delayed
-    subscribes :restart, "template[/etc/hadoop/conf/yarn-site.xml]", :delayed
-    subscribes :restart, "template[/etc/hadoop/conf/hbase-site.xml]", :delayed
-  end
-end
-
