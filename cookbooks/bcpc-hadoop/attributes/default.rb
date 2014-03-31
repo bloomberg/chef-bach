165556aa64b91af1036c230b53423400bd049099
Clay Baenziger <cbaenziger@bloomberg.net>
2014-03-31 11:50:48 -0400

    Add default HDP-Utils repo

--- cookbooks/bcpc-hadoop/attributes/default.rb
+++ cookbooks/bcpc-hadoop/attributes/default.rb
@@ -10,6 +10,7 @@ default["bcpc"]["namenode"]["id"] = -1
 default["bcpc"]["hadoop"]["distribution"]["version"] = 'HDP' 
 default["bcpc"]["hadoop"]["distribution"]["key"] = 'hortonworks.key' 
 default["bcpc"]["repos"]["hortonworks"] = 'http://public-repo-1.hortonworks.com/HDP/ubuntu12/2.x' 
+default["bcpc"]["repos"]["hdp_utils"] = 'http://public-repo-1.hortonworks.com/HDP-UTILS-1.1.0.16/repos/ubuntu12'
 default["bcpc"]["hadoop"]["disks"] = []
 default["bcpc"]["hadoop"]["oozie"]["admins"] = []
 default["bcpc"]["hadoop"]["hdfs"]["failed_volumes_tolerated"] = 1
