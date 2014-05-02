3025394584fd0111f7cec04e12d1cb2ec750b628
Clay Baenziger <cbaenziger@bloomberg.net>
2014-05-02 18:44:24 -0400

    Add Snappy support for HBase

--- cookbooks/bcpc-hadoop/recipes/region_server.rb
+++ cookbooks/bcpc-hadoop/recipes/region_server.rb
@@ -1,5 +1,14 @@
-package "hbase-regionserver" do
+package "hbase-regionserver libsnappy1" do
   action :install
+end
+
+directory "/usr/lib/hbase/lib/native/Linux-amd64-64/" do
+  recursive true
+  action :create
+end
+
+link "/usr/lib/hbase/lib/native/Linux-amd64-64/libsnappy.so.1" do
+  to "/usr/lib/libsnappy.so.1"
 end
 
 service "hbase-regionserver" do
