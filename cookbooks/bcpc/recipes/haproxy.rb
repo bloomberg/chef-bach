76dc74ee21eaa8f2ccce57607fafa0d3f85ed283
Clay Baenziger <cbaenziger@bloomberg.net>
2014-05-07 19:03:01 +0000

    Allow HAProxy to be self-tuning for the parts of the stack installed

--- cookbooks/bcpc/recipes/haproxy.rb
+++ cookbooks/bcpc/recipes/haproxy.rb
@@ -36,9 +36,18 @@ bash "enable-defaults-haproxy" do
 end
 
 template "/etc/haproxy/haproxy.cfg" do
-    source "haproxy.cfg.erb"
+    source node[:bcpc][:haproxy][:config]
     mode 00644
-	variables( :servers => get_head_nodes, :all_servers => get_all_nodes )
+	variables( :nova_servers => get_nodes_for("nova-work","bcpc"),
+                   :mysql_servers => get_nodes_for("mysql","bcpc"),
+                   :rabbitmq_servers => get_nodes_for("rabbitmq","bcpc"),
+                   :ldap_servers => get_nodes_for("389ds","bcpc"),
+                   :keystone_servers => get_nodes_for("keystone","bcpc"),
+                   :glance_servers => get_nodes_for("glance","bcpc"),
+                   :cinder_servers => get_nodes_for("cinder","bcpc"),
+                   :horizon_servers => get_nodes_for("horizon","bcpc"),
+                   :elasticsearch_servers => get_nodes_for("elasticsearch","bcpc"),
+                   :radosgw_servers => get_nodes_for("ceph-rgw","bcpc"))
 	notifies :restart, "service[haproxy]", :immediately
 end
 
