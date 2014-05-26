68add8e49c0f9c8351fc2d7adc793e211c202ead
Clay Baenziger <cbaenziger@bloomberg.net>
2014-05-26 18:21:25 -0400

    One more snappy fix (for HBase master)

--- cookbooks/bcpc-hadoop/recipes/hbase_master.rb
+++ cookbooks/bcpc-hadoop/recipes/hbase_master.rb
@@ -14,6 +14,15 @@ bash "create-hbase-dir" do
   not_if "sudo -u hdfs hadoop fs -test -d /hbase"
 end
 
+directory "/usr/lib/hbase/lib/native/Linux-amd64-64/" do
+  recursive true
+  action :create
+end
+
+link "/usr/lib/hbase/lib/native/Linux-amd64-64/libsnappy.so.1" do
+  to "/usr/lib/libsnappy.so.1"
+end
+
 service "hbase-master" do
   action [:enable, :start]
 end
