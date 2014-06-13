c4abb3d8aae99a3483f2094878b5018e4e38213f
Clay Baenziger <cbaenziger@bloomberg.net>
2014-06-13 12:03:53 -0400

    Break-out PHPMyAdmin recipe for machines not using Apache2 recipe

--- cookbooks/bcpc/recipes/mysql.rb
+++ cookbooks/bcpc/recipes/mysql.rb
@@ -150,37 +150,3 @@ service "xinetd" do
   supports :status => true, :restart => true, :reload => true
   action [ :enable, :start ]
 end
-
-package "debconf-utils"
-
-bash "phpmyadmin-debconf-setup" do
-  make_config('mysql-phpmyadmin-password', secure_password)
-  code <<-EOH
-    set -e
-    debconf-set-selections <<< 'phpmyadmin phpmyadmin/dbconfig-install boolean true'
-    debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/admin-pass password #{get_config('mysql-root-password')}'
-    debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password #{get_config('mysql-phpmyadmin-password')}' 
-    debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' 
-  EOH
-  not_if "debconf-get-selections | grep phpmyadmin >/dev/null 2>&1"
-end
-
-package "phpmyadmin" do
-  action :upgrade
-end
-
-bash "phpmyadmin-config-setup" do
-  user "root"
-  code <<-EOH
-    echo '$cfg["AllowArbitraryServer"] = TRUE;' >> /etc/phpmyadmin/config.inc.php
-  EOH
-  not_if "grep -q AllowArbitraryServer /etc/phpmyadmin/config.inc.php"
-end
-
-template "/etc/apache2/vhost-ssl-root.d/phpmyadmin.conf" do
-    source "apache-vhost-ssl-root-phpmyadmin.conf.erb"
-    owner "root"
-    group "root"
-    mode 00644
-    notifies :restart, "service[apache2]", :delayed
-end
