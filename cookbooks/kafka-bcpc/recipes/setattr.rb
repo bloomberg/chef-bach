781dbd303969d015deb91663853e4fad059201b0
Biju Nair <bnair10@bloomberg.net>
2014-10-08 09:11:27 -0400

    Changes to allow Kafka set-up to be able to use existing Hadoop ZooKeeper quorum

--- cookbooks/kafka-bcpc/recipes/setattr.rb
+++ cookbooks/kafka-bcpc/recipes/setattr.rb
@@ -7,18 +7,20 @@ node.override['java']['jdk_version'] = '7'
 node.override['java']['jdk']['7']['x86_64']['url'] = get_binary_server_url + "jdk-7u51-linux-x64.tar.gz"
 node.override['java']['jdk']['7']['i586']['url'] = get_binary_server_url + "jdk-7u51-linux-i586.tar.gz"
 
-# Get Kafka Zookeeper servers
-zk_hosts = get_zk_nodes
-
+# Get Kafka ZooKeeper servers
+# Override ZooKeeper related node attribute if Kafka specific ZooKeeper quorum is used
+if node[:use_hadoop_zookeeper_quorum]
+  zk_hosts = node[:bcpc][:hadoop][:zookeeper][:servers]
+else
+  zk_hosts = get_req_node_attributes(get_zk_nodes,HOSTNAME_NODENO_ATTR_SRCH_KEYS)
+  node.override[:bcpc][:hadoop][:zookeeper][:servers] = zk_hosts
+end
 # Override Kafka related node attributes
-node.override[:kafka][:zookeeper][:connect] = zk_hosts.map{|x| float_host(x[:hostname])}
+node.override[:kafka][:zookeeper][:connect] = zk_hosts.map{|x| float_host(x['hostname'])}
 node.override[:kafka][:base_url] = get_binary_server_url + "kafka"
 node.override[:kafka][:host_name] = float_host(node[:fqdn])
 node.override[:kafka][:advertised_host_name] = float_host(node[:fqdn])
-node.override[:kafka][:advertised_port] = 9092
+node.override[:kafka][:advertised_port] = 6667
 node.override[:kafka][:jmx_port] = node[:bcpc][:hadoop][:kafka][:jmx][:port]
 node.override[:kafka][:automatic_start] = true
 node.override[:kafka][:automatic_restart] = true
-
-# Override Zookeeper related node attributes
-node.override[:bcpc][:hadoop][:zookeeper][:servers] = get_req_node_attributes(get_zk_nodes,HOSTNAME_NODENO_ATTR_SRCH_KEYS)
