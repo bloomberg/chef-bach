7e0911713c3454b23dab0991bb4421811ebe8222
Biju Nair <bnair10@bloomberg.net>
2014-10-08 09:12:25 -0400

    Changes to remove instance variable from library function znode_exists? and also to handle any ZK connection failure gracefully

--- cookbooks/bcpc-hadoop/libraries/utils.rb
+++ cookbooks/bcpc-hadoop/libraries/utils.rb
@@ -172,18 +172,20 @@ def znode_exists?(znode_path, zk_host="localhost:2181")
   require 'zookeeper'
   znode_found = false
   begin
-    @zk = Zookeeper.new(zk_host)
-    if !@zk.connected?
-      raise "znode_exists : Unable to connect to zookeeper"
+    zk = Zookeeper.new(zk_host)
+    if !zk.connected?
+      raise "znode_exists : Unable to connect to zookeeper quorum #{zk_host}"
     end 
-    r = @zk.get(:path => znode_path)
+    r = zk.get(:path => znode_path)
     if r[:rc] == 0
       znode_found = true
     end 
   rescue Exception => e
     puts e.message
   ensure
-    @zk.close unless @zk.closed?
+    if !zk.nil?
+      zk.close unless zk.closed?
+    end
   end
   return znode_found
 end
