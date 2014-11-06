006d765691a1b0f2729d157f463bbca804609e25
Biju Nair <bnair10@bloomberg.net>
2014-11-06 08:39:05 -0500

    Changes to move non HDFS datanode resources out of datanode recipe

--- cookbooks/bcpc-hadoop/recipes/datanode.rb
+++ cookbooks/bcpc-hadoop/recipes/datanode.rb
@@ -1,40 +1,10 @@
 include_recipe 'bcpc-hadoop::hadoop_config'
 
 %w{hadoop-hdfs-datanode
-   hadoop-client
-   sqoop
-   lzop
-   hadoop-lzo}.each do |pkg|
+   hadoop-client}.each do |pkg|
   package pkg do
     action :upgrade
   end
-end
-
-# Install Sqoop Bits
-template "/etc/sqoop/conf/sqoop-env.sh" do
-  source "sq_sqoop-env.sh.erb"
-  mode "0444"
-  action :create
-end
-
-# Install Hive Bits
-# workaround for hcatalog dpkg not creating the hcat user it requires
-user "hcat" do 
-  username "hcat"
-  system true
-  shell "/bin/bash"
-  home "/usr/lib/hcatalog"
-  supports :manage_home => false
-end
-
-%w{hive hcatalog libmysql-java}.each do |pkg|
-  package pkg do
-    action :upgrade
-  end
-end
-
-link "/usr/lib/hive/lib/mysql.jar" do
-  to "/usr/share/java/mysql.jar"
 end
 
 # Setup HDFS datanode bits
--- cookbooks/bcpc-hadoop/recipes/yarn_nodemanager.rb
+++ cookbooks/bcpc-hadoop/recipes/yarn_nodemanager.rb
@@ -1,9 +1,39 @@
 
 %w{hadoop-yarn-nodemanager
-  hadoop-mapreduce}.each do |pkg|
+  hadoop-mapreduce
+  sqoop
+  lzop
+  hadoop-lzo}.each do |pkg|
   package pkg do
     action :upgrade
   end
+end
+
+# Install Sqoop Bits
+template "/etc/sqoop/conf/sqoop-env.sh" do
+  source "sq_sqoop-env.sh.erb"
+  mode "0444"
+  action :create
+end
+
+# Install Hive Bits
+# workaround for hcatalog dpkg not creating the hcat user it requires
+user "hcat" do
+  username "hcat"
+  system true
+  shell "/bin/bash"
+  home "/usr/lib/hcatalog"
+  supports :manage_home => false
+end
+
+%w{hive hcatalog libmysql-java}.each do |pkg|
+  package pkg do
+    action :upgrade
+  end
+end
+
+link "/usr/lib/hive/lib/mysql.jar" do
+  to "/usr/share/java/mysql.jar"
 end
 
 # Install YARN Bits
