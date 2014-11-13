9f510cdb1fdada6cbbf6776f5e20eb662fa302b3
Biju Nair <bnair10@bloomberg.net>
2014-11-13 14:02:21 -0500

    Changes to move attributes from recipes and new attributes to control Kafka restart

--- cookbooks/kafka-bcpc/recipes/setattr.rb
+++ cookbooks/kafka-bcpc/recipes/setattr.rb
@@ -10,14 +10,10 @@ node.override['java']['jdk']['7']['i586']['url'] = get_binary_server_url + "jdk-
 # Get Kafka ZooKeeper servers
 # Override ZooKeeper related node attribute if Kafka specific ZooKeeper quorum is used
 if node[:use_hadoop_zookeeper_quorum]
-  zk_hosts = node[:bcpc][:hadoop][:zookeeper][:servers]
+  zk_hosts = get_node_attributes(HOSTNAME_NODENO_ATTR_SRCH_KEYS,"zookeeper_server","bcpc-hadoop")
 else
   zk_hosts = get_req_node_attributes(get_zk_nodes,HOSTNAME_NODENO_ATTR_SRCH_KEYS)
   node.override[:bcpc][:hadoop][:zookeeper][:servers] = zk_hosts
 end
 # Override Kafka related node attributes
 node.override[:kafka][:broker][:zookeeper][:connect] = zk_hosts.map{|x| float_host(x['hostname'])}
-node.override[:kafka][:base_url] = get_binary_server_url + "kafka"
-node.override[:kafka][:broker][:host_name] = float_host(node[:fqdn])
-node.override[:kafka][:broker][:advertised_host_name] = float_host(node[:fqdn])
-node.override[:kafka][:broker][:jmx_port] = node[:bcpc][:hadoop][:kafka][:jmx][:port]
