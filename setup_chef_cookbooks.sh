6f7f29b35237c79561be830c51d7684a69ee7d3c
Biju Nair <bnair10@bloomberg.net>
2014-11-18 15:58:54 -0500

    Point to jmxtrans-cookbook git repo which includes the changes to define alias in JMXtrans configuration file

--- setup_chef_cookbooks.sh
+++ setup_chef_cookbooks.sh
@@ -70,5 +70,5 @@ if [[ ! -d kafka ]]; then
   git clone https://github.com/mthssdrbrg/kafka-cookbook.git kafka
 fi
 [[ -d java ]] || git clone https://github.com/socrata-cookbooks/java.git java
-[[ -d jmxtrans ]] || git clone https://github.com/bijugs/chef-jmxtrans.git jmxtrans
+[[ -d jmxtrans ]] || git clone https://github.com/bijugs/jmxtrans-cookbook.git jmxtrans
 [[ -d cobblerd ]] || git clone https://github.com/cbaenziger/cobbler-cookbook.git cobblerd -b cobbler_profile
