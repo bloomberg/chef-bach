d5f6ea3533fd299aaab227e2f20a6a9768882b32
Amit Anand <aanand59@bloomberg.net>
2015-08-03 08:48:53 -0400

    Tune nodemanager to use 50% of resources(memory/cpu)

--- cookbooks/bcpc-hadoop/attributes/default.rb
+++ cookbooks/bcpc-hadoop/attributes/default.rb
@@ -14,9 +14,9 @@ default["bcpc"]["repos"]["hdp_utils"] = 'http://public-repo-1.hortonworks.com/HD
 default["bcpc"]["hadoop"]["disks"] = []
 default["bcpc"]["hadoop"]["oozie"]["admins"] = []
 default["bcpc"]["hadoop"]["oozie"]["memory_opts"] = "-Xmx2048m -XX:MaxPermSize=256m"
-default["bcpc"]["hadoop"]["yarn"]["nodemanager"]["avail_memory"]["ratio"] = 0.8
+default["bcpc"]["hadoop"]["yarn"]["nodemanager"]["avail_memory"]["ratio"] = 0.5
 default["bcpc"]["hadoop"]["yarn"]["nodemanager"]["avail_memory"]["size"] = nil
-default["bcpc"]["hadoop"]["yarn"]["nodemanager"]["avail_vcpu"]["ratio"] = 0.8
+default["bcpc"]["hadoop"]["yarn"]["nodemanager"]["avail_vcpu"]["ratio"] = 0.5
 default["bcpc"]["hadoop"]["yarn"]["nodemanager"]["avail_vcpu"]["count"] = nil
 default["bcpc"]["hadoop"]["yarn"]["scheduler"] = "org.apache.hadoop.yarn.server.resourcemanager.scheduler.fair.FairScheduler"
 default['bcpc']['hadoop']['yarn']['historyserver']['heap']["size"] = 128
