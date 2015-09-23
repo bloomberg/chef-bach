ff851176c599c12bd1af1a9d66cdd5bada558563
Amit Anand <aanand59@bloomberg.net>
2015-09-23 10:37:01 -0400

    Start Kerberos services after installation/configuration

--- cookbooks/bach_krb5/recipes/krb5_server.rb
+++ cookbooks/bach_krb5/recipes/krb5_server.rb
@@ -13,14 +13,20 @@ end
 
 include_recipe "krb5::kadmin_init"
 
-kdc_resource = resources("service[krb5-kdc]")
-kdc_resource.supports(:status => true, :restart => true, :reload => false)
-kdc_resource.action([:enable, :start])
-kdc_resource.subscribes(:restart, "template #{node['krb5']['data_dir']}/kdc.conf", :delayed) 
+ruby_block 'start_kerberos_services' do
+  block do
+    kdc_resource = run_context.resource_collection.find("service[krb5-kdc]")
+    kdc_resource.supports(:status => true, :restart => true, :reload => false)
+    kdc_resource.action([:enable, :start])
+    kdc_resource.subscribes(:restart, "template #{node['krb5']['data_dir']}/kdc.conf", :delayed)
+    kdc_resource.run_action(:start)
 
-kadm_resource = resources("service[krb5-admin-server]")
-kadm_resource.supports(:status => true, :restart => true, :reload => false)
-kadm_resource.action([:enable, :start])
-kadm_resource.subscribes(:restart, "template node['krb5']['kdc_conf']['realms'][default_realm]['acl_file']", :delayed)
+    kadm_resource = run_context.resource_collection.find("service[krb5-admin-server]")
+    kadm_resource.supports(:status => true, :restart => true, :reload => false)
+    kadm_resource.action([:enable, :start])
+    kadm_resource.subscribes(:restart, "template node['krb5']['kdc_conf']['realms'][default_realm]['acl_file']", :delayed)
+    kadm_resource.run_action(:start)
+  end
+end
 
 include_recipe "bach_krb5::gems"
