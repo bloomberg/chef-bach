6ac3e67cf968e4bb00ad2e676671cf301a72a7f1
Andrew Jones <andrew@jones.ec>
2015-09-28 16:48:34 -0400

    Remove remaining shell scripts

--- finish-head.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# You can put fixups here for the case when the 'knife bootstrap -E
-# ...' command step doesnt succeed fully, in assigning a role to a
-# node. For example see below. Currently none should be needed
-#service glance-api       restart
-#service glance-registry  restart
-#chef-client
--- remove-dead-osd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-OSD="$1"
-if [[ -z "$1" ]]; then
-    echo "Usage $0 osd number to remove"
-    exit
-fi
-sudo ceph osd crush remove osd."$OSD"
-sudo ceph auth del osd."$OSD"
-sudo ceph osd rm "$OSD"
