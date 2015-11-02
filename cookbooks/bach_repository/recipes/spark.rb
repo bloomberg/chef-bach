836e012c73caae81f68f8b74a4d8d979cbfbee31
Andrew Jones <andrew@jones.ec>
2015-11-02 11:10:15 -0500

    Update bach_repository with spark 1.4.1

--- cookbooks/bach_repository/recipes/spark.rb
+++ cookbooks/bach_repository/recipes/spark.rb
@@ -7,17 +7,17 @@ require 'tmpdir'
 include_recipe 'bach_repository::directory'
 bins_dir = node['bach']['repository']['bins_directory']
 spark_extract_dir = Dir.mktmpdir
-spark_tar_file = "spark-1.4.0-bin-hadoop2.6.tgz"
-spark_extracted_file_name = "spark-1.4.0-bin-hadoop2.6"
 spark_install_dir = "/usr/spark"
 spark_pkg_prefix = "spark"
-spark_pkg_version = "1.4.0"
+spark_pkg_version = "1.4.1"
+spark_tar_file = "spark-#{spark_pkg_version}-bin-hadoop2.6.tgz"
+spark_extracted_file_name = "spark-#{spark_pkg_version}-bin-hadoop2.6"
 spark_deb_path =
   "#{bins_dir}/#{spark_pkg_prefix}_#{spark_pkg_version}_amd64.deb"
 
 remote_file "#{bins_dir}/#{spark_tar_file}" do
-  source 'http://d3kbcqa49mib13.cloudfront.net/spark-1.4.0-bin-hadoop2.6.tgz'
-  checksum '19c7b4b58f3b84bed513d4d532344290d104dc394fb74c6b7bdbabab69e1f605'
+  source "http://d3kbcqa49mib13.cloudfront.net/#{spark_tar_file}"
+  checksum '9cde95349cccfeb99643d2dadb63f8e88ac355e0038aae7d5029142ce94ae370'
 end
 
 execute "extract spark tar" do
