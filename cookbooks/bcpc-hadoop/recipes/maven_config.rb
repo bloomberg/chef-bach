97f7ead3dfa49307063fc30d4d1d7c9f61553dc3
Andrew Jones <andrew@jones.ec>
2015-11-16 13:16:20 -0500

    Update maven_config recipe maven cookbook version

--- cookbooks/bcpc-hadoop/recipes/maven_config.rb
+++ cookbooks/bcpc-hadoop/recipes/maven_config.rb
@@ -2,14 +2,15 @@ require 'pathname'
 
 node.override['maven']['install_java'] = false
 
-internet_download_url = node['maven']['3']['url']
-maven_file = Pathname.new(node['maven']['3']['url']).basename
+internet_download_url = node['maven']['url']
+maven_file = Pathname.new(node['maven']['url']).basename
 
-node.override['maven']['3']['url'] = "#{get_binary_server_url}/#{maven_file}"
+node.override['maven']['url'] = "#{get_binary_server_url}/#{maven_file}"
 
 # download Maven only if not already stashed in the bins directory
 remote_file "/home/vagrant/chef-bcpc/bins/#{maven_file}" do
   source internet_download_url
-  action :create_if_missing
-  mode '0555'
+  action :create
+  mode 0555
+  checksum node['maven']['checksum']
 end
