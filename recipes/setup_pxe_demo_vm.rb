412ca53a6d9b1af516764e9a9a8d8c975c03ec9b
Andrew Jones <andrew@jones.ec>
2015-11-18 14:59:35 -0500

    Now creating VMs for PXE use

--- recipes/setup_pxe_demo_vm.rb
+++ recipes/setup_pxe_demo_vm.rb
@@ -17,75 +17,26 @@ require 'chef/provisioning/ssh_driver'
 # wait until it answers on 22
 # use ssh profivisioning
 
-# do i use vagrant to stand up the VM? if i do, what does it expect to find? no.  vagrant expects a lot of stuff and i don't want to write a driver like steve
-
 # See ROM-O-MATIC.md for instruction on how to generate a new gPXE ROM.
-pxe_rom_path = File.join(Chef::Config.file_cache_path, 'gpxe.rom')
 cookbook_file pxe_rom_path do
   source 'gpxe-1.0.1-80861004.rom'
-  mode 0444
+  mode 0660
 end
 
-def get_vbox_vm_info(name:)
-  require 'mixlib/shellout'
-  c = Mixlib::ShellOut.new('vboxmanage', 'showvminfo',
-                           name, '--machinereadable')
-  c.run_command!
-  tuples = c.stdout.split("\n")
-    .map{ |element| e.split("=") }
-    .flatten
-    .map{ |string| string.gsub(/^"/, '').gsub(/"$/, '') }
-  Hash[*tuples]
-end
+worker_node_count = node[:bach][:cluster][:node_count].to_i
+total_node_count = worker_node_count + 2
 
-def create_vbox_vm(name:)
-  system('vboxmanage', 'createvm',
-         '--name', name,
-         '--ostype', 'Ubuntu_64',
-         '--register')
+1.upto(total_node_count).each do |n|
+  vm_name = "bach-vm#{n}-b#{build_id}"
+  vm_mgmt_ip = "10.0.101." + (3 + n).to_s
+  vm_storage_ip = "10.0.101." + (19 + n).to_s
+  vm_floating_ip = "10.0.101." + (35 + n).to_s
+  my_netmask = '255.255.255.240'
 
-  system('vboxmanage', 'modifyvm', name,
-         '--memory', node[:bach][:cluster][:demo][:memory])
+  ruby_block "#{vm_name}-create" do
+    block do
+      create_vbox_vm(name: vm_name)
+    end
+  end
 
-  system('vboxmanage', 'modifyvm', name,
-         '--cpus', node[:bach][:cluster][:demo][:cpus])
-
-  system('vboxmanage', 'modifyvm', name,
-         '--cpus', node[:bach][:cluster][:demo][:cpus])
-
-  #
-  # The first interface on a vagrant box is always NAT, so our three
-  # cluster networks are the second, third, and fourth adapters on the
-  # bootstrap VM.
-  #
-  iface1, iface2, iface3 =
-    get_vbox_vm_info(name: bootstrap_vm_name).values_at("hostonlyadapter2",
-                                                        "hostonlyadapter3",
-                                                        "hostonlyadapter4")
-
-  system('vboxmanage', 'modifyvm', name,
-         '--nic1', 'hostonly',
-         '--hostonlyadapter1', iface1,
-         '--nictype', '82543GC')
-  
-  system('vboxmanage', 'modifyvm', name,
-         '--nic2', 'hostonly',
-         '--hostonlyadapter2', iface2,
-         '--nictype', '82543GC')
-  
-  system('vboxmanage', 'modifyvm', name,
-         '--nic3', 'hostonly',
-         '--hostonlyadapter3', iface3,
-         '--nictype', '82543GC')
-
-  system('vboxmanage', 'setextradata', name,
-         'VBoxInternal/Devices/pcbios/0/Config/LanBootRom',
-         pxe_rom_path)
-
-  system('vboxmanage', 'storagectl', name,
-         '--name', "SATA Controller",
-         '--add', 'sata')
-end
-
-def destroy_vbox_vm(name:)
 end
