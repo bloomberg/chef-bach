38abd0592fce1bb871d389fc62de00a8b98959ab
Andrew Jones <andrew@jones.ec>
2015-11-18 11:04:31 -0500

    Fix up bcpc-hadoop metadata for new maven cookbook

--- cookbooks/bcpc-hadoop/metadata.rb
+++ cookbooks/bcpc-hadoop/metadata.rb
@@ -1,14 +1,15 @@
-name             "bcpc-hadoop"
-maintainer       "Bloomberg Finance L.P."
-maintainer_email "rrichardson6@bloomberg.net"
-license          "Apache License 2.0"
-description      "Installs/Configures Bloomberg Clustered Private Hadoop Cloud (BCPHC)"
+name             'bcpc-hadoop'
+maintainer       'Bloomberg Finance L.P.'
+maintainer_email 'compute@bloomberg.net'
+license          'Apache License 2.0'
+description      'Installs/Configures Bloomberg Clustered Private Hadoop Cloud (BCPHC)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          '0.1.0'
 
-depends "bcpc", ">= 0.5.0"
-depends "dpkg_autostart"
-depends "sysctl"
-depends "pam"
+depends 'bcpc', '>= 0.5.0'
+depends 'dpkg_autostart'
 depends 'java', '>= 1.28.0'
+depends 'maven', '~> 2'
+depends 'pam'
+depends 'sysctl'
 depends 'ulimit'
