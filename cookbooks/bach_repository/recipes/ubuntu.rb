253711baa6b046aa6e3f44ecc8a200b8ab3a9c9f
Andrew Jones <andrew@jones.ec>
2015-11-30 11:14:57 -0500

    Update ubuntu installer image

--- cookbooks/bach_repository/recipes/ubuntu.rb
+++ cookbooks/bach_repository/recipes/ubuntu.rb
@@ -6,7 +6,7 @@ include_recipe 'bach_repository::directory'
 bins_dir = node['bach']['repository']['bins_directory']
 
 remote_file "#{bins_dir}/ubuntu-12.04-mini.iso" do
-  source 'http://archive.ubuntu.com/ubuntu/dists/precise/main/installer-amd64/current/images/netboot/mini.iso'
+  source 'http://archive.ubuntu.com/ubuntu/dists/precise-updates/main/installer-amd64/20101020ubuntu136.21/images/netboot/mini.iso'
   mode 0444
-  checksum '7df121f07878909646c8f7862065ed7182126b95eadbf5e1abb115449cfba714'
+  checksum '6150143c06369134c15ac771a3b349a18c7d4660c1755b8fbefc032269a74f8f'
 end
