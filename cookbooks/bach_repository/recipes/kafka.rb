0bf74d1cbfcafee4b6aac2fdaa84fdbfe3fcd01f
Andrew Jones <andrew@jones.ec>
2016-02-11 17:09:10 -0500

    bach_repository: correct kafka bins locations.

--- cookbooks/bach_repository/recipes/kafka.rb
+++ cookbooks/bach_repository/recipes/kafka.rb
@@ -3,7 +3,13 @@
 # Recipe:: kafka
 #
 include_recipe 'bach_repository::directory'
-bins_dir = node['bach']['repository']['bins_directory']
+
+kafka_bins_dir =
+  File.join(node['bach']['repository']['bins_directory'],'kafka')
+
+directory kafka_bins_dir do
+  mode 0555
+end
 
 {
   '0.8.1' => 
@@ -11,7 +17,13 @@ bins_dir = node['bach']['repository']['bins_directory']
   '0.8.1.1' =>
     'cb141c1d50b1bd0d741d68e5e21c090341d961cd801e11e42fb693fa53e9aaed'
 }.each do |version, checksum|
-  remote_file "#{bins_dir}/kafka_2.9.2-#{version}.tgz" do
+  versioned_dir = File.join(kafka_bins_dir,version)
+  
+  directory versioned_dir do
+    mode 0555
+  end
+  
+  remote_file "#{versioned_dir}/kafka_2.9.2-#{version}.tgz" do
     source "https://archive.apache.org/dist/kafka/#{version}" +
            "/kafka_2.9.2-#{version}.tgz"
     checksum checksum    
