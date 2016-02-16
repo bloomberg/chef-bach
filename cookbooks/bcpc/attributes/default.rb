01ecbb7e8516e45c25d344387aa2ff89ca815fc6
Andrew Jones <andrew@jones.ec>
2016-02-16 18:34:48 -0500

    Derive 'node_number' field from MAC addresses
    The node_number was previously derived from a node's management IP.
    In a routed rack configuration, this could cause the node_number to
    wrap around and overlap inside a single cluster.
    
    This commit derives the node_number from the MAC address on the
    management interface.  The MAC is converted to an integer.  The node
    number is this integer modulo Integer.MAX_VALUE.
    
    This gives us a ~31 bit positive integer that is guaranteed to be
    unique inside any single-vendor customer.  It is very likely to be
    unique across any two NICs, but not certainly so, since only the
    last 7 bits of a 24 bit OUI are present.
    
    Zookeeper and keepalived use (node_number % 8) to derive an 8 bit
    value from the 31 bit node number.  In a sufficiently large cluster
    this may overlap.  A helper method added to libraries/node_numbers.rb
    does a node search to raise an exception in the event of an overlap.

--- cookbooks/bcpc/attributes/default.rb
+++ cookbooks/bcpc/attributes/default.rb
@@ -3,78 +3,124 @@
 #  General configuration for this cluster
 #
 ###########################################
-default['bcpc']['country'] = "US"
-default['bcpc']['state'] = "NY"
-default['bcpc']['location'] = "New York"
-default['bcpc']['organization'] = "Bloomberg"
+default['bcpc']['country'] = 'US'
+default['bcpc']['state'] = 'NY'
+default['bcpc']['location'] = 'New York'
+default['bcpc']['organization'] = 'Bloomberg'
 # Should be kvm (or qemu if testing in VMs)
-default['bcpc']['virt_type'] = "kvm"
+default['bcpc']['virt_type'] = 'kvm'
 # Region name for this cluster
 default['bcpc']['region_name'] = node.chef_environment
 # Domain name that will be used for DNS
-default['bcpc']['domain_name'] = "bcpc.example.com"
+default['bcpc']['domain_name'] = 'bcpc.example.com'
 # Key if Cobalt+VMS is to be used
 default['bcpc']['vms_key'] = nil
 
 default['bcpc']['encrypt_data_bag'] = false
 
-# Specify the kernel you wish to install. For default latest LTS kernel use "linux-server"
-default['bcpc']['bootstrap']['preseed']['kernel'] = "linux-generic-lts-trusty"
-default['bcpc']['bootstrap']['preseed']['add_kernel_opts'] = "console=ttyS0"
-default['bcpc']['bootstrap']['preseed']['late_command'] = "true"
+# Specify the kernel you wish to install.
+# For default, (latest LTS kernel) use "linux-server"
+default['bcpc']['bootstrap']['preseed']['kernel'] = 'linux-generic-lts-trusty'
+default['bcpc']['bootstrap']['preseed']['add_kernel_opts'] = 'console=ttyS0'
+default['bcpc']['bootstrap']['preseed']['late_command'] = 'true'
 default['bcpc']['bootstrap']['admin_users'] = []
+
+#
+# The node_number is used to derive Kafka broker IDs, Zookeeper myid
+# files, keepalived node priorities, and other values.  It must be
+# unique within a cluster.
+#
+# The node number is generated from the integer value of the
+# default interface mac_address, modulo Java's Integer.MAX_VALUE.
+#
+# For 8-bit node numbers, (e.g. zookeeper, keepalived) there is a
+# helper method defined in libraries/node_numbers.rb
+#
+unless node['bcpc']['node_number']
+  interface_name = node[:network][:default_interface]
+  default_interface = node[:network][:interfaces][interface_name]
+
+  all_macs = default_interface[:addresses].select do |_addr, hash|
+    hash['family'] == 'lladdr'
+  end
+
+  unless all_macs.count > 0
+    fail 'Could not find MAC addresses for ' +
+      'node[:network][:default_interface] ' +
+      "(#{node[:network][:default_interface]}) !"
+  end
+
+  mac_address = all_macs.keys.sort.first
+
+  max_value = (2**31 - 1) # Java Integer.MAX_VALUE
+  integer_mac = mac_address.downcase.split(':').join.to_i(16)
+  node.set['bcpc']['node_number'] = integer_mac % max_value
+end
 
 ###########################################
 #
 #  Host-specific defaults for the cluster
 #
 ###########################################
-default['bcpc']['bootstrap']['interface'] = "eth0"
-default['bcpc']['bootstrap']['pxe_interface'] = "eth1"
-default['bcpc']['bootstrap']['server'] = "10.0.100.3"
+default['bcpc']['bootstrap']['interface'] = 'eth0'
+default['bcpc']['bootstrap']['pxe_interface'] = 'eth1'
+default['bcpc']['bootstrap']['server'] = '10.0.100.3'
 default['bcpc']['bootstrap']['vip'] = node['bcpc']['bootstrap']['server']
-default['bcpc']['bootstrap']['dhcp_range'] = "10.0.100.14 10.0.100.250"
-default['bcpc']['bootstrap']['dhcp_subnet'] = "10.0.100.0"
+default['bcpc']['bootstrap']['dhcp_range'] = '10.0.100.14 10.0.100.250'
+default['bcpc']['bootstrap']['dhcp_subnet'] = '10.0.100.0'
 
 ###########################################
 #
 #  Network settings for the cluster
 #
 ###########################################
-default['bcpc']['management']['vip'] = "1.2.3.5"
-default['bcpc']['management']['ip'] = "1.2.3.4"
+default['bcpc']['management']['vip'] = '1.2.3.5'
+default['bcpc']['management']['ip'] = '1.2.3.4'
 
-default['bcpc']['metadata']['ip'] = "169.254.169.254"
+default['bcpc']['metadata']['ip'] = '169.254.169.254'
 
-default['bcpc']['ntp_servers'] = [ "pool.ntp.org" ]
-default['bcpc']['dns_servers'] = [ "8.8.8.8", "8.8.4.4" ]
+default['bcpc']['ntp_servers'] = ['pool.ntp.org']
+default['bcpc']['dns_servers'] = ['8.8.8.8', '8.8.4.4']
 
 ###########################################
 #
 #  Repos for things we rely on
 #
 ###########################################
-default['bcpc']['repos']['rabbitmq'] = "http://www.rabbitmq.com/debian"
-default['bcpc']['repos']['mysql'] = "http://repo.percona.com/apt"
-default['bcpc']['repos']['hwraid'] = "http://hwraid.le-vert.net/ubuntu"
-default['bcpc']['repos']['gridcentric'] = "http://downloads.gridcentric.com/packages/%s/%s/ubuntu"
-default['bcpc']['repos']['ubuntu-tools'] = "http://ppa.launchpad.net/canonical-support/support-tools/ubuntu"
-default['bcpc']['ubuntu']['version'] = "precise"
-default["bcpc"]["repos"]["hortonworks"] = 'http://public-repo-1.hortonworks.com/HDP/ubuntu12/2.x/updates/2.3.4.0'
-default["bcpc"]["repos"]["hdp_utils"] = 'http://public-repo-1.hortonworks.com/HDP-UTILS-1.1.0.20/repos/ubuntu12'
+default['bcpc']['ubuntu']['version'] = 'precise'
+
+default['bcpc']['repos'].tap do |repos|
+  repos['rabbitmq'] = 'http://www.rabbitmq.com/debian'
+
+  repos['mysql'] = 'http://repo.percona.com/apt'
+
+  repos['hwraid'] = 'http://hwraid.le-vert.net/ubuntu'
+
+  repos['gridcentric'] =
+    'http://downloads.gridcentric.com/packages/%s/%s/ubuntu'
+
+  repos['ubuntu-tools'] =
+    'http://ppa.launchpad.net/canonical-support/support-tools/ubuntu'
+
+  repos['hortonworks'] =
+    'http://public-repo-1.hortonworks.com/HDP/ubuntu12/2.x/updates/2.3.4.0'
+
+  repos['hdp_utils'] =
+    'http://public-repo-1.hortonworks.com/HDP-UTILS-1.1.0.20/repos/ubuntu12'
+end
 
 ###########################################
 #
 #  Default names for db's, pools, and users
 #
 ###########################################
-default['bcpc']['pdns_dbname'] = "pdns"
-default['bcpc']['zabbix_dbname'] = "zabbix"
+default['bcpc']['pdns_dbname'] = 'pdns'
+default['bcpc']['zabbix_dbname'] = 'zabbix'
 
-default['bcpc']['admin_tenant'] = "AdminTenant"
-default['bcpc']['admin_role'] = "Admin"
-default['bcpc']['member_role'] = "Member"
-default['bcpc']['admin_email'] = "admin@localhost.com"
+default['bcpc']['admin_tenant'] = 'AdminTenant'
+default['bcpc']['admin_role'] = 'Admin'
+default['bcpc']['member_role'] = 'Member'
+default['bcpc']['admin_email'] = 'admin@localhost.com'
 
 default[:bcpc][:ports][:apache][:radosgw] = 8080
 default[:bcpc][:ports][:apache][:radosgw_https] = 8443
@@ -82,22 +128,38 @@ default[:bcpc][:ports][:haproxy][:radosgw] = 80
 default[:bcpc][:ports][:haproxy][:radosgw_https] = 443
 
 # Memory where InnoDB caches table and index data (in MB). Default is 128M.
-default['bcpc']['mysql']['innodb_buffer_pool_size'] = [(node[:memory][:total].to_i / 1024 * 0.02).floor, 128].max
+default['bcpc']['mysql']['innodb_buffer_pool_size'] =
+  ([(node[:memory][:total].to_i / 1024 * 0.02).floor, 128].max)
 
 #################################################
 #  attributes for chef vault download and install
 #################################################
-default['bcpc']['chefvault']['filename'] = "chef-vault-2.2.4.gem"
-default['bcpc']['chefvault']['checksum'] = "8d89c96554f614ec2a80ef20e98b0574c355a6ea119a30bd49aa9cfdcde15b4a"
+default['bcpc']['chefvault']['filename'] =
+  'chef-vault-2.2.4.gem'
+
+default['bcpc']['chefvault']['checksum'] =
+  '8d89c96554f614ec2a80ef20e98b0574c355a6ea119a30bd49aa9cfdcde15b4a'
+
 # bcpc binary server pathnames
 default['bcpc']['bin_dir']['path'] = '/home/vagrant/chef-bcpc/bins/'
 default['bcpc']['bin_dir']['gems'] = "#{node['bcpc']['bin_dir']['path']}/gems"
+
 # rubygems download website URL
 default['bcpc']['gem_source'] = 'https://rubygems.org/downloads'
 
 # mysql connector attributes
-default['bcpc']['mysql']['connector']['version'] = '5.1.37'
-default['bcpc']['mysql']['connector']['tarball_md5sum'] = '9ef584d3328735db51bd5dde3f602c22'
-default['bcpc']['mysql']['connector']['url'] = "http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-#{node['bcpc']['mysql']['connector']['version']}.tar.gz"
-default['bcpc']['mysql']['connector']['package']['short_name'] = 'mysql-connector-java'
-default['bcpc']['mysql']['connector']['package']['name'] = "#{node['bcpc']['mysql']['connector']['package']['short_name']}_#{node['bcpc']['mysql']['connector']['version']}_all.deb"
+default['bcpc']['mysql']['connector'].tap do |connector|
+  connector['version'] = '5.1.37'
+
+  connector['tarball_md5sum'] = '9ef584d3328735db51bd5dde3f602c22'
+
+  connector['url'] =
+    'http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-' +
+    node['bcpc']['mysql']['connector']['version'] + '.tar.gz'
+
+  connector['package']['short_name'] = 'mysql-connector-java'
+
+  connector['package']['name'] =
+    node['bcpc']['mysql']['connector']['package']['short_name'] + '_' +
+    node['bcpc']['mysql']['connector']['version'] + '_all.deb'
+end
