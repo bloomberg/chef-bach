ab5b25629aba12039dd2c5badc3ef0ed1281c5af
Andrew Jones <andrew@jones.ec>
2016-02-19 19:18:16 -0500

    Move most kafka attributes out of environment, into attributes file.

--- cookbooks/kafka-bcpc/attributes/default.rb
+++ cookbooks/kafka-bcpc/attributes/default.rb
@@ -5,12 +5,14 @@
 # Attribute to indidate whether an existing Hadoop Zookeeper
 # can be used. If not Kafka Zookeeper quorum need to be created 
 default[:use_hadoop_zookeeper_quorum] = false
+
 #
 # Overwriting community kafka cookbook attributes 
 #
 default[:kafka][:broker][:port] = 6667
 default[:kafka][:broker][:advertised_port] = 6667
 default[:kafka][:broker][:broker_id] = node[:bcpc][:node_number]
+default[:kafka][:broker][:reserved_broker_max_id] = (2 ** 31) - 1
 default[:kafka][:automatic_start] = true
 default[:kafka][:automatic_restart] = true
 
@@ -22,6 +24,12 @@ default[:kafka][:broker][:controlled][:shutdown][:enable] = true
 default[:kafka][:broker][:controlled][:shutdown][:max][:retries] = 3
 default[:kafka][:broker][:controlled][:shutdown][:retry][:backoff][:ms] = 5000
 
-
-
+#
+# These attributes are normally overriden in the Chef environment.
+#
+default[:kafka][:version] = '0.9.0.0'
+default[:kafka][:scala_version] = '2.11'
+default[:kafka][:checksum] = ''
+default[:kafka][:md5sum] = ''
+default[:kafka][:log_dir] = '/var/log/kafka'
 
