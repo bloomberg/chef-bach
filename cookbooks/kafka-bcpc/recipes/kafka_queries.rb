6fcab401bd5257c38ec826330bfb7b4ab2245f75
Dan Bahir <dannybahir@gmail.com>
2016-04-08 17:31:56 -0400

    remove depency on zabbix triggers

--- cookbooks/kafka-bcpc/recipes/kafka_queries.rb
+++ cookbooks/kafka-bcpc/recipes/kafka_queries.rb
@@ -3,33 +3,30 @@ trigger_chk_period = "#{node[:bcpc][:hadoop][:zabbix][:trigger_chk_period]}m"
 node.set[:bcpc][:hadoop][:graphite][:service_queries][:kafka_head] = {
   'kafka_head.activeControllerCount' => {
      'query' => "jmx.kafka.*.kafka.KafkaController.ActiveControllerCount",
-     'trigger_val' => "max(3m)",
+     'trigger_val' => "max(10m)",
      'trigger_cond' => "=0",
      'trigger_name' => "KafkaControllerCount",
      'enable' => true,
-     'trigger_dep' => ["KafkaControllerCount"],
      'trigger_desc' => "Kafka broker seems to be down",
      'severity' => 4,
      'route_to' => "admin"
   },
   'kafka_head.OfflinePartitionsCount' => {
      'query' => "jmx.kafka.*.kafka.KafkaController.OfflinePartitionsCount",
-     'trigger_val' => "min(3m)",
+     'trigger_val' => "min(10m)",
      'trigger_cond' => "=0",
      'trigger_name' => "KafkaOfflinePartitionsCount",
      'enable' => true,
-     'trigger_dep' => ["KafkaOfflinePartitionsCount"],
      'trigger_desc' => "A Kafka partition seems to be offline",
      'severity' => 4,
      'route_to' => "admin"
   },
   'kafka_head.UnderReplicatedPartitions' => {
      'query' => "jmx.kafka.*.kafka.ReplicaManager.UnderReplicatedPartitions",
-     'trigger_val' => "max(3m)",
+     'trigger_val' => "max(10m)",
      'trigger_cond' => ">0",
      'trigger_name' => "KafkaUnderReplicatedPartitions",
      'enable' => true,
-     'trigger_dep' => ["KafkaUnderReplicatedPartitions"],
      'trigger_desc' => "Kafka broker seems to have under replicated partitions",
      'severity' => 4,
      'route_to' => "admin"
