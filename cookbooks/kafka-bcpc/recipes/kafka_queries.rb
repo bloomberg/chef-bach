0ab5d3a93dcbec5da7794e08f7fe241262ee31c9
Dan Bahir <dannybahir@gmail.com>
2016-04-08 17:00:35 -0400

    changed zabbix trigger interval to 3 min

--- cookbooks/kafka-bcpc/recipes/kafka_queries.rb
+++ cookbooks/kafka-bcpc/recipes/kafka_queries.rb
@@ -3,7 +3,7 @@ trigger_chk_period = "#{node[:bcpc][:hadoop][:zabbix][:trigger_chk_period]}m"
 node.set[:bcpc][:hadoop][:graphite][:service_queries][:kafka_head] = {
   'kafka_head.activeControllerCount' => {
      'query' => "jmx.kafka.*.kafka.KafkaController.ActiveControllerCount",
-     'trigger_val' => "max(10m)",
+     'trigger_val' => "max(3m)",
      'trigger_cond' => "=0",
      'trigger_name' => "KafkaControllerCount",
      'enable' => true,
@@ -14,7 +14,7 @@ node.set[:bcpc][:hadoop][:graphite][:service_queries][:kafka_head] = {
   },
   'kafka_head.OfflinePartitionsCount' => {
      'query' => "jmx.kafka.*.kafka.KafkaController.OfflinePartitionsCount",
-     'trigger_val' => "min(10m)",
+     'trigger_val' => "min(3m)",
      'trigger_cond' => "=0",
      'trigger_name' => "KafkaOfflinePartitionsCount",
      'enable' => true,
@@ -25,7 +25,7 @@ node.set[:bcpc][:hadoop][:graphite][:service_queries][:kafka_head] = {
   },
   'kafka_head.UnderReplicatedPartitions' => {
      'query' => "jmx.kafka.*.kafka.ReplicaManager.UnderReplicatedPartitions",
-     'trigger_val' => "max(10m)",
+     'trigger_val' => "max(3m)",
      'trigger_cond' => ">0",
      'trigger_name' => "KafkaUnderReplicatedPartitions",
      'enable' => true,
