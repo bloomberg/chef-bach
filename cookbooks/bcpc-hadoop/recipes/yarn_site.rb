1a7534c4ccaf66ed6f13ab009cf8f763c163f63c
Andrew Jones <andrew@jones.ec>
2016-05-06 15:54:40 -0400

    Remove old yarn-site templates

--- cookbooks/bcpc-hadoop/recipes/yarn_site.rb
+++ cookbooks/bcpc-hadoop/recipes/yarn_site.rb
@@ -203,18 +203,6 @@ complete_yarn_site_hash =
   yarn_site_generated_values.merge(yarn_site_values)
 
 template "/etc/hadoop/conf/yarn-site.xml" do
-  source "hdp_yarn-site.xml.erb"
-  mode 0644
-  variables(:nn_hosts => node[:bcpc][:hadoop][:nn_hosts],
-            :zk_hosts => node[:bcpc][:hadoop][:zookeeper][:servers],
-            :jn_hosts => node[:bcpc][:hadoop][:jn_hosts],
-            :rm_hosts => node[:bcpc][:hadoop][:rm_hosts],
-            :dn_hosts => node[:bcpc][:hadoop][:dn_hosts],
-            :hs_hosts => node[:bcpc][:hadoop][:hs_hosts],
-            :mounts => node[:bcpc][:hadoop][:mounts])
-end
-
-template "/etc/hadoop/conf/yarn-site.fresh.xml" do
   source "generic_site.xml.erb"
   mode 0644
   variables(:options => complete_yarn_site_hash)
