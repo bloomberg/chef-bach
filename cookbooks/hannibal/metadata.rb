a9ecefc9647b345ba269f173982e1bae4ee41e46
Andrew Jones <andrew@jones.ec>
2016-08-11 11:46:00 -0400

    Update all cookbook versions to 1.4.0

--- cookbooks/hannibal/metadata.rb
+++ cookbooks/hannibal/metadata.rb
@@ -2,7 +2,7 @@ name             'hannibal'
 maintainer       'Bloomberg Finance L.P.'
 description      'Recipes to setup pre-requisites, build and install hannibal on cluster'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.4.0'
 
 depends "ark"
 
