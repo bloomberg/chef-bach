7f761d3cf7386ec6c5878132ce0d33af93be0eb3
Andrew Jones <andrew@jones.ec>
2016-08-29 10:56:13 -0400

    Update all cookbook versions to 1.5.0

--- cookbooks/bach_spark/metadata.rb
+++ cookbooks/bach_spark/metadata.rb
@@ -4,11 +4,11 @@ name             'bach_spark'
 maintainer       'Bloomberg Finance L.P.'
 description      'Cookbook to setup Apache Spark'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.5.0'
 
 %w(ubuntu).each do |os|
   supports os
 end
 
-depends 'bcpc', '= 0.1.0'
-depends 'bcpc-hadoop', '= 0.1.0'
+depends 'bcpc', '= 1.5.0'
+depends 'bcpc-hadoop', '= 1.5.0'
--- cookbooks/bcpc-hadoop/metadata.rb
+++ cookbooks/bcpc-hadoop/metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'compute@bloomberg.net'
 license          'Apache License 2.0'
 description      'Installs/Configures Bloomberg Clustered Private Hadoop Cloud (BCPHC)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.5.0'
 
-depends 'bcpc', '= 0.1.0'
+depends 'bcpc', '= 1.5.0'
 depends 'java', '>= 1.28.0'
 depends 'maven', '~> 2.1.1'
 depends 'pam'
