3999b381f96070abac7bd8f47c7af7222817797f
Lev Bronshtein <lbronshtein@bloomberg.net>
2016-09-12 11:18:15 -0400

    Update all cookbook versions to 1.7.0

--- cookbooks/bach_common/metadata.rb
+++ cookbooks/bach_common/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'compute@bloomberg.net'
 license          'All rights reserved'
 description      'bach_common contains common recipes for the BACH cookbooks'
 long_description 'bach_common contains common recipes for the BACH cookbooks'
-version          '0.1.0'
+version          '1.7.0'
 
 supports 'ubuntu', '12.04'
--- cookbooks/bcpc/metadata.rb
+++ cookbooks/bcpc/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'compute@bloomberg.net'
 license          'Apache License 2.0'
 description      'Installs/Configures Bloomberg Clustered Private Cloud (BCPC)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.7.0'
 
 depends 'chef-vault', '~> 1.3.0'
 depends 'apt', '~> 2.4.0'
