e58ebc01914bffe28e5179d1f918d75a33d28991
Andrew Jones <andrew@jones.ec>
2016-09-23 15:06:41 -0400

    Reduce rubocop offenses in bcpc::networking (122 to 19)

--- cookbooks/bcpc/recipes/networking.rb
+++ cookbooks/bcpc/recipes/networking.rb
@@ -17,77 +17,84 @@
 # limitations under the License.
 #
 
-include_recipe "bcpc::default"
-include_recipe "bcpc::certs"
+include_recipe 'bcpc::default'
+include_recipe 'bcpc::certs'
 
-template "/etc/hosts" do
-    source "hosts.erb"
-    mode 00644
-    variables( :servers => get_all_nodes )
+template '/etc/hosts' do
+  source 'hosts.erb'
+  mode 00644
+  variables(servers: get_all_nodes)
 end
 
-template "/etc/ssh/sshd_config" do
-    source "sshd_config.erb"
-    mode 00644
-    notifies :restart, "service[ssh]", :immediately
+template '/etc/ssh/sshd_config' do
+  source 'sshd_config.erb'
+  mode 00644
+  notifies :restart, 'service[ssh]', :immediately
 end
 
-service "ssh" do
-    action [ :enable, :start ]
+service 'ssh' do
+  action [:enable, :start]
 end
 
-service "cron" do
-    action [ :enable, :start ]
+service 'cron' do
+  action [:enable, :start]
 end
 
 # Core networking package
-package "vlan"
+package 'vlan'
 
 # Useful system tools
-package "fio"
-package "bc"
-package "htop"
-package "sysstat"
-package "iperf"
+package 'fio'
+package 'bc'
+package 'htop'
+package 'sysstat'
+package 'iperf'
 
 # Remove spurious logging failures from this package
-package "powernap" do
-    action :remove
+package 'powernap' do
+  action :remove
 end
 
-bash "enable-ip-forwarding" do
-    user "root"
-    code <<-EOH
-        echo "1" > /proc/sys/net/ipv4/ip_forward
-        sed --in-place '/^net.ipv4.ip_forward/d' /etc/sysctl.conf
-        echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
+bash 'enable-ip-forwarding' do
+  user 'root'
+  code(
+    <<-EOH
+      echo "1" > /proc/sys/net/ipv4/ip_forward
+      sed --in-place '/^net.ipv4.ip_forward/d' /etc/sysctl.conf
+      echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
     EOH
-    not_if "grep -e '^net.ipv4.ip_forward=1' /etc/sysctl.conf"
+  )
+  not_if "grep -e '^net.ipv4.ip_forward=1' /etc/sysctl.conf"
 end
 
-bash "enable-nonlocal-bind" do
-    user "root"
-    code <<-EOH
-        echo "1" > /proc/sys/net/ipv4/ip_nonlocal_bind
-        sed --in-place '/^net.ipv4.ip_nonlocal_bind/d' /etc/sysctl.conf
-        echo 'net.ipv4.ip_nonlocal_bind=1' >> /etc/sysctl.conf
+bash 'enable-nonlocal-bind' do
+  user 'root'
+  code(
+    <<-EOH
+      echo "1" > /proc/sys/net/ipv4/ip_nonlocal_bind
+      sed --in-place '/^net.ipv4.ip_nonlocal_bind/d' /etc/sysctl.conf
+      echo 'net.ipv4.ip_nonlocal_bind=1' >> /etc/sysctl.conf
     EOH
-    not_if "grep -e '^net.ipv4.ip_nonlocal_bind=1' /etc/sysctl.conf"
+  )
+  not_if "grep -e '^net.ipv4.ip_nonlocal_bind=1' /etc/sysctl.conf"
 end
 
-bash "set-tcp-keepalive-timeout" do
-    user "root"
-    code <<-EOH
-        echo "1800" > /proc/sys/net/ipv4/tcp_keepalive_time
-        sed --in-place '/^net.ipv4.tcp_keepalive_time/d' /etc/sysctl.conf
-        echo 'net.ipv4.tcp_keepalive_time=1800' >> /etc/sysctl.conf
+bash 'set-tcp-keepalive-timeout' do
+  user 'root'
+  code(
+    <<-EOH
+      echo "1800" > /proc/sys/net/ipv4/tcp_keepalive_time
+      sed --in-place '/^net.ipv4.tcp_keepalive_time/d' /etc/sysctl.conf
+      echo 'net.ipv4.tcp_keepalive_time=1800' >> /etc/sysctl.conf
     EOH
-    not_if "grep -e '^net.ipv4.tcp_keepalive_time=1800' /etc/sysctl.conf"
+  )
+  not_if "grep -e '^net.ipv4.tcp_keepalive_time=1800' /etc/sysctl.conf"
 end
 
-bash "enable-mellanox" do
-    user "root"
-    code <<-EOH
+bash 'enable-mellanox' do
+  user 'root'
+  code(
+    <<-EOH
                 if [ -z "`lsmod | grep mlx4_en`" ]; then
                    modprobe mlx4_en
                 fi
@@ -95,89 +102,98 @@ bash "enable-mellanox" do
                    echo "mlx4_en" >> /etc/modules
                 fi
     EOH
-    only_if "lspci | grep Mellanox"
+  )
+  only_if 'lspci | grep Mellanox'
 end
 
 subnet = node[:bcpc][:management][:subnet]
+
 # XXX change subnet to pod or cell!!!
-if ["floating", "storage", "management"].select{|i| node[:bcpc][:networks][subnet][i].attribute?("slaves")}.any?
+if %w(floating storage management).select do |i|
+    node[:bcpc][:networks][subnet][i].attribute?('slaves')
+  end.any?
+
   package 'ifenslave' do
     action :upgrade
   end
-  
-  bash "enable-bonding" do
-    user "root"
-    code <<-EOH
-      modprobe bonding
-      echo 'bonding' >> /etc/modules
-    EOH
-    not_if "grep -e '^bonding' /etc/modules"
+
+  node.run_state['bcpc_networking_modules'] = %w(bonding 8021q)
+else
+  node.run_state['bcpc_networking_modules'] = %w(8021q)
+end
+
+node.run_state['bcpc_networking_modules'].each do |module_name|
+  execute "enable-#{module_name}-module" do
+    command "echo '#{module_name}' >> /etc/modules"
+    not_if "grep -e '^#{module_name}' /etc/modules"
+  end
+
+  execute "load-#{module_name}-module" do
+    command "modprobe #{module_name}"
+    not_if "lsmod | grep -e '^#{module_name}'"
   end
 end
 
-bash "enable-8021q" do
-    user "root"
-    code <<-EOH
-        modprobe 8021q
-        echo '8021q' >> /etc/modules
-    EOH
-    not_if "grep -e '^8021q' /etc/modules"
-end
-
-directory "/etc/network/interfaces.d" do
-  owner "root"
-  group "root"
+directory '/etc/network/interfaces.d' do
+  owner 'root'
+  group 'root'
   mode 00755
   action :create
 end
 
-bash "setup-interfaces-source" do
-  user "root"
-  code <<-EOH
-    echo "source /etc/network/interfaces.d/*.cfg" >> /etc/network/interfaces
-  EOH
-  not_if 'grep "^source /etc/network/interfaces.d/\*.cfg" /etc/network/interfaces'
+bash 'setup-interfaces-source' do
+  user 'root'
+  code(
+    <<-EOH
+      echo "source /etc/network/interfaces.d/*.cfg" >> /etc/network/interfaces
+    EOH
+  )
+  not_if 'grep "^source /etc/network/interfaces.d/\*.cfg" ' \
+    '/etc/network/interfaces'
 end
 
 # set up the DNS resolvers
 # we want the VIP which will be running powerdns to be first on the list
 # but the first entry in our master list is also the only one in pdns,
 # so make that the last entry to minimize double failures when upstream dies.
-resolvers=node[:bcpc][:dns_servers].dup
-if node[:bcpc][:management][:vip] and get_nodes_for("powerdns").length() > 0
+resolvers = node[:bcpc][:dns_servers].dup
+if node[:bcpc][:management][:vip] && get_nodes_for('powerdns').any?
   resolvers.push resolvers.shift
   resolvers.unshift node[:bcpc][:management][:vip]
 end
 
-bash "update resolvers" do
-  code <<-EOH
-  echo "#{(resolvers.map{|r| "nameserver #{r}"} + ["search #{node[:bcpc][:domain_name]}"]).join('\n')}" | resolvconf -a #{node[:bcpc][:networks][subnet][:management][:interface]}.inet
-  EOH
+bash 'update resolvers' do
+  code(
+    <<-EOH
+      echo "#{(resolvers.map { |r| "nameserver #{r}" } + ["search #{node[:bcpc][:domain_name]}"]).join('\n')}" | resolvconf -a #{node[:bcpc][:networks][subnet][:management][:interface]}.inet
+    EOH
+  )
 end
 
 ifaces = %w(management storage floating)
 ifaces.each_index do |i|
   iface = ifaces[i]
   device_name = node[:bcpc][:networks][subnet][iface][:interface]
-  next if iface != "management" and \
-      node[:bcpc][:networks][subnet][:management][:interface] == device_name
+  next if iface != 'management' &&
+    node[:bcpc][:networks][subnet][:management][:interface] == device_name
   template "/etc/network/interfaces.d/#{device_name}.cfg" do
-    source "network.iface.erb"
-    owner "root"
-    group "root"
+    source 'network.iface.erb'
+    owner 'root'
+    group 'root'
     mode 00644
     variables(
-      :interface => node[:bcpc][:networks][subnet][iface][:interface],
-      :ip => node[:bcpc][iface][:ip],
-      :netmask => node[:bcpc][:networks][subnet][iface][:netmask],
-      :gateway => node[:bcpc][:networks][subnet][iface][:gateway],
-      :slaves => node[:bcpc][:networks][subnet][iface].attribute?("slaves") ? node[:bcpc][:networks][subnet][iface]["slaves"] : false, 
-      :dns => resolvers,
-      :mtu => node[:bcpc][:networks][subnet][iface][:mtu],
-      :metric => i*100
-    )
+               interface: node[:bcpc][:networks][subnet][iface][:interface],
+               ip: node[:bcpc][iface][:ip],
+               netmask: node[:bcpc][:networks][subnet][iface][:netmask],
+               gateway: node[:bcpc][:networks][subnet][iface][:gateway],
+               slaves: node[:bcpc][:networks][subnet][iface].attribute?('slaves') ?
+                 node[:bcpc][:networks][subnet][iface]['slaves'] : false, 
+               dns: resolvers,
+               mtu: node[:bcpc][:networks][subnet][iface][:mtu],
+               metric: i*100
+             )
   end
-  
+
   bash "#{iface} up" do
     code <<-EOH
       ifup #{device_name} #{node[:bcpc][iface].attribute?("slaves") and node[:bcpc][iface][:slaves].join(" ")}
@@ -186,8 +202,8 @@ ifaces.each_index do |i|
   end
 end
 
-bash "interface-mgmt-make-static-if-dhcp" do
-  user "root"
+bash 'interface-mgmt-make-static-if-dhcp' do
+  user 'root'
   code <<-EOH
   sed --in-place '/\\(.*#{node[:bcpc][:networks][subnet][:management][:interface]}.*\\)/d' /etc/network/interfaces
   resolvconf -d #{node[:bcpc][:networks][subnet][:management][:interface]}.dhclient
@@ -197,35 +213,35 @@ bash "interface-mgmt-make-static-if-dhcp" do
 end
 
 if node[:bcpc][:networks][subnet][:management][:interface] != node[:bcpc][:networks][subnet][:storage][:interface]
-  bash "routing-storage" do
-      user "root"
-      code "echo '2 storage' >> /etc/iproute2/rt_tables"
-      not_if "grep -e '^2 storage' /etc/iproute2/rt_tables"
+  bash 'routing-storage' do
+    user 'root'
+    code "echo '2 storage' >> /etc/iproute2/rt_tables"
+    not_if "grep -e '^2 storage' /etc/iproute2/rt_tables"
   end
 end
-  
+
 if node[:bcpc][:networks][subnet][:management][:interface] != node[:bcpc][:networks][subnet][:storage][:interface] or \
-   node[:bcpc][:networks][subnet][:management][:interface] != node[:bcpc][:networks][subnet][:floating][:interface]
-  bash "routing-management" do
-      user "root"
-      code "echo '1 mgmt' >> /etc/iproute2/rt_tables"
-      not_if "grep -e '^1 mgmt' /etc/iproute2/rt_tables"
+    node[:bcpc][:networks][subnet][:management][:interface] != node[:bcpc][:networks][subnet][:floating][:interface]
+  bash 'routing-management' do
+    user 'root'
+    code "echo '1 mgmt' >> /etc/iproute2/rt_tables"
+    not_if "grep -e '^1 mgmt' /etc/iproute2/rt_tables"
   end
-  
-  template "/etc/network/if-up.d/bcpc-routing" do
-      mode 00775
-      source "bcpc-routing.erb"
-      notifies :run, "execute[run-routing-script-once]", :immediately
+
+  template '/etc/network/if-up.d/bcpc-routing' do
+    mode 00775
+    source 'bcpc-routing.erb'
+    notifies :run, 'execute[run-routing-script-once]', :immediately
   end
-  
-  execute "run-routing-script-once" do
-      action :nothing
-      command "/etc/network/if-up.d/bcpc-routing"
+
+  execute 'run-routing-script-once' do
+    action :nothing
+    command '/etc/network/if-up.d/bcpc-routing'
   end
 end
-  
-bash "disable-noninteractive-pam-logging" do
-    user "root"
-    code "sed --in-place 's/^\\(session\\s*required\\s*pam_unix.so\\)/#\\1/' /etc/pam.d/common-session-noninteractive"
-    only_if "grep -e '^session\\s*required\\s*pam_unix.so' /etc/pam.d/common-session-noninteractive"
+
+bash 'disable-noninteractive-pam-logging' do
+  user 'root'
+  code "sed --in-place 's/^\\(session\\s*required\\s*pam_unix.so\\)/#\\1/' /etc/pam.d/common-session-noninteractive"
+  only_if "grep -e '^session\\s*required\\s*pam_unix.so' /etc/pam.d/common-session-noninteractive"
 end
