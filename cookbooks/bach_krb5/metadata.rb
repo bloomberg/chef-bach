9959b47c3111a7762488fbc12a1a396ad85b98c6
Andrew Jones <andrew@jones.ec>
2016-10-07 18:17:32 -0400

    Update for chef-bach 2.0.0 release

--- cookbooks/bach_krb5/metadata.rb
+++ cookbooks/bach_krb5/metadata.rb
@@ -4,11 +4,11 @@ name             'bach_krb5'
 maintainer       'Bloomberg Finance L.P.'
 description      'Wrapper cookbook for krb5 community cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '2.0.0'
 
 depends 'krb5', '~> 2.0.0'
-depends 'bcpc', '= 0.1.0'
-depends 'bcpc-hadoop', '= 0.1.0'
+depends 'bcpc', '= 2.0.0'
+depends 'bcpc-hadoop', '= 2.0.0'
 
 %w(ubuntu).each do |os|
   supports os
