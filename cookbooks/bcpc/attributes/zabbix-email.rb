8283f6a31c5c6c2022d205315ce37a78ef88ee56
Lei Chen <lchen403@bloomberg.net>
2016-10-21 13:10:41 -0400

    add zabbix email support as a recipe bcpc::zabbix-email

---
+++ cookbooks/bcpc/attributes/zabbix-email.rb
@@ -0,0 +1,3 @@
+default["bcpc"]["hadoop"]["zabbix"]["mail_to_admin"] = "admin@example.com"
+default["bcpc"]["hadoop"]["zabbix"]["mail_to_tenant"] = "tenant@example.com"
+default["bcpc"]["hadoop"]["zabbix"]["mail_smtp_server_port"] = "smtp.example.com:25"
--- cookbooks/bcpc/attributes/zabbix.rb
+++ cookbooks/bcpc/attributes/zabbix.rb
@@ -3,7 +3,7 @@ default['bcpc']['zabbix']['group'] = "adm"
 default['bcpc']['zabbix']['server_port'] = 10051
 default['bcpc']['zabbix']['web_port'] = 7777
 default['bcpc']['zabbix']['scripts']['sender'] = "/usr/local/bin/run_zabbix_sender.sh"
-default['bcpc']['zabbix']['scripts']['mail'] = "/usr/local/bin/zbx_mail.sh"
+default['bcpc']['zabbix']['scripts']['mail'] = "/usr/local/bin/zbx_logger.sh"
 default['bcpc']['zabbix']['scripts']['query_graphite'] = "/usr/local/bin/query_graphite.py"
 # Interval (in seconds) during which we expect chef-client to have run at least once
 default['bcpc']['zabbix']['chef_client_check_interval'] = (node['chef_client']['interval'].to_i + node['chef_client']['splay'].to_i) * 2
