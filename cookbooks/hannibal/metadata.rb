15d2f3abf46fca3bcb115280decae5ed4f6613ce
Andrew Jones <andrew@jones.ec>
2016-11-03 12:53:07 -0400

    Update for chef-bach 2.0.2 release

--- cookbooks/hannibal/metadata.rb
+++ cookbooks/hannibal/metadata.rb
@@ -2,7 +2,7 @@ name             'hannibal'
 maintainer       'Bloomberg Finance L.P.'
 description      'Recipes to setup pre-requisites, build and install hannibal on cluster'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.1'
+version          '2.0.2'
 
 depends "ark"
 
--- cookbooks/kafka-bcpc/metadata.rb
+++ cookbooks/kafka-bcpc/metadata.rb
@@ -4,7 +4,7 @@ name             'kafka-bcpc'
 maintainer       'Bloomberg Finance L.P.'
 description      'Recipes to setup prerequisites for Kafka cluster'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.1'
+version          '2.0.2'
 
 depends "kafka"
 depends "sysctl"
