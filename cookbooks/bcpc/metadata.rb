f3c2ba17a9a9f3bbff4be91c8946ba5f3be9d84f
Salvatore LaMendola <slamendola2@bloomberg.net>
2016-12-08 16:22:52 -0500

    Update all cookbook versions to 2.1.0

--- cookbooks/bcpc/metadata.rb
+++ cookbooks/bcpc/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'compute@bloomberg.net'
 license          'Apache License 2.0'
 description      'Installs/Configures Bloomberg Clustered Private Cloud (BCPC)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '2.1.0'
 
 depends 'apt', '>= 2.4.0'
 depends 'bfd'
