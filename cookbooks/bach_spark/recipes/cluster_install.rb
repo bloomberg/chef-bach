233d8c6083c20e320bbbccb51f67cf82703ad17e
Lev Bronshtein <lbronshtein@bloomberg.net>
2017-01-03 17:59:39 -0500

    passes rubocop

--- cookbooks/bach_spark/recipes/cluster_install.rb
+++ cookbooks/bach_spark/recipes/cluster_install.rb
@@ -4,38 +4,47 @@ spark_pkg_version = node[:spark][:package][:version]
 spark_bin_dir = node[:spark][:bin][:dir]
 hdfs_url = node['spark']['hdfs_url']
 
-bash "create-hdfs-spark-history-dir" do
+bash 'create-hdfs-spark-history-dir' do
   code <<-EOH
   hdfs dfs -mkdir -p #{hdfs_url}/spark-history/
   hdfs dfs -chmod -R 1777 #{hdfs_url}/spark-history/
   hdfs dfs -chown -R hdfs:hdfs #{hdfs_url}/spark-history
   EOH
-  user "hdfs"
-  not_if "hdfs dfs -test -d #{hdfs_url}/spark-history", :user => 'hdfs'
+  user 'hdfs'
+  not_if "hdfs dfs -test -d #{hdfs_url}/spark-history", user: 'hdfs'
 end
 
 execute "create spark #{spark_pkg_version} HDFS directory" do
   command "hdfs dfs -mkdir -p #{hdfs_url}/apps/spark/#{spark_pkg_version}"
-  user "hdfs"
+  user 'hdfs'
 end
 
 execute "create spark #{spark_pkg_version} archive" do
-  command "tar -czvf /tmp/spark_#{spark_pkg_version}_jars.tgz -C #{spark_bin_dir}/jars ."
-  user "hdfs"
-  not_if "hdfs dfs -test -f #{hdfs_url}/apps/spark/#{spark_pkg_version}/spark_jars.tgz", :user => 'hdfs'
-  notifies :run, "execute[upload #{spark_pkg_version} archive to HDFS]", :immediately
+  command "tar -czvf /tmp/spark_#{spark_pkg_version}_jars.tgz "\
+    "-C #{spark_bin_dir}/jars ."
+  user 'hdfs'
+  not_if "hdfs dfs -test -f #{hdfs_url}/apps/spark"\
+    "/#{spark_pkg_version}/spark_jars.tgz", user: 'hdfs'
+  notifies :run,
+           "execute[upload #{spark_pkg_version} archive to HDFS]",
+           :immediately
 end
 
 execute "upload #{spark_pkg_version} archive to HDFS" do
-  command "hdfs dfs -copyFromLocal /tmp/spark_#{spark_pkg_version}_jars.tgz #{hdfs_url}/apps/spark/#{spark_pkg_version}/spark_jars.tgz"
-  user "hdfs"
+  command "hdfs dfs -copyFromLocal /tmp/spark_#{spark_pkg_version}_jars.tgz "\
+    "#{hdfs_url}/apps/spark/#{spark_pkg_version}/spark_jars.tgz"
+  user 'hdfs'
   action :nothing
-  not_if "hdfs dfs -test -f #{hdfs_url}/apps/spark/#{spark_pkg_version}/spark_jars.tgz", :user => 'hdfs'
-  notifies :run, "execute[cleanup /tmp/spark_#{spark_pkg_version}_jars.tgz]", :immediately
+  not_if 'hdfs dfs -test -f '\
+         "#{hdfs_url}/apps/spark/#{spark_pkg_version}/spark_jars.tgz",
+         user: 'hdfs'
+  notifies :run,
+           "execute[cleanup /tmp/spark_#{spark_pkg_version}_jars.tgz]",
+           :immediately
 end
 
-execute "cleanup /tmp/spark_#{spark_pkg_version}_jars.tgz" do 
+execute "cleanup /tmp/spark_#{spark_pkg_version}_jars.tgz" do
   command "rm /tmp/spark_#{spark_pkg_version}_jars.tgz"
-  user "hdfs"
+  user 'hdfs'
   action :nothing
 end
