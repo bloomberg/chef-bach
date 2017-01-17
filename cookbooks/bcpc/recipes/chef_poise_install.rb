cfcfa496a90275c015eb6bcce7e6b5a8f3016fe7
Clay Baenziger <cwb@clayb.net>
2017-01-17 23:27:07 +0000

    Oops missed bcpc::chef_poise_install

---
+++ cookbooks/bcpc/recipes/chef_poise_install.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: bach_reposity
+# Recipe:: poise install
+#
+# Copyright 2017, Bloomberg Finance L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+chef_gem 'poise' do
+  version '~>2.0'
+  options "--clear-sources -s #{get_binary_server_url}"
+  compile_time true
+end
+
+#
+# BACH typically runs chef-client with an abnormal umask, which causes
+# rubygems to install specifications with bad permissions.
+#
+# This ruby block restores the permissions to root/root, rw-r--r--
+#
+ruby_block 'fix-chef-gemspec-permissions' do
+  block do
+    Dir.glob(Gem.default_dir + '/specifications/*.gemspec').each do |path|
+      File.chown(0, 0, path)
+      File.chmod(0644, path)
+    end
+  end
+end.run_action(:create)
+
+Gem.clear_paths
