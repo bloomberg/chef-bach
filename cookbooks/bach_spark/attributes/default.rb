d3ab246706870ca115aa822f05a2e0a6ad280eea
Lev Bronshtein <lbronshtein@bloomberg.net>
2017-01-19 11:03:19 -0500

    Added additional environment variables

--- cookbooks/bach_spark/attributes/default.rb
+++ cookbooks/bach_spark/attributes/default.rb
@@ -53,3 +53,8 @@ default.bach_spark.environment.SPARK_CLASSPATH =
     '$SPARK_DIST_CLASSPATH:$SPARK_CLASSPATH'
 default.bach_spark.environment.SPARK_LOCAL_DIRS =
     '/home/$(whoami)/.spark_logs'
+default.bach_spark.environment.LD_LIBRARY_PATH = '/usr/hdp/current'\
+    '/hadoop-client/lib/native:/usr/hdp/current'\
+    '/hadoop-client/lib/native/Linux-amd64-64:$LD_LIBRARY_PATH'
+default.bach_spark.environment.HADOOP_CONF = '/usr/hadoop/conf'
+default.bach_spark.environment.HADOOP_HOME = "/usr/hdp/#{node[:bcpc][:hadoop][:distribution][:release}]/hadoop”
