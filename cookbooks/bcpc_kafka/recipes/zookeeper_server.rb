83b2d22e261337d5ff6af8c47221862a2c5eddb5
Andrew Jones <andrew@jones.ec>
2017-01-27 18:36:27 -0500

    Move zookeeper config recipe from role to recipe

--- cookbooks/bcpc_kafka/recipes/zookeeper_server.rb
+++ cookbooks/bcpc_kafka/recipes/zookeeper_server.rb
@@ -8,6 +8,7 @@ include_recipe 'bcpc_kafka::default'
 # For the time being, this will have to be force_override.
 node.force_override[:bcpc][:hadoop][:kerberos][:enable] = false
 
+include_recipe 'bcpc-hadoop::zookeeper_config'
 include_recipe 'bcpc-hadoop::zookeeper_server'
 
 include_recipe 'bcpc::diamond'
