75934836bfa34052585c000f1457741a6f2abb88
Andrew Jones <andrew@jones.ec>
2017-02-15 15:30:43 -0500

    Finish moving bcpc-hadoop::graphite_disk_wrapper to converge-time

--- cookbooks/bcpc-hadoop/recipes/graphite_disk_wrapper.rb
+++ cookbooks/bcpc-hadoop/recipes/graphite_disk_wrapper.rb
@@ -7,17 +7,22 @@ ruby_block 'graphite-directory-structure' do
 
     if reservation_requests.include?("graphite_disk") then
       disk_index = reservation_requests.index("graphite_disk")
+      graphite_dir = "/disk/#{disk_index}/graphite_disk"
 
-      Chef::Resource::Directory.new("graphite-#{disk_index}").tap do |dd|
-        dd.path "/disk/#{disk_index}/graphite_disk"
+      Chef::Resource::Directory.new("graphite-#{disk_index}",
+                                    run_context).tap do |dd|
+        dd.path graphite_dir
         dd.owner 'root'
         dd.group 'root'
         dd.recursive false
+        dd.run_action(:create)
       end
 
-      Chef::Resource::Link.new(node[:bcpc][:graphite][:install_dir]).tap do |ll|
-        ll.to "/disk/#{disk_index}/graphite_disk"
+      Chef::Resource::Link.new(node[:bcpc][:graphite][:install_dir],
+                               run_context).tap do |ll|
+        ll.to graphite_dir
         ll.link_type :symbolic
+        ll.run_action(:create)
       end
     end
   end
