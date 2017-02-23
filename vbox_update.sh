124823cb0aac382a5ea73d33bf27b83665fa4d20
Clay Baenziger <cbaenziger@bloomberg.net>
2017-02-22 21:10:26 -0500

    Remove old, un-maintained scripts; and maintain one that could be useful

--- vbox_update.sh
+++ vbox_update.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-vagrant ssh -c "rsync -avP --exclude vbox --exclude .chef /chef-bcpc-host/ /home/vagrant/chef-bcpc/"
-vagrant ssh -c "cd chef-bcpc && knife environment from file environments/*.json && knife role from file roles/*.json && knife role from file roles/*.rb && knife cookbook upload -a -o cookbooks"
+vagrant ssh -c "rsync -avP --exclude vbox --exclude .chef /chef-bcpc-host/ " \
+               "/home/vagrant/chef-bcpc/"
+vagrant ssh -c "cd chef-bcpc && source proxy_setup.sh && " \
+               "/opt/chefdk/embedded/bin/berks vendor vendor/cookbooks"
+vagrant ssh -c "cd chef-bcpc && " \
+               "knife environment from file environments/*.json && " \
+               "sudo knife role from file roles/*.json -u admin -k /etc/chef-server/admin.pem; " \
+               "r=\$? && " \
+               "sudo knife role from file roles/*.rb -u admin -k /etc/chef-server/admin.pem; " \
+               "r=\$((r & \$? )) && [[ \$r -lt 1 ]] && " \
+               "knife cookbook upload -a -o cookbooks"
