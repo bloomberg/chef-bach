d78420796e7312760ac73945f7215fd9e7a2a7be
Clay Baenziger <cbaenziger@bloomberg.net>
2017-03-02 19:47:13 -0500

    Use authconfig force flag to prevent debconf error from poping up about changed PAM files

--- cookbooks/bach_krb5/attributes/default.rb
+++ cookbooks/bach_krb5/attributes/default.rb
@@ -9,6 +9,7 @@ node.override[:krb5][:krb5_conf][:realms][:default_realm_kdcs] = [ "#{node[:bcpc
 node.override[:krb5][:admin_principal] = "root/admin"
 node.override[:krb5][:krb5_conf][:libdefaults][:proxiable] = true
 node.override[:krb5][:krb5_conf][:libdefaults][:renew_lifetime] = "7d"
+node.override[:krb5][:client][:authconfig] = 'pam-auth-update --force --package krb5'
 default_realm = node[:bcpc][:domain_name].upcase
 node.override[:krb5][:kdc_conf][:realms][default_realm][:supported_enctypes] = "arcfour-hmac:normal des3-hmac-sha1:normal des-cbc-crc:normal des:normal des:v4 des:norealm des:onlyrealm des:afs3"
 node.override[:krb5][:kdc_conf][:realms][default_realm][:default_principal_flags] = "+renewable"
