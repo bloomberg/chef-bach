d80c4bc65c49b1e4b1dd04a995ea82b8e22397da
Clay Baenziger <cwb@clayb.net>
2017-03-29 13:48:33 +0000

    Start checking if machines booted

--- wait_for_hosts.rb
+++ wait_for_hosts.rb
@@ -51,15 +51,49 @@ def parsed_apache_log
   end
 end
 
+#
+# Returns:
+#  - Apache log entries matching booting host URLs
+#
+def booted_hosts
+# Example log entries
+# 10.0.100.11 - - [29/Mar/2017:04:04:28 +0000] "GET /cblr/svc/op/gpxe/system/bcpc-vm1 HTTP/1.1" 200 753 "-" "iPXE/1.0.0+"
+# 10.0.100.11 - - [29/Mar/2017:04:04:28 +0000] "GET /cobbler/images/ubuntu-14.04-mini-x86_64/linux HTTP/1.1" 200 6939425 "-" "iPXE/1.0.0+"
+# 10.0.100.12 - - [29/Mar/2017:04:04:28 +0000] "GET /cblr/svc/op/gpxe/system/bcpc-vm2 HTTP/1.1" 200 753 "-" "iPXE/1.0.0+"
+# 10.0.100.12 - - [29/Mar/2017:04:04:29 +0000] "GET /cobbler/images/ubuntu-14.04-mini-x86_64/linux HTTP/1.1" 200 6939425 "-" "iPXE/1.0.0+"
+# 10.0.100.13 - - [29/Mar/2017:04:04:29 +0000] "GET /cblr/svc/op/gpxe/system/bcpc-vm3 HTTP/1.1" 200 753 "-" "iPXE/1.0.0+"
+# 10.0.100.13 - - [29/Mar/2017:04:04:29 +0000] "GET /cobbler/images/ubuntu-14.04-mini-x86_64/linux HTTP/1.1" 200 6939425 "-" "iPXE/1.0.0+"
+# 10.0.100.11 - - [29/Mar/2017:04:04:28 +0000] "GET /cobbler/images/ubuntu-14.04-mini-x86_64/initrd.gz HTTP/1.1" 200 41147799 "-" "iPXE/1.0.0+"
+# 10.0.100.12 - - [29/Mar/2017:04:04:29 +0000] "GET /cobbler/images/ubuntu-14.04-mini-x86_64/initrd.gz HTTP/1.1" 200 41147799 "-" "iPXE/1.0.0+"
+# 10.0.100.13 - - [29/Mar/2017:04:04:30 +0000] "GET /cobbler/images/ubuntu-14.04-mini-x86_64/initrd.gz HTTP/1.1" 200 41147799 "-" "iPXE/1.0.0+"
+  gpxe_url = Regexp.new('/cblr/svc/op/gpxe/system/')
+  ubuntu_url = Regexp.new('/cobbler/images/ubuntu-.*-x86_64/linux')
+  initrd_url = Regexp.new('/cobbler/images/ubuntu-.*-x86_64/initrd.gz')
+  match_strings = [gpxe_url, ubuntu_url, initrd_url]
+
+  boot_log_entries = parsed_apache_log.select do |entry|
+    entry[:request].select { |e| match_strings.any? { |x| x.match(e) } }
+  end
+
+# parse host name here?
+  @request_regex ||=
+    Regexp.new('/cblr/svc/op/nopxe/system/(.*?)\s')
+
+  boot_log_entries.map do |entry|
+    md = @request_regex.match(entry[:request])
+    md.nil? ? nil : md[1]
+  end
+end
+
 def installed_hosts
-  pxe_log_entries = parsed_apache_log.select do |entry|
+  cobbler_nopxe_log_entries = parsed_apache_log.select do |entry|
     entry[:request].include?('/cblr/svc/op/nopxe/system/')
   end
 
   @request_regex ||=
     Regexp.new('/cblr/svc/op/nopxe/system/(.*?)\s')
 
-  pxe_log_entries.map do |entry|
+  cobbler_nopxe_log_entries.map do |entry|
     md = @request_regex.match(entry[:request])
     md.nil? ? nil : md[1]
   end
