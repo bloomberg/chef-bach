05bd466f2fdd0603bcf4563ad1bea2353ae5ae1a
Andrew Jones <andrew@jones.ec>
2017-04-24 16:33:37 -0400

    Update all cookbook versions for chef-bach 2.4.0 release

--- cookbooks/bach_spark/metadata.rb
+++ cookbooks/bach_spark/metadata.rb
@@ -4,11 +4,11 @@ name             'bach_spark'
 maintainer       'Bloomberg Finance L.P.'
 description      'Cookbook to setup Apache Spark'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '2.4.0'
 
 %w(ubuntu).each do |os|
   supports os
 end
 
-depends 'bcpc', '= 0.1.0'
-depends 'bcpc-hadoop', '= 0.1.0'
+depends 'bcpc', '= 2.4.0'
+depends 'bcpc-hadoop', '= 2.4.0'
--- cookbooks/bcpc-hadoop/metadata.rb
+++ cookbooks/bcpc-hadoop/metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'compute@bloomberg.net'
 license          'Apache License 2.0'
 description      'Installs/Configures Bloomberg Clustered Private Hadoop Cloud (BCPHC)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '2.4.0'
 
-depends 'bcpc', '= 0.1.0'
-depends 'bach_krb5', '= 0.1.0'
+depends 'bcpc', '= 2.4.0'
+depends 'bach_krb5', '= 2.4.0'
 depends 'database'
 depends 'java', '>= 1.28.0'
 depends 'maven', '~> 2.1.1'
