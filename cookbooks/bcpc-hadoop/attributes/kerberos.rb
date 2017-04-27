a2a6cfe25256b4c88eb23a7914927ec38a2d00b9
Amit Anand <aanand59@bloomberg.net>
2017-04-27 09:19:59 -0400

    Add group for keytabs and grant read permissions to it

--- cookbooks/bcpc-hadoop/attributes/kerberos.rb
+++ cookbooks/bcpc-hadoop/attributes/kerberos.rb
@@ -6,104 +6,117 @@ default['bcpc']['hadoop']['kerberos']['data'] = {
     principal: 'hdfs',
     keytab: 'hdfs.service.keytab',
     owner: 'hdfs',
+    group: 'hadoop',
     princhost: '_HOST',
-    perms: '0600',
+    perms: '0440',
     spnego_keytab: 'hdfs.service.keytab'
   },
   datanode: {
     principal: 'hdfs',
     keytab: 'hdfs.service.keytab',
     owner: 'hdfs',
+    group: 'hadoop',
     princhost: '_HOST',
-    perms: '0600',
+    perms: '0440',
     spnego_keytab: 'hdfs.service.keytab'
   },
   journalnode: {
     principal: 'hdfs',
     keytab: 'hdfs.service.keytab',
     owner: 'hdfs',
+    group: 'hadoop',
     princhost: '_HOST',
-    perms: '0600',
+    perms: '0440',
     spnego_keytab: 'hdfs.service.keytab'
   },
   resourcemanager: {
     principal: 'yarn',
     keytab: 'yarn.service.keytab',
     owner: 'yarn',
+    group: 'hadoop',
     princhost: '_HOST',
-    perms: '0600',
+    perms: '0440',
     spnego_keytab: 'yarn.service.keytab'
   },
   nodemanager: {
     principal: 'yarn',
     keytab: 'yarn.service.keytab',
     owner: 'yarn',
+    group: 'hadoop',
     princhost: '_HOST',
-    perms: '0600',
+    perms: '0440',
     spnego_keytab: 'yarn.service.keytab'
   },
   historyserver: {
     principal: 'mapred',
     keytab: 'mapred.service.keytab',
     owner: 'mapred',
+    group: 'hadoop',
     princhost: '_HOST',
-    perms: '0600',
+    perms: '0440',
     spnego_keytab: 'mapred.service.keytab'
   },
   spnego: {
     principal: 'HTTP',
     keytab: 'spnego.service.keytab',
     owner: 'hdfs',
+    group: 'hadoop',
     princhost: '_HOST',
-    perms: '0600',
+    perms: '0440',
     spnego_keytab: 'spnego.service.keytab'
   },
   zookeeper: {
     principal: 'zookeeper',
     keytab: 'zookeeper.service.keytab',
     owner: 'zookeeper',
+    group: 'hadoop',
     princhost: '_HOST',
-    perms: '0600',
+    perms: '0440',
     spnego_keytab: 'zookeeper.service.keytab'
   },
   hbase: {
     principal: 'hbase',
     keytab: 'hbase.service.keytab',
     owner: 'hbase',
+    group: 'hadoop',
     princhost: '_HOST',
-    perms: '0600',
+    perms: '0440',
     spnego_keytab: 'hbase.service.keytab'
   },
   httpfs: {
     principal: 'httpfs',
     keytab: 'httpfs.service.keytab',
     owner: 'httpfs',
+    group: 'hadoop',
     princhost: '_HOST',
-    perms: '0600',
+    perms: '0440',
     spnego_keytab: 'httpfs.service.keytab'
   },
   hive: {
     principal: 'hive',
     keytab: 'hive.service.keytab',
     owner: 'hive',
+    group: 'hadoop',
     princhost: '_HOST',
-    perms: '0600',
+    perms: '0440',
     spnego_keytab: 'hive.service.keytab'
   },
   oozie: {
     principal: 'oozie',
     keytab: 'oozie.service.keytab',
     owner: 'oozie',
+    group: 'hadoop',
     princhost: '_HOST',
-    perms: '0600',
+    perms: '0440',
     spnego_keytab: 'oozie.service.keytab'
   },
   flume: {
     principal: 'flume',
     keytab: 'flume.service.keytab',
     owner: 'flume',
+    group: 'hadoop',
     princhost: '_HOST',
-    perms: '0600',
+    perms: '0440',
     spnego_keytab: 'flume.service.keytab'
   }
 }
