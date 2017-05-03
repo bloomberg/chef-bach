dd368b1d72a1168c4429da62240ccd9dd7cae8ac
Andrew Jones <andrew@jones.ec>
2017-05-03 17:05:07 -0400

    Update all cookbook versions for chef-bach 2.4.1 release

--- cookbooks/hannibal/metadata.rb
+++ cookbooks/hannibal/metadata.rb
@@ -2,7 +2,7 @@ name             'hannibal'
 maintainer       'Bloomberg Finance L.P.'
 description      'Recipes to setup pre-requisites, build and install hannibal on cluster'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.4.0'
+version          '2.4.1'
 
 depends "ark"
 
