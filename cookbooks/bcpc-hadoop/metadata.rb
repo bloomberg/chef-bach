0ae416ea6a4057c6ba5671d35fdc7b24075312a1
Allan Espinosa <aespinosa33@bloomberg.net>
2017-08-04 14:42:11 -0400

    Cut as release 3.0.4

--- cookbooks/bcpc-hadoop/metadata.rb
+++ cookbooks/bcpc-hadoop/metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'hadoop@bloomberg.net'
 license          'Apache License 2.0'
 description      'Installs/Configures Bloomberg Clustered Private Hadoop Cloud (BCPHC)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.3'
+version          '3.0.4'
 
-depends 'bcpc', '= 3.0.3'
-depends 'bach_krb5', '= 3.0.3'
+depends 'bcpc', '= 3.0.4'
+depends 'bach_krb5', '= 3.0.4'
 depends 'database'
 depends 'java'
 depends 'maven'
