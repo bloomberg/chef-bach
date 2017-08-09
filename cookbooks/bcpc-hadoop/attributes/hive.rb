f453007a6752620250bc4a25dce68ea1e9c5644f
Allan Espinosa <aespinosa33@bloomberg.net>
2017-08-09 16:57:41 -0400

    Set hive.warehouse.subdir.inherit.perms to false

--- cookbooks/bcpc-hadoop/attributes/hive.rb
+++ cookbooks/bcpc-hadoop/attributes/hive.rb
@@ -32,7 +32,7 @@ default[:bcpc][:hadoop][:hive][:site_xml].tap do |site_xml|
   site_xml['hive.stats.dbclass'] = 'fs'
   site_xml['hive.stats.jdbcdriver'] = 'com.mysql.jdbc.Driver'
   site_xml['hive.support.concurrency'] = true
-  site_xml['hive.warehouse.subdir.inherit.perms'] = true
+  site_xml['hive.warehouse.subdir.inherit.perms'] = false
   site_xml['hive.cluster.delegation.token.store.class'] =
     'org.apache.hadoop.hive.thrift.ZooKeeperTokenStore'
   site_xml['hive.cluster.delegation.token.store.zookeeper.connectString'] =
