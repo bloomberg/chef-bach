1794fed49ed848580234db5ee3b1b5d2f38ed5d8
Clay Baenziger <cbaenziger@bloomberg.net>
2017-08-23 11:57:21 -0400

    Install libaugeas and ensure PKG_CONFIG_PATH is set correctly

--- vm-to-cluster.sh
+++ vm-to-cluster.sh
@@ -9,6 +9,14 @@ export PATH=/opt/chefdk/embedded/bin:/usr/bin:/bin
 
 REPO_DIR="`dirname ${BASH_SOURCE[0]}`"
 cd $REPO_DIR
+. ./proxy_setup.sh
+
+if [ $(dpkg-query -W -f='${Status}' libaugeas-dev 2>/dev/null | grep -c 'ok installed') -ne 1 ] && [ "$(uname)" != "Darwin" ]; then
+  echo "#### Need libaugeas-dev for the Augeas Gem" > /dev/stderr
+  sudo apt-get install -y libaugeas-dev
+fi
+
+export PKG_CONFIG_PATH=/usr/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/share/pkgconfig
 bundle config --local PATH vendor/bundle
 bundle config --local DISABLE_SHARED_GEMS true
 bundle package --path vendor/bundle > /dev/null
