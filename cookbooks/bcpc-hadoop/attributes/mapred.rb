a766b4350f60fe7764bc7d6779620df85419e19b
Allan Espinosa <aespinosa33@bloomberg.net>
2017-09-08 10:24:51 -0400

    Add the missing .jar extension

--- cookbooks/bcpc-hadoop/attributes/mapred.rb
+++ cookbooks/bcpc-hadoop/attributes/mapred.rb
@@ -43,7 +43,7 @@ default[:bcpc][:hadoop][:mapreduce][:site_xml].tap do |site_xml|
      '$PWD/mr-framework/hadoop/share/hadoop/hdfs/*',
      '$PWD/mr-framework/hadoop/share/hadoop/hdfs/lib/*',
      "#{hdp_path}/hadoop/lib/hadoop-lzo-0.6.0." +
-       "${hdp.version}",
+       "${hdp.version}.jar",
      "#{hdp_path}/phoenix/phoenix-server.jar",
      '$HADOOP_CONF_DIR',
     ].join(',')
