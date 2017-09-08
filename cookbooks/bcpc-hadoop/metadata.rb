75c7298b44c96a4981bc1a3a21200de0e3f18788
Allan Espinosa <aespinosa33@bloomberg.net>
2017-09-08 10:26:49 -0400

    Cut 3.0.24 for the missing .jar extension

--- cookbooks/bcpc-hadoop/metadata.rb
+++ cookbooks/bcpc-hadoop/metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'hadoop@bloomberg.net'
 license          'Apache License 2.0'
 description      'Installs/Configures Bloomberg Clustered Private Hadoop Cloud (BCPHC)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.23'
+version          '3.0.24'
 
-depends 'bcpc', '= 3.0.23'
-depends 'bach_krb5', '= 3.0.23'
+depends 'bcpc', '= 3.0.24'
+depends 'bach_krb5', '= 3.0.24'
 depends 'database'
 depends 'java'
 depends 'maven'
