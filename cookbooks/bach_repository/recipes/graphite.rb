ae8ea0b0c54d1e3ca7875778563974e071efe004
Lei Chen <lchen403@bloomberg.net>
2017-09-25 14:34:08 -0400

    upgrade Graphite to 1.0.2

--- cookbooks/bach_repository/recipes/graphite.rb
+++ cookbooks/bach_repository/recipes/graphite.rb
@@ -21,45 +21,66 @@ fpm_path = node['bach']['repository']['fpm_bin']
 epoch = Time.now.strftime('%s')
 
 [
- {
-  name: 'pytz',
-  version: '2015.6',
-  url: 'https://pypi.python.org/packages/source/p/pytz/pytz-2015.6.zip',
-  checksum: '2b3b20919afcf06f90a4e58aa2a2c82a601b71dd5a352af36478e5337d4a16cd'
- },
- {
-  name: 'django',
-  version: '1.5.4',
-  url: 'https://pypi.python.org/packages/source/D/Django/Django-1.5.4.tar.gz',
-  checksum: '428defe3fd515dfc8613039bb0a80622a13fb4b988c5be48db07ec098ea1704e'
- },
- {
-  name: 'pyparsing',
-  version: '2.0.6',
-  url: 'https://pypi.python.org/packages/source/p/pyparsing/pyparsing-2.0.6.zip',
-  checksum: '697f04b1b5d01741f4f7b7267ff2f2cc845c336e65efa71609339a250a0e9c72'
- },
- {
-  name: 'carbon',
-  version: '0.10.0-rc1+git20161102',
-  url: 'https://github.com/graphite-project/carbon/archive/' \
-    '0a6944d144955dc398f722384bce2e42ed348f61.zip',
-  checksum: 'a123e26f36dfe3685e764fd1ea6b87465101f3b5778856744b133a00d771a6b0'
- },
- {
-  name: 'whisper',
-  version: '0.10.0-rc1+git20161102',
-  url: 'https://github.com/graphite-project/whisper/archive/' \
-    '0ff96849376fa6943214f6634be7225f2f44cd2e.zip',
-  checksum: 'bfadd161981f36a0ef218ce9021035d85480511f9d4e75db88beac6ab03d9f94'
- },
- {
-  name: 'graphite-web',
-  version: '0.10.0-rc1+git20161102',
-  url: 'https://github.com/graphite-project/graphite-web/archive/' \
-    '8b142c8d9130857b6d47981e0e92930984a250e1.zip',
-  checksum: 'ed6134ce114f243db47ca3006ac5a2df7fa6d3d231ab072a82d0707220e08af2'
- }
+  {
+    name: 'django',
+    version: '1.8.18',
+    url: 'https://github.com/django/django/archive/1.8.18.tar.gz',
+    checksum: 'ce9e3111855b37e3b62a253836d7a398260f50c4942cbb9826f17fd44794cba6'
+  },
+  {
+    name: 'django-tagging',
+    version: '0.4.3',
+    url: 'https://github.com/Fantomas42/django-tagging/archive/v0.4.3.tar.gz',
+    checksum: '509cf96817e4d43cec5253995f63756974a08cb4bed7d2f4358849bef042c310'
+  },
+  {
+    name: 'pytz',
+    version: '2015.6',
+    url: 'https://pypi.python.org/packages/source/p/pytz/pytz-2015.6.zip',
+    checksum: '2b3b20919afcf06f90a4e58aa2a2c82a601b71dd5a352af36478e5337d4a16cd'
+  },
+  {
+    name: 'scandir',
+    version: '1.5',
+    url: 'https://github.com/benhoyt/scandir/archive/v1.5.tar.gz',
+    checksum: '3b2be74e0be665d22adaf841d1cadab7ed4aa2001a9bb81452e0f498cd0780d8'
+  },
+  {
+    name: 'pycparser',
+    version: '2.18',
+    url: 'https://github.com/eliben/pycparser/archive/release_v2.18.tar.gz',
+    checksum: 'd5ead0f43ac5a8dd89e8475ada557037bbeb7ed709491861e84356ef17a3f8ac'
+  },
+  {
+   name: 'pyparsing',
+   version: '2.0.6',
+   url: 'https://pypi.python.org/packages/source/p/pyparsing/pyparsing-2.0.6.zip',
+   checksum: '697f04b1b5d01741f4f7b7267ff2f2cc845c336e65efa71609339a250a0e9c72'
+  },
+  {
+    name: 'txamqp',
+    version: '0.7.0',
+    url: 'https://github.com/txamqp/txamqp/archive/0.7.0.tar.gz',
+    checksum: '8ba99b861270c56fa0e574ef1c9a1eb86b72464470617976fcd2669af0e6b23e'
+  },
+  {
+    name: 'carbon',
+    version: '1.0.2',
+    url: 'https://github.com/graphite-project/carbon/archive/1.0.2.zip',
+    checksum: 'c93b18051403c0fd19b805fc0a185ca82623187e9e1a6ca6c4e84c9c5239e404'
+  },
+  {
+    name: 'whisper',
+    version: '1.0.2',
+    url: 'https://github.com/graphite-project/whisper/archive/1.0.2.zip',
+    checksum: 'e8d27c236a064d532d6fe921be89dd63e6166cdc8e31cc8ea85b692b750d20d4'
+  },
+  {
+    name: 'graphite-web',
+    version: '1.0.2',
+    url: 'https://github.com/graphite-project/graphite-web/archive/1.0.2.zip',
+    checksum: 'a72ade2cb0ee261ac8dbbdd5f432b835606c9ddd13948659f7977feb4b13b244'
+  }
 ].each do |package|
 
   deb_name = "python-#{package[:name]}_#{package[:version]}_all.deb"
