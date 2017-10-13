29211422139a7c8cd9df030e86f7c18dfd149e81
Allan Espinosa <aespinosa33@bloomberg.net>
2017-10-13 12:51:33 -0400

    Cut 3.0.31 detailng JobHistoryServer ACLs

--- cookbooks/bcpc-hadoop/metadata.rb
+++ cookbooks/bcpc-hadoop/metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'hadoop@bloomberg.net'
 license          'Apache License 2.0'
 description      'Installs/Configures Bloomberg Clustered Private Hadoop Cloud (BCPHC)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.30'
+version          '3.0.31'
 
-depends 'bcpc', '= 3.0.30'
-depends 'bach_krb5', '= 3.0.30'
+depends 'bcpc', '= 3.0.31'
+depends 'bach_krb5', '= 3.0.31'
 depends 'database'
 depends 'java'
 depends 'maven'
--- cookbooks/bcpc_kafka/metadata.rb
+++ cookbooks/bcpc_kafka/metadata.rb
@@ -5,7 +5,7 @@ maintainer       'Bloomberg Finance L.P.'
 maintainer_email 'hadoop@bloomberg.net'
 description      'Recipes to setup prerequisites for Kafka cluster'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.30'
+version          '3.0.31'
 
 depends 'bcpc'
 depends 'bcpc-hadoop'
