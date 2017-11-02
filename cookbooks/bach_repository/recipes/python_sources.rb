fa7ef31fd97c9cf2d3b4f80c8caafaa7b9d26dbc
Vinesh Chemmala Paul <vpaul@hortonworks.com>
2017-11-02 14:32:00 +0530

    Fix pypi access issue with http. Changing the URL to https

--- cookbooks/bach_repository/recipes/python_sources.rb
+++ cookbooks/bach_repository/recipes/python_sources.rb
@@ -13,7 +13,7 @@ directory "#{bins_dir}/python" do
 end
 
 remote_file "#{bins_dir}/python/pyrabbit-1.0.1.tar.gz" do
-  source 'http://pypi.python.org/packages/source/p/pyrabbit/pyrabbit-1.0.1.tar.gz'
+  source 'https://pypi.python.org/packages/source/p/pyrabbit/pyrabbit-1.0.1.tar.gz'
   mode 0444
   checksum '7bc2b89fb332f62f4aaa891b1a4c8bfa38b32d154d44403afc83f00755528bdc'
 end
