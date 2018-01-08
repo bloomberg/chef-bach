2e82437f26324c39d1675a096ed68fee16254314
Lev Bronshtein <lbronshtein@bloomberg.net>
2018-01-08 15:31:57 -0500

    should be slider-client

--- cookbooks/bcpc-hadoop/recipes/slider.rb
+++ cookbooks/bcpc-hadoop/recipes/slider.rb
@@ -1,3 +1,5 @@
+# vim: tabstop=2:shiftwidth=2:softtabstop=2
+
 ::Chef::Recipe.send(:include, Bcpc_Hadoop::Helper)
 
 [ hwx_pkg_str("slider", node[:bcpc][:hadoop][:distribution][:release]) ].each do |pkg|
@@ -6,4 +8,16 @@
   end
 end
 
-hdp_select('slider', node[:bcpc][:hadoop][:distribution][:active_release])
+hdp_select('slider-client', node[:bcpc][:hadoop][:distribution][:active_release])
+
+node.default['bcpc']['hadoop']['slider']['env'] = {}
+node.default['bcpc']['hadoop']['sliser']['env'].tap do |slider_env|
+  slider_env['JAVA_HOME'] = node[:bcpc][:hadoop][:java]
+  slider_env['HADOOP_CONF_DIR'] = '/etc/hadoop/conf'
+end
+
+template '/etc/slider/conf/slider-env.sh' do
+  source 'generic_env.sh.erb'
+  mode 0o0555
+  variables(options: node['bcpc']['hadoop']['slider']['env'])
+end
