25f8499802cc19f07871db48945b287dd68ea3d5
Lev Bronshtein <lbronshtein@bloomberg.net>
2018-01-11 17:35:23 -0500

    service not service, parametrized locla user creation

--- cookbooks/bcpc-hadoop/recipes/phoenix_query_server.rb
+++ cookbooks/bcpc-hadoop/recipes/phoenix_query_server.rb
@@ -1,19 +1,6 @@
 # vim: tabstop=2:shiftwidth=2:softtabstop=2
 
-node.default['bcpc']['hadoop']['kerberos']['data'] = \
-  node['bcpc']['hadoop']['kerberos']['data'].to_h.update(
-    {
-      phoenixqs: {
-        principal: 'phoenixqs',
-        keytab: 'phoenixqs.service.keytab',
-        owner: 'phoenixqs',
-        group: 'phoenixqs',
-        princhost: '_HOST',
-        perms: '0440',
-        spnego_keytab: 'phoenixqs.service.keytab'
-      }
-    }
-)
+include_recipe 'bcpc-hadoop::phoenixqs_kerberos'
 
 site_xml = node.default[:bcpc][:hadoop][:hbase][:site_xml]
 generated_values = {}
@@ -29,12 +16,25 @@ generated_values['phoenix.queryserver.serialization'] = 'JSON'
 
 site_xml.merge!(generated_values)
 
-cookbook_file '/etc/init.d/pqs' do
-  source 'etc_initd_pqs.sh'
+qs_runas = node['bcpc']['hadoop']['phoenix']['phoenixqs']['username']
+
+user qs_runas do
+  comment 'Runs phoenix queryserver'
+  only_if { node['bcpc']['hadoop']['phoenix']['phoenixqs']['localuser'] }
+end
+
+group qs_runas do
+  members [ qs_runas ] 
+  only_if { node['bcpc']['hadoop']['phoenix']['phoenixqs']['localuser'] }
+end
+
+template '/etc/init.d/pqs' do
+  source 'etc_initd_pqs.erb'
+  variables (qs_runas: qs_runas) 
   mode 0o755
   notifies :restart, 'service[pqs]', :delayed
 end
 
-serivce 'pqs' do
+service 'pqs' do
   action [:enable, :start]
 end
