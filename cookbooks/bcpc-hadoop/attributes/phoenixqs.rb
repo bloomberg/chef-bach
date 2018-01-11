6a71d957ce00608ab97d6e3dd5a0e41390d10678
Lev Bronshtein <lbronshtein@bloomberg.net>
2018-01-11 17:28:24 -0500

    attributes

---
+++ cookbooks/bcpc-hadoop/attributes/phoenixqs.rb
@@ -0,0 +1,3 @@
+default['bcpc']['hadoop']['phoenix']['phoenixqs']['username'] = 'phoenixqs'
+default['bcpc']['hadoop']['phoenix']['phoenixqs']['principal'] = ''
+default['bcpc']['hadoop']['phoenix']['phoenixqs']['localuser'] = true
