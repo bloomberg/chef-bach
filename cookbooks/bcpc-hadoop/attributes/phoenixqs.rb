139bd3a3db99e4a7498a76a54053cc22dd725498
Lev Bronshtein <lbronshtein@bloomberg.net>
2018-01-11 18:35:48 -0500

    defined principal

--- cookbooks/bcpc-hadoop/attributes/phoenixqs.rb
+++ cookbooks/bcpc-hadoop/attributes/phoenixqs.rb
@@ -1,3 +1,4 @@
 default['bcpc']['hadoop']['phoenix']['phoenixqs']['username'] = 'phoenixqs'
-default['bcpc']['hadoop']['phoenix']['phoenixqs']['principal'] = ''
+default['bcpc']['hadoop']['phoenix']['phoenixqs']['principal'] =\
+  "HTTP/#{node['fqdn']}@#{node[:bcpc][:hadoop][:kerberos][:realm]}" 
 default['bcpc']['hadoop']['phoenix']['phoenixqs']['localuser'] = true
