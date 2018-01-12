0894ed38fd1cedaa7d72ff06a9b70773e173b150
Lev Bronshtein <lbronshtein@bloomberg.net>
2018-01-12 12:19:14 -0500

    syntax fixes

--- cookbooks/bcpc-hadoop/recipes/phoenix_query_server.rb
+++ cookbooks/bcpc-hadoop/recipes/phoenix_query_server.rb
@@ -5,7 +5,7 @@ include_recipe 'bcpc-hadoop::phoenixqs_kerberos'
 site_xml = node.default[:bcpc][:hadoop][:hbase][:site_xml]
 generated_values = {}
 generated_values['phoenix.queryserver.kerberos.principal'] =\
-  node['bcpc']['hadoop']['phoenix']['phoenixqs']['principal'] =\
+  node['bcpc']['hadoop']['phoenix']['phoenixqs']['principal'] 
 generated_values['phoenix.queryserver.keytab.file'] =\
   "#{node[:bcpc][:hadoop][:kerberos][:keytab][:dir]}/" +
   node[:bcpc][:hadoop][:kerberos][:data][:phoenixqs][:keytab]
@@ -28,7 +28,7 @@ end
 
 template '/etc/init.d/pqs' do
   source 'etc_initd_pqs.erb'
-  variables (qs_runas: qs_runas) 
+  variables(qs_runas: qs_runas) 
   mode 0o755
   notifies :restart, 'service[pqs]', :delayed
 end
