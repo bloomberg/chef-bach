2b9e680f57cc5e6124b9b1589cd375a2ea9da168
Lev Bronshtein <lbronshtein@bloomberg.net>
2018-01-12 09:39:31 -0500

    additional settings moved to attributes

--- cookbooks/bcpc-hadoop/attributes/phoenixqs.rb
+++ cookbooks/bcpc-hadoop/attributes/phoenixqs.rb
@@ -1,4 +1,7 @@
 default['bcpc']['hadoop']['phoenix']['phoenixqs']['username'] = 'phoenixqs'
+# For UGI to be able to work properly, we need to specify float host 
+# not _HOST, especially since we do not provide keytabs for non floats
 default['bcpc']['hadoop']['phoenix']['phoenixqs']['principal'] =\
-  "HTTP/#{node['fqdn']}@#{node[:bcpc][:hadoop][:kerberos][:realm]}" 
+  "HTTP/#{float_host(node['fqdn'])}@#{node[:bcpc][:hadoop][:kerberos][:realm]}" 
 default['bcpc']['hadoop']['phoenix']['phoenixqs']['localuser'] = true
+default['bcpc']['hadoop']['phoenix']['phoenixqs']['serialization'] = 'JSON'
--- cookbooks/bcpc-hadoop/recipes/phoenix_query_server.rb
+++ cookbooks/bcpc-hadoop/recipes/phoenix_query_server.rb
@@ -4,15 +4,13 @@ include_recipe 'bcpc-hadoop::phoenixqs_kerberos'
 
 site_xml = node.default[:bcpc][:hadoop][:hbase][:site_xml]
 generated_values = {}
-# For UGI to be able to work properly, we need to specify float host 
-# not _HOST, especially since we do not provide keytabs for non floats
 generated_values['phoenix.queryserver.kerberos.principal'] =\
-  "#{node[:bcpc][:hadoop][:kerberos][:data][:phoenixqs][:principal]}/" +
-  float_host(node['fqdn']) +
-  "@#{node[:bcpc][:hadoop][:kerberos][:realm]}"
+  node['bcpc']['hadoop']['phoenix']['phoenixqs']['principal'] =\
 generated_values['phoenix.queryserver.keytab.file'] =\
-  "#{node[:bcpc][:hadoop][:kerberos][:keytab][:dir]}/#{node[:bcpc][:hadoop][:kerberos][:data][:phoenixqs][:keytab]}"
-generated_values['phoenix.queryserver.serialization'] = 'JSON'
+  "#{node[:bcpc][:hadoop][:kerberos][:keytab][:dir]}/" +
+  node[:bcpc][:hadoop][:kerberos][:data][:phoenixqs][:keytab]
+generated_values['phoenix.queryserver.serialization'] =\
+  node['bcpc']['hadoop']['phoenix']['phoenixqs']['serialization']
 
 site_xml.merge!(generated_values)
 
