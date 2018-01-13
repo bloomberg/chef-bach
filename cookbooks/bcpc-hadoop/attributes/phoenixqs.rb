a58746c2b86559de6aefa48879e7a258dd42ebbd
Lev Bronshtein <lbronshtein@bloomberg.net>
2018-01-13 09:51:47 -0500

    keytab location can now be chnaged

--- cookbooks/bcpc-hadoop/attributes/phoenixqs.rb
+++ cookbooks/bcpc-hadoop/attributes/phoenixqs.rb
@@ -3,5 +3,8 @@ default['bcpc']['hadoop']['phoenix']['phoenixqs']['username'] = 'phoenixqs'
 # not _HOST, especially since we do not provide keytabs for non floats
 default['bcpc']['hadoop']['phoenix']['phoenixqs']['principal'] =\
   "HTTP/#{float_host(node['fqdn'])}@#{node[:bcpc][:hadoop][:kerberos][:realm]}" 
+default['bcpc']['hadoop']['phoenix']['phoenixqs']['keytab'] =\
+  "#{node[:bcpc][:hadoop][:kerberos][:keytab][:dir]}/" +
+  node[:bcpc][:hadoop][:kerberos][:data][:spnego][:keytab]
 default['bcpc']['hadoop']['phoenix']['phoenixqs']['localuser'] = true
 default['bcpc']['hadoop']['phoenix']['phoenixqs']['serialization'] = 'JSON'
