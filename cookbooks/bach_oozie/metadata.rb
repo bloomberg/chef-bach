9c5614d926d4d10fbd8de356af5deb0afad898e4
ronny-macmaster <ronnymacmaster@gmail.com>
2018-02-22 14:53:10 -0500

    add bloomberg license to default files.

--- cookbooks/bach_oozie/metadata.rb
+++ cookbooks/bach_oozie/metadata.rb
@@ -1,19 +1,7 @@
-name 'bach_oozie'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures bach_oozie'
-long_description 'Installs/Configures bach_oozie'
-version '0.1.0'
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/bach_oozie/issues' if respond_to?(:issues_url)
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/bach_oozie' if respond_to?(:source_url)
+name             'bach_oozie'
+maintainer       'Bloomberg Finance L.P.'
+maintainer_email 'hadoop@bloomberg.net'
+license          'Apache License 2.0'
+description      'Library cookbook for a simple oozie client'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
