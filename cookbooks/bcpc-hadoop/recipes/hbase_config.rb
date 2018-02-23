a31e74fdd9b17b40e71bc05665cc4bb7be4c0f7c
Clay Baenziger <cbaenziger@bloomberg.net>
2018-02-23 11:51:20 -0500

    Add support for HBASE-16700
    (cherry picked from commit 7cf87ea108bc62fee7198791b83cc4dffd13a647)

--- cookbooks/bcpc-hadoop/recipes/hbase_config.rb
+++ cookbooks/bcpc-hadoop/recipes/hbase_config.rb
@@ -81,15 +81,25 @@ end
 if node[:bcpc][:hadoop][:kerberos][:enable] == true then
   generated_values['hbase.security.authorization'] = 'true'
   generated_values['hbase.superuser'] = node[:bcpc][:hadoop][:hbase][:superusers].join(',')
-  generated_values['hbase.coprocessor.region.classes'] =
-    'org.apache.hadoop.hbase.security.token.TokenProvider,' +
-    'org.apache.hadoop.hbase.security.access.SecureBulkLoadEndpoint,' +
-    'org.apache.hadoop.hbase.security.access.AccessController'
+  region_classes = [
+     'org.apache.hadoop.hbase.security.token.TokenProvider',
+     'org.apache.hadoop.hbase.security.access.SecureBulkLoadEndpoint',
+     'org.apache.hadoop.hbase.security.access.AccessController',
+     node['bcpc']['hadoop']['hbase']['site_xml'].fetch('hbase.coprocessor.region.classes', nil)
+  ].select{|c| c}.join(',')
+  regionserver_classes = [
+    'org.apache.hadoop.hbase.security.access.AccessController',
+    node['bcpc']['hadoop']['hbase']['site_xml'].fetch('hbase.coprocessor.regionserver.classes', nil)
+  ].select{|c| c}.join(',')
+  master_classes = [
+    'org.apache.hadoop.hbase.security.access.AccessController',
+    'org.apache.hadoop.hbase.security.access.CoprocessorWhitelistMasterObserver',
+    node['bcpc']['hadoop']['hbase']['site_xml'].fetch('hbase.coprocessor.master.classes', nil)
+  ].select{|c| c}.join(',')
+  generated_values['hbase.coprocessor.region.classes'] = region_classes
+  generated_values['hbase.coprocessor.regionserver.classes'] = regionserver_classes
+  generated_values['hbase.coprocessor.master.classes'] = master_classes
   generated_values['hbase.security.exec.permission.checks'] = 'true'
-  generated_values['hbase.coprocessor.regionserver.classes'] =
-    'org.apache.hadoop.hbase.security.access.AccessController'
-  generated_values['hbase.coprocessor.master.classes'] =
-    'org.apache.hadoop.hbase.security.access.AccessController'
   generated_values['hbase.security.authentication'] = 'kerberos'
   generated_values['hbase.master.kerberos.principal'] =
     "#{node[:bcpc][:hadoop][:kerberos][:data][:hbase][:principal]}/" +
