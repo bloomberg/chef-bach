021136bbf3193ae172ad804e6d4136fe13dc0991
Amit Anand <aanand59@bloomberg.net>
2018-04-11 17:14:47 -0400

    Implemented post review comments

--- cookbooks/bach_hannibal/recipes/default.rb
+++ cookbooks/bach_hannibal/recipes/default.rb
@@ -1,1 +1,5 @@
 node.force_default[:hannibal][:download_url] = get_binary_server_url
+
+include_recipe "bach_hannibal::hannibalhb"
+include_recipe "hannibal::default"
+include_recipe "hannibal::hannibal_service"
