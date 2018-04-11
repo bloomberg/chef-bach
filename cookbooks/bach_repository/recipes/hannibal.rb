1949452584d8e1c92216bd0def5b08e93f4fe8e6
Amit Anand <aanand59@bloomberg.net>
2018-04-11 16:53:12 -0400

    Add hannibal tar to bins

---
+++ cookbooks/bach_repository/recipes/hannibal.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: bach_repository
+# Recipe:: oracle_java
+#
+require 'pathname'
+include_recipe 'bach_repository::directory'
+bins_dir = node['bach']['repository']['bins_directory']
+
+hbase_version = node[:hannibal][:hbase_version]
+src_filename = "hannibal-hbase#{hbase_version}.tgz"
+
+remote_file "#{bins_dir}/#{src_filename}" do
+  source "#{node[:hannibal][:download_url]}/#{src_filename}"
+  user 'root'
+  group 'root'
+  mode 0444
+  checksum node[:hannibal][:checksum]["#{hbase_version}"]
+end
