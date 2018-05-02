1e7d53fe6da4ae4b32e4893788d621dd8e86cf0f
Amit Anand <aanand59@bloomberg.net>
2018-05-02 15:27:44 -0400

    Fixed syntax errors

--- cookbooks/bach_ambari/recipes/default.rb
+++ cookbooks/bach_ambari/recipes/default.rb
@@ -18,64 +18,64 @@
 #
 
 ambari_admin_password = make_config('ambari-admin-password', secure_password)
-force_default['ambari']['admin']['password'] = ambari_admin_password
+node.force_default['ambari']['admin']['password'] = ambari_admin_password
 
 namenodes_cach = get_namenodes
 namenodes = namenodes_cach.map { |nn| "namenode#{nn['node_id']}" }.join(',')
 
 namenodes_fqdn = namenodes_cach.map { |nn| float_host(nn['fqdn']) }
 
-force_default['ambari']['webhdfs.ha.namenode.http-address.nn1'] = \
+node.force_default['ambari']['webhdfs.ha.namenode.http-address.nn1'] = \
   "#{namenodes_fqdn[0]}:" \
   "#{node['bcpc']['hadoop']['namenode']['http']['port']}"
 
-force_default['ambari']['webhdfs.ha.namenode.https-address.nn1'] = \
+node.force_default['ambari']['webhdfs.ha.namenode.https-address.nn1'] = \
   "#{namenodes_fqdn[0]}:" \
   "#{node['bcpc']['hadoop']['namenode']['https']['port']}"
 
-force_default['ambari']['webhdfs.ha.namenode.rpc-address.nn1'] = \
+node.force_default['ambari']['webhdfs.ha.namenode.rpc-address.nn1'] = \
   "#{namenodes_fqdn[0]}:" \
   "#{node['bcpc']['hadoop']['namenode']['rpc']['port']}"
 
 if node['bcpc']['hadoop']['hdfs']['HA']
-  force_default['ambari']['webhdfs.ha.namenode.http-address.nn2'] = \
+  node.force_default['ambari']['webhdfs.ha.namenode.http-address.nn2'] = \
     "#{namenodes_fqdn[1]}:" \
     "#{node['bcpc']['hadoop']['namenode']['http']['port']}"
 
-  force_default['ambari']['webhdfs.ha.namenode.https-address.nn2'] = \
+  node.force_default['ambari']['webhdfs.ha.namenode.https-address.nn2'] = \
     "#{namenodes_fqdn[1]}:" \
     "#{node['bcpc']['hadoop']['namenode']['https']['port']}"
 
-  force_default['ambari']['webhdfs.ha.namenode.rpc-address.nn2'] = \
+  node.force_default['ambari']['webhdfs.ha.namenode.rpc-address.nn2'] = \
     "#{namenodes_fqdn[1]}:" \
     "#{node['bcpc']['hadoop']['namenode']['rpc']['port']}"
 end
 
-force_default['ambari']['webhdfs.ha.namenodes.list'] = namenodes
-force_default['ambari']['webhdfs.nameservices'] = node.chef_environment
-force_default['ambari']['webhdfs.url'] = \
+node.force_default['ambari']['webhdfs.ha.namenodes.list'] = namenodes
+node.force_default['ambari']['webhdfs.nameservices'] = node.chef_environment
+node.force_default['ambari']['webhdfs.url'] = \
   "webhdfs://#{node.chef_environment}"
 
 ambari_proxy_user = node['bcpc']['hadoop']['proxyuser']['ambari'].to_s
 kerberos_realm = node['bcpc']['hadoop']['kerberos']['realm']
 
 if node['bcpc']['hadoop']['kerberos']['enable']
-  force_default['ambari']['webhdfs.auth'] =
+  node.force_default['ambari']['webhdfs.auth'] =
     "auth=KERBEROS;proxyuser=#{ambari_proxy_user}"
-  force_default['ambari']['kerberos']['enabled'] = true
-  force_default['ambari']['kerberos']['principal'] =
+  node.force_default['ambari']['kerberos']['enabled'] = true
+  node.force_default['ambari']['kerberos']['principal'] =
     "#{ambari_proxy_user}/#{float_host(node['fqdn'])}@#{kerberos_realm}"
-  force_default['ambari']['hadoop.security.authentication'] = 'kerberos'
-  force_default['ambari']['timeline.http.auth.type'] = 'kerberos'
-  force_default['ambari']['hadoop.http.auth.type'] = 'kerberos'
+  node.force_default['ambari']['hadoop.security.authentication'] = 'kerberos'
+  node.force_default['ambari']['timeline.http.auth.type'] = 'kerberos'
+  node.force_default['ambari']['hadoop.http.auth.type'] = 'kerberos'
 end
 
 if node['ambari']['ldap_password'].nil?
-  force_default['ambari']['ldap_password'] = \
+  node.force_default['ambari']['ldap_password'] = \
     get_config('password', 'ldap', 'os')
 end
 
-force_default['ambari']['ambari_server_base_url'] = \
+node.force_default['ambari']['ambari_server_base_url'] = \
   "http://#{float_host(node['fqdn'])}:8080"
 
 set_hosts
@@ -97,7 +97,7 @@ if node['bcpc']['hadoop']['kerberos']['enable'] && hive_srvr_auth == 'KERBEROS'
   hive_jdbc_url += hive_principal
 end
 
-force_default['ambari']['hive.jdbc.url'] = hive_jdbc_url
+node.force_default['ambari']['hive.jdbc.url'] = hive_jdbc_url
 ts_port = node['bcpc']['hadoop']['yarn']['timeline_server']['webapp']['port']
 rm_port = node['bcpc']['hadoop']['yarn']['resourcemanager']['webapp']['port']
 
@@ -105,13 +105,13 @@ resource_manager_url = \
   node['bcpc']['hadoop']['rm_hosts']
   .map { |r| "http://#{float_host(r['hostname'])}:#{rm_port}" }.join(',')
 
-force_default['ambari']['yarn.resourcemanager.url'] = resource_manager_url
+node.force_default['ambari']['yarn.resourcemanager.url'] = resource_manager_url
 
 timeline_server = \
   get_timeline_servers
   .map { |e| "http://#{float_host(e['hostname'])}:#{ts_port}" }.first
 
-force_default['ambari']['yarn.ats.url'] = timeline_server
+node.force_default['ambari']['yarn.ats.url'] = timeline_server
 
 oozie_port = node['bcpc']['hadoop']['oozie_port']
 oozie_ha_port = node['bcpc']['ha_oozie']['port']
@@ -132,8 +132,8 @@ rm_nport = node['bcpc']['hadoop']['yarn']['resourcemanager']['port']
 rm_address = node['bcpc']['hadoop']['rm_hosts']
              .map { |r| "http://#{float_host(r['hostname'])}:#{rm_nport}" }
              .join(',')
-default['ambari']['oozie.service.uri'] = "#{oozie_url}/oozie"
-default['ambari']['yarn.resourcemanager.address'] = rm_address
+node.default['ambari']['oozie.service.uri'] = "#{oozie_url}/oozie"
+node.default['ambari']['yarn.resourcemanager.address'] = rm_address
 
 # install mysql jdbc driver
 package 'mysql-connector-java' do
