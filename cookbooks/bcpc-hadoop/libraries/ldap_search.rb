0cd3cc695a620b733c17a600dccff10b38272d75
ronny-macmaster <ronnymacmaster@gmail.com>
2018-08-03 17:04:12 -0400

    fix ldapsearch for hdfs dir creation

---
+++ cookbooks/bcpc-hadoop/libraries/ldap_search.rb
@@ -0,0 +1,69 @@
+# ldap_search.rb
+# Routines for querying the LDAP server
+
+module Bcpc_Hadoop::Helper
+  class LDAPSearch
+    include Chef::Mixin::ShellOut
+    # Initialize LDAPSearch
+    # host - LDAP Host to query
+    # port - LDAP Port to query
+    # user - Principal to use for simple authentication
+    # password - Password to use for simple authentication
+    # base_dn - DN to begin search from
+    # roles_dn - DN to search for role accounts from
+    def initialize(config)
+      @host = config[:host]
+      @port = config[:port]
+
+      @user = config[:user]
+      @password = config[:password]
+
+      @base_dn = config[:base_dn]
+      @roles_dn = config[:roles_dn]
+    end
+
+    # Find objects in an LDAP group
+    # @mode: Symbol of object type to find for (e.g. :user, :role, :group)
+    # @group: fully distinguished group DN to find objects for
+    # @attr: String of attribute to find (e.g. "sAMAccountName" or "dn")
+    #
+    # Returns Array of string object names found
+    # Raises RuntimeError on any unspecified error
+    def group_search(mode, group, attr = 'sAMAccountName')
+      strategy = {
+        role: "'(&" \
+          "(objectClass=user)" \
+          "(!(objectClass=computer))" \
+          "(memberOf=#{@roles_dn})" \
+          "(memberOf=%{group})" \
+        ")'",
+        user: "'(&" \
+          "(objectClass=user)" \
+          "(!(objectClass=computer))" \
+          "(employeeID=*)" \
+          "(memberOf=%{group})" \
+        ")'",
+        group: "'(&" \
+          "(objectClass=group)" \
+          "(memberOf=%{group})" \
+        ")'"
+      }
+
+      ldap_filter = strategy[mode] or raise TypeError,
+        "Unknown search strategy: #{mode}. valid options: #{strategy.keys}"
+
+      auth = (@user && @password && "-D '#{@user}' -w #{@password}")
+      ldap_search = "ldapsearch -LLL -o ldif-wrap=no -E pr=1000/noprompt " \
+        "-h #{@host} -p #{@port} -b '#{@base_dn}' #{auth} " \
+        "#{ldap_filter} %{attr} "
+
+      return shell_out!(ldap_search % {
+        group: group,
+        attr: attr,
+      }).stdout.split("\n").reduce([]) do |acc, curr|
+        object = curr[/^#{attr}: (.*)/, 1]
+        object ? acc.push(object) : acc
+      end
+    end
+  end
+end
--- cookbooks/bcpc-hadoop/recipes/hdfs_group_directories.rb
+++ cookbooks/bcpc-hadoop/recipes/hdfs_group_directories.rb
@@ -1,77 +1,8 @@
-require "mixlib/shellout"
-
 Chef::Resource::RubyBlock.send(:include, Bcpc_Hadoop::Helper)
 
-%w(ldap-utils libsasl2-modules-gssapi-mit).each do |pkg|
-  package pkg do
-    action :upgrade
-  end
-end
-
-ruby_block "generate_group_dirs" do
+ruby_block "hdfs_group_directories" do
   block do
-    initialize_ldap(node[:bcpc][:hadoop][:ldap_query_keytab], "ldap://#{node[:bcpc][:hadoop][:domain]}")
-    LDAP_BASE="DC=#{node[:bcpc][:hadoop][:base_dn]}"
-
-    access_groups = []
-    roles = []
-
-    # Get all the role accounts from cluster group
-    roles.concat(find_object_by_group(LDAP_BASE, :role, "CN=#{node[:bcpc][:hadoop][:acl_group]},#{node[:bcpc][:hadoop][:group_ou]}"))
-
-    # Get all the groups from cluster group
-    access_groups = find_object_by_group(LDAP_BASE, :group, "CN=#{node[:bcpc][:hadoop][:acl_group]},#{node[:bcpc][:hadoop][:group_ou]}", 'dn')
-
-    # Loop through each group and push role accounts from each group to role_groups array
-    while !access_groups.empty? do
-      distName = access_groups.shift
-      # Find all the role accounts in the given group
-      roles.concat(find_object_by_group(LDAP_BASE, :role, distName))
-
-      # Find all the groups in the given group
-      access_groups.concat(find_object_by_group(LDAP_BASE, :group, distName, 'dn'))
-    end
-
-
-    # Build a hash role_groups = {group1: [user1, user2, ...], group2: [...], ...}
-
-    # Get all group DN's roles are in
-    role_groups = {}
-    while user = roles.pop()
-      groups(user).each do |g|
-        role_groups[g] = []
-      end
-    end
-
-    # For each group get a list of all user and role accounts in the group
-    role_groups.keys.each do |g|
-      begin
-        role_groups[g] = role_groups[g].concat(getent(:group, g)['members'])
-      rescue KeyError
-        # remove groups which are not found
-        # (e.g. race conditions and groups which do not resolve to a name)
-        role_groups.delete(g)
-      end
-    end
-
-    require 'thread'
-    group_queue = Queue.new()
-    # create a directory if the group has more than one user and matches group naming rules
-    role_groups.each_pair{|g, users| group_queue.push(g) if filter_nonproject_groups(g, users, node[:bcpc][:hadoop][:group_dir_prohibited_groups])}
-    threads = (0..node[:bcpc][:hadoop][:dir_threads]).map do
-      Thread.new do
-        begin
-          while group = group_queue.pop(true)
-            # Chef::Log does not seem to work in RubyBlocks and may not be thread safe
-            # puts will step on itself but splat to the screen if in debug mode
-            puts "Group: #{group}" if Chef::Config[:log_level] == :debug
-            new_dir_creation(node[:bcpc][:hadoop][:hdfs_url], "/groups/#{group}", "hdfs:#{group}", node[:bcpc][:hadoop][:hadoop][:group_dir_mode], node.run_context)
-          end
-        rescue ThreadError => e
-        end
-      end
-    end
-    threads.map(&:join)
+    # create the /groups dirs: mode, dirs, home, perms
+    dir_creation('groups', cluster_groups, '/groups', '0770')
   end
-  action :run
 end
