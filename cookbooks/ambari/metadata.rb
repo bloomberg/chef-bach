150975165b49728a840580d12278478b34159c15
Salvatore LaMendola <slamendola2@bloomberg.net>
2018-08-06 17:27:52 -0400

    Cut new maintenance release v3.3.22

--- cookbooks/ambari/metadata.rb
+++ cookbooks/ambari/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'hadoop@bloomberg.net'
 license 'Apache License 2.0'
 description 'Installs/Configures ambari-views-chef'
 long_description 'Installs/Configures ambari-views-chef'
-version '3.3.21'
+version '3.3.22'
 
 depends 'postgresql'
