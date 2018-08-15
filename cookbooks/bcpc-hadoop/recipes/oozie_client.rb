ced55a3dbfba513ae46206467205d7974b8077ae
Clay Baenziger <cbaenziger@bloomberg.net>
2018-08-15 11:34:18 -0400

    No need for Oozie config files on clients

--- cookbooks/bcpc-hadoop/recipes/oozie_client.rb
+++ cookbooks/bcpc-hadoop/recipes/oozie_client.rb
@@ -2,9 +2,7 @@
 # Recipe Name : oozie_client
 # Description : To setup oozie-client
 
-include_recipe 'bcpc-hadoop::oozie_config'
 ::Chef::Recipe.send(:include, Bcpc_Hadoop::Helper)
-::Chef::Resource::Bash.send(:include, Bcpc_Hadoop::Helper)
 
 package hwx_pkg_str('oozie-client', node[:bcpc][:hadoop][:distribution][:release]) do
    action :install
