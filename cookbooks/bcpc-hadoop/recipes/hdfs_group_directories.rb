895dc7471b9fba83c82d4bcf1a41021ac493deba
Ronald Macmaster <ronnymacmaster@gmail.com>
2018-08-30 18:54:06 -0400

    break dir_creation library dependency on global state. document methods
    (cherry picked from commit 2f0460f564c12d4c0d3bf6a6cb2fb99783aa58ef)

--- cookbooks/bcpc-hadoop/recipes/hdfs_group_directories.rb
+++ cookbooks/bcpc-hadoop/recipes/hdfs_group_directories.rb
@@ -3,13 +3,23 @@ Chef::Resource::RubyBlock.send(:include, Chef::Mixin::DeepMerge)
 
 ruby_block "hdfs_group_directories" do
   block do
+    # Default configuration for group directory creation
+    groups_dir_creation =
+      node.default['bcpc']['hadoop']['dir_creation']['groups']
 
     # Create directories for existing LDAP groups.
-    dirinfo = node.default['bcpc']['hadoop']['hdfs']['groups']['dirinfo']
+    # Add dirinfo entries for each user returned by LDAP queries.
+    dirinfo = groups_dir_creation['dirinfo']
     (cluster_groups).each do |group|
-      dirinfo[group] = deep_merge(dirinfo[group], { group: group })
+      space_quota = groups_dir_creation["#{group}_space_quota"]
+      ns_quota = groups_dir_creation["#{group}_ns_quota"]
+
+      dirinfo[group] = deep_merge({ group: group }, dirinfo[group])
+      dirinfo[group] = deep_merge(dirinfo[group], { space_quota: space_quota })
+      dirinfo[group] = deep_merge(dirinfo[group], { ns_quota: ns_quota })
     end
 
-    dir_creation('groups')
+    # Create group directories in /groups.
+    dir_creation('/groups', groups_dir_creation['defaults'], dirinfo)
   end
 end
--- cookbooks/bcpc-hadoop/recipes/hdfs_user_directories.rb
+++ cookbooks/bcpc-hadoop/recipes/hdfs_user_directories.rb
@@ -3,13 +3,23 @@ Chef::Resource::RubyBlock.send(:include, Chef::Mixin::DeepMerge)
 
 ruby_block "hdfs_user_directories" do
   block do
+    # Default configuration for user directory creation
+    user_dir_creation =
+      node.default['bcpc']['hadoop']['dir_creation']['user']
 
     # Create directories for existing LDAP users and role accounts.
-    dirinfo = node.default['bcpc']['hadoop']['hdfs']['user']['dirinfo']
+    # Add dirinfo entries for each user returned by LDAP queries.
+    dirinfo = user_dir_creation['dirinfo']
     (cluster_users + cluster_roles).each do |user|
-      dirinfo[user] = deep_merge(dirinfo[user], { owner: user })
+      space_quota = user_dir_creation["#{user}_space_quota"]
+      ns_quota = user_dir_creation["#{user}_ns_quota"]
+
+      dirinfo[user] = deep_merge({ owner: user }, dirinfo[user])
+      dirinfo[user] = deep_merge(dirinfo[user], { space_quota: space_quota })
+      dirinfo[user] = deep_merge(dirinfo[user], { ns_quota: ns_quota })
     end
 
-    dir_creation('user')
+    # Create user directories in /user.
+    dir_creation('/user', user_dir_creation['defaults'], dirinfo)
   end
 end
