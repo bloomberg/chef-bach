42884dd02083d6948915a824ac131932d3b58573
Lei Chen <lchen403@bloomberg.net>
2018-09-17 15:17:13 -0400

    put worker nodes dont need the bach_web recipe
    (cherry picked from commit 5a63427e96f8870732b1a4875e9a5e4236d66b46)

--- cookbooks/bcpc-hadoop/recipes/namenode_master.rb
+++ cookbooks/bcpc-hadoop/recipes/namenode_master.rb
@@ -3,6 +3,7 @@ require 'digest'
 require 'mixlib/shellout'
 
 include_recipe 'bcpc-hadoop::hadoop_config'
+include_recipe 'bcpc-hadoop::hdfs_web'
 include_recipe 'bcpc-hadoop::namenode_queries'
 ::Chef::Recipe.send(:include, Bcpc_Hadoop::Helper)
 ::Chef::Resource::Bash.send(:include, Bcpc_Hadoop::Helper)
