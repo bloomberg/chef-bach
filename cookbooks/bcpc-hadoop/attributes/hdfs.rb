faab7ae5822203343c14054247194bd0977b743a
Lei Chen <lchen403@bloomberg.net>
2018-09-27 17:14:40 -0400

    2018-09-27_17-14

--- cookbooks/bcpc-hadoop/attributes/hdfs.rb
+++ cookbooks/bcpc-hadoop/attributes/hdfs.rb
@@ -126,8 +126,6 @@ default['bcpc']['bach_web']['services']['hdfs'] = {
     'blacklist' => [
       'hadoop.security.group.mapping.ldap.bind.password.file'
     ],
-    'whitelist' => [
-      '.*'
-    ]
+    'whitelist' => nil
   }
 }
