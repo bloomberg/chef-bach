0829596caedeb16d6187abf57de88bc74c79a4b9
Clay Baenziger <cbaenziger@bloomberg.net>
2018-10-06 21:49:42 -0400

    Fix for Chef-BACH missing recipe bcpc-hadoop::hdfs_web

--- cookbooks/bcpc-hadoop/recipes/namenode_standby.rb
+++ cookbooks/bcpc-hadoop/recipes/namenode_standby.rb
@@ -1,5 +1,4 @@
 include_recipe 'bcpc-hadoop::hadoop_config'
-include_recipe 'bcpc-hadoop::hdfs_web'
 require "base64"
 ::Chef::Recipe.send(:include, Bcpc_Hadoop::Helper)
 ::Chef::Resource::Bash.send(:include, Bcpc_Hadoop::Helper)
