0465a1e60397ab01c74e4e80ce80f530ec93df4e
Salvatore LaMendola <slamendola2@bloomberg.net>
2018-10-23 16:58:02 -0400

    Cut new maintenance release v3.3.27

--- cookbooks/bach_krb5/metadata.rb
+++ cookbooks/bach_krb5/metadata.rb
@@ -5,10 +5,10 @@ maintainer       'Bloomberg Finance L.P.'
 maintainer_email 'hadoop@bloomberg.net'
 description      'Wrapper cookbook for krb5 community cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.3.26'
+version          '3.3.27'
 
-depends 'bcpc', '= 3.3.26'
-depends 'bcpc-hadoop', '= 3.3.26'
+depends 'bcpc', '= 3.3.27'
+depends 'bcpc-hadoop', '= 3.3.27'
 depends 'krb5'
 
 %w(ubuntu).each do |os|
