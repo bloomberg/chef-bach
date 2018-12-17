df9c2cd0648576804cc868a040425ba3b6f169e6
Allan Espinosa <aespinosa33@bloomberg.net>
2018-12-17 13:15:10 -0500

    Fix typo

--- cookbooks/bach_roles/recipes/admin.rb
+++ cookbooks/bach_roles/recipes/admin.rb
@@ -1,6 +1,6 @@
 # "Role" recipe that is supposed to generate databags and supply them to the
 # chef-server.  Mean to be run in an "admin" account on the chef-server.h
-include_recipe 'bcpc::admin_repositorhy_apt'
+include_recipe 'bcpc::admin_repository_apt'
 include_recipe 'bcpc::admin_certs'
 include_recipe 'bcpc-hadoop::admin_ssl'
 include_recipe 'bcpc::admin_cobbler'
@@ -16,3 +16,7 @@ include_recipe 'bcpc::admin_ssh'
 # c-a-r bootstrap related credentials
 include_recipe 'bcpc::mysql_data_bags'
 include_recipe 'bcpc::admin'
+
+# c-a-r hadoop related credentials
+
+# TODO: c-a-r kakfa related credentials
